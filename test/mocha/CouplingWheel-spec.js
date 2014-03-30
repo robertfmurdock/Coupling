712542e453a4191fb40b2b9c9bceae0c98b20de4
robertfmurdock <rmurdock@pillartechnology.com>
2014-03-30 16:19:22 -0400

    Some refactoring to the Coupling Game to help better reflect how turns will go. (spin for first, then spin for partner).
    - RMUR

--- test/mocha/CouplingWheel-spec.js
+++ test/mocha/CouplingWheel-spec.js
@@ -4,6 +4,13 @@ var sinon = require('sinon');
 
 describe('Coupling Wheel', function () {
 
+    it('should know the players on the wheel', function () {
+        var players = ['Scooby', 'Shaggy', 'Scrappy'];
+        var couplingWheel = new CouplingWheel(players);
+
+        should(players).eql(couplingWheel.players);
+    });
+
     it('randomly chooses a person on the wheel', function () {
         var players = ['Scooby', 'Shaggy', 'Scrappy'];
         var randomStub = sinon.stub(Math, 'random');
@@ -12,7 +19,8 @@ describe('Coupling Wheel', function () {
             randomStub.returns(expectedIndex / players.length);
             var couplingWheel = new CouplingWheel(players);
             var foundPlayer = couplingWheel.spin();
-            should(players[expectedIndex]).eql(foundPlayer);
+            var expectedPlayer = players[expectedIndex];
+            should(expectedPlayer).eql(foundPlayer);
         }
 
         checkWorksForIndex(1);
