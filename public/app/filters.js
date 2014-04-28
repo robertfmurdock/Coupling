d0ff5563883e3a3c6a83eef84eb92e21654925b8
robertfmurdock <rmurdock@pillartechnology.com>
2014-04-27 22:23:58 -0400

    Made the tribes selection page look infinitely better. - RMUR

--- public/app/filters.js
+++ public/app/filters.js
@@ -2,10 +2,20 @@ var filters = angular.module("coupling.filters", []);
 filters.filter('gravatarUrl', function (gravatarService) {
     return function (player, options) {
         if (player && player.image) {
-            return "/images/icons/" + player.image;
+            return "/images/icons/players/" + player.image;
         } else {
             var email = player && player.email ? player.email : "";
             return gravatarService.url(email, options);
+        }
+    }
+});
+
+filters.filter('tribeImageUrl', function () {
+    return function (tribe) {
+        if (tribe && tribe.image) {
+            return "/images/icons/tribes/" + tribe.image;
+        } else {
+            return "/images/icons/tribes/no-tribe-icon.jpg";
         }
     }
 });
