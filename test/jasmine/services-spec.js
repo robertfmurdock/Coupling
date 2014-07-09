3900ef64a1a2b6d8d59c1a7a93f27c079fe61d29
robertfmurdock <rmurdock@pillartechnology.com>
2014-07-09 11:27:55 -0400

    Basic unit test rearrangement. - RMUR

--- test/jasmine/services-spec.js
+++ test/jasmine/services-spec.js
@@ -17,39 +17,40 @@ describe('Service: ', function () {
             });
         });
 
-        it('can request tribes successfully', function () {
-            var expectedTribes = [
-                {_id: 'one'},
-                {_id: 'two'}
-            ];
+        describe('get tribes', function () {
+            it('calls back with tribes on success', function () {
+                var expectedTribes = [
+                    {_id: 'one'},
+                    {_id: 'two'}
+                ];
 
-            httpBackend.whenGET('/api/tribes').respond(200, expectedTribes);
+                httpBackend.whenGET('/api/tribes').respond(200, expectedTribes);
 
-            var returnedTribes = null;
-            Coupling.getTribes(function (resultTribes) {
-                returnedTribes = resultTribes;
+                var returnedTribes = null;
+                Coupling.getTribes(function (resultTribes) {
+                    returnedTribes = resultTribes;
+                });
+
+                httpBackend.flush();
+
+                expect(returnedTribes).toEqual(expectedTribes);
             });
 
-            httpBackend.flush();
+            it('shows error on failure', function () {
+                var statusCode = 404;
+                var url = '/api/tribes';
+                var expectedData = 'nonsense';
+                httpBackend.whenGET(url).respond(statusCode, expectedData);
+                var callCount = 0;
+                Coupling.getTribes(function () {
+                    callCount++;
+                });
 
-            expect(returnedTribes).toEqual(expectedTribes);
-        });
-
-        it('will handle requesting tribes unsuccessfully', function () {
-            var statusCode = 404;
-            var url = '/api/tribes';
-            var expectedData = 'nonsense';
-            httpBackend.whenGET(url).respond(statusCode, expectedData);
-            var callCount = 0;
-            Coupling.getTribes(function () {
-                callCount++;
+                var alertSpy = spyOn(window, 'alert');
+                httpBackend.flush();
+                expect(callCount).toBe(0);
+                expect(alertSpy).toHaveBeenCalledWith('There was a problem loading ' + url + ' ' + expectedData + ' Got status code: ' + statusCode);
             });
-
-            var alertSpy = spyOn(window, 'alert');
-            httpBackend.flush();
-            expect(callCount).toBe(0);
-
-            expect(alertSpy).toHaveBeenCalledWith('There was a problem loading ' + url + ' ' + expectedData + ' Got status code: ' + statusCode);
         });
 
 
