71b07ccb5843744c60c06e883b8b02907b0fbda3
robertfmurdock <rmurdock@pillartechnology.com>
2014-07-11 17:09:26 -0400

    More futzing around for cloudbees.
    - RMUR

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ var app = express();
 
 var port = process.env.PORT || config.port;
 var mongoUrl = process.env.MONGOHQ_URL_MONGOURL || config.mongoUrl;
+
 app.set('port', port);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
