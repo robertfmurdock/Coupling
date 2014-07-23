f337cc8c4352535c372b3ad3144ff3d6f3f656b3
robertfmurdock <rmurdock@pillartechnology.com>
2014-07-23 11:27:23 -0400

    Adding new object to manage persisting of user information to mongo and back. Not fully implemented yet.
    - RMUR

---
+++ test/mocha/UserDataService-spec.js
@@ -0,0 +1,34 @@
+"use strict";
+var should = require('should');
+var mongoUrl = 'localhost/UsersTest';
+var monk = require('monk');
+var _ = require('underscore');
+var Comparators = require('../../lib/Comparators');
+var database = monk(mongoUrl);
+var UserDataService = require("../../lib/UserDataService");
+var userDataService = new UserDataService(mongoUrl);
+
+describe('UserDataService', function () {
+
+    var usersCollection = database.get('users');
+
+    beforeEach(function () {
+        usersCollection.drop();
+    });
+
+    describe('findOrCreate', function () {
+        it('will create a user if it does not already exist', function (done) {
+            var email = 'awesome.o@super.coo';
+            userDataService.findOrCreate(email, function (user) {
+                should.exist(user);
+                user.email.should.equal(email);
+
+                usersCollection.find({}, function (error, docs) {
+                    docs.length.should.equal(1);
+                    docs[0].should.eql(user);
+                    done(error);
+                });
+            });
+        });
+    });
+});
