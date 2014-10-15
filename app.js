f8430c66f0d952ebe80e876171df8f66c31eb709
robertfmurdock <rmurdock@pillartechnology.com>
2014-10-15 12:56:48 -0400

    Delaying starting of express server until a connection to Mongo is made. Hopefully this helps resolve the Jenkins issue.

--- app.js
+++ app.js
@@ -21,98 +21,99 @@ var TribeRoutes = require('./routes/tribes');
 var spin = require('./routes/spin');
 var config = require('./config');
 var userDataService = new UserDataService(config.mongoUrl);
+var mongoStore = new MongoStore({
+    url: config.mongoUrl
+}, function () {
+    console.log('Finished initializing session storage. Starting app.');
+    var app = express();
 
-var app = express();
+    app.set('port', config.port);
+    app.set('views', path.join(__dirname, 'views'));
+    app.set('view engine', 'jade');
+    app.use(favicon('public/images/favicon.ico'));
+    app.use(logger('dev'));
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.json());
+    app.use(methodOverride());
+    app.use(express.static(path.join(__dirname, 'public')));
+    app.use(cookieParser());
 
-app.set('port', config.port);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.use(favicon('public/images/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-app.use(methodOverride());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(cookieParser());
-app.use(session({
-    secret: config.secret,
-    resave: true,
-    saveUninitialized: true,
-    store: new MongoStore({
-        url: config.mongoUrl
-    }, function () {
-        console.log('Finished initializing session storage.');
-    })
-}));
-app.use(passport.initialize());
-app.use(passport.session());
+    app.use(session({
+        secret: config.secret,
+        resave: true,
+        saveUninitialized: true,
+        store: mongoStore
+    }));
+    app.use(passport.initialize());
+    app.use(passport.session());
 
 // development only
-if ('development' == app.get('env')) {
-    app.use(errorHandler());
-}
-
-passport.serializeUser(userDataService.serializeUser);
-passport.deserializeUser(userDataService.deserializeUser);
-
-passport.use(new GoogleStrategy({
-        clientID: config.googleClientID,
-        clientSecret: config.googleClientSecret,
-        callbackURL: config.publicUrl + '/auth/google/callback',
-        scope: 'https://www.googleapis.com/auth/plus.login email'
-    },
-    function (accessToken, refreshToken, profile, done) {
-        userDataService.findOrCreate(profile.emails[0].value, function (user) {
-            done(null, user);
-        });
+    if ('development' == app.get('env')) {
+        app.use(errorHandler());
     }
-));
 
-app.get('/auth/google', passport.authenticate('google'));
-app.get('/auth/google/callback', passport.authenticate('google', { successRedirect: '/', failureRedirect: '/auth/google' }));
+    passport.serializeUser(userDataService.serializeUser);
+    passport.deserializeUser(userDataService.deserializeUser);
 
-if ('development' == app.get('env')) {
-    console.log('Dev Environment: enabling test login');
-    passport.use(new LocalStrategy(function (username, password, done) {
-        userDataService.findOrCreate(username, function (user) {
-            done(null, user);
-        });
-    }));
-    app.get('/test-login', passport.authenticate('local', { successRedirect: '/', failureRedirect: '/login' }));
-}
+    passport.use(new GoogleStrategy({
+            clientID: config.googleClientID,
+            clientSecret: config.googleClientSecret,
+            callbackURL: config.publicUrl + '/auth/google/callback',
+            scope: 'https://www.googleapis.com/auth/plus.login email'
+        },
+        function (accessToken, refreshToken, profile, done) {
+            userDataService.findOrCreate(profile.emails[0].value, function (user) {
+                done(null, user);
+            });
+        }
+    ));
 
-var checkApiAccess = function (request, response, next) {
-    if (config.requiresAuthentication && !request.isAuthenticated())
-        response.send(401);
-    else
-        next();
-};
+    app.get('/auth/google', passport.authenticate('google'));
+    app.get('/auth/google/callback', passport.authenticate('google', { successRedirect: '/', failureRedirect: '/auth/google' }));
 
-var tribes = new TribeRoutes(config.mongoUrl);
-app.get('/', routes.index);
-app.all('/api/*', checkApiAccess);
-app.route('/api/tribes')
-    .get(tribes.list)
-    .post(tribes.save);
-app.post('/api/:tribeId/spin', spin(config.mongoUrl));
+    if ('development' == app.get('env')) {
+        console.log('Dev Environment: enabling test login');
+        passport.use(new LocalStrategy(function (username, password, done) {
+            userDataService.findOrCreate(username, function (user) {
+                done(null, user);
+            });
+        }));
+        app.get('/test-login', passport.authenticate('local', { successRedirect: '/', failureRedirect: '/login' }));
+    }
 
-var history = new HistoryRoutes(config.mongoUrl);
-app.route('/api/:tribeId/history')
-    .get(history.list)
-    .post(history.savePairs);
-app.delete('/api/:tribeId/history/:id', history.deleteMember);
+    var checkApiAccess = function (request, response, next) {
+        if (config.requiresAuthentication && !request.isAuthenticated())
+            response.send(401);
+        else
+            next();
+    };
 
-var players = new PlayerRoutes(config.mongoUrl);
-app.route('/api/:tribeId/players')
-    .get(players.listTribeMembers)
-    .post(players.savePlayer);
-app.delete('/api/:tribeId/players/:playerId', players.removePlayer);
+    var tribes = new TribeRoutes(config.mongoUrl);
+    app.get('/', routes.index);
+    app.all('/api/*', checkApiAccess);
+    app.route('/api/tribes')
+        .get(tribes.list)
+        .post(tribes.save);
+    app.post('/api/:tribeId/spin', spin(config.mongoUrl));
 
-app.get('/partials/:name', routes.partials);
-app.get('*', routes.index);
+    var history = new HistoryRoutes(config.mongoUrl);
+    app.route('/api/:tribeId/history')
+        .get(history.list)
+        .post(history.savePairs);
+    app.delete('/api/:tribeId/history/:id', history.deleteMember);
 
-http.createServer(app).listen(app.get('port'), function () {
-    console.log('Express server listening on port ' + app.get('port'));
-    console.log('Deployed at: ' + config.buildDate);
-    console.log('Git revision: ' + config.gitRev);
+    var players = new PlayerRoutes(config.mongoUrl);
+    app.route('/api/:tribeId/players')
+        .get(players.listTribeMembers)
+        .post(players.savePlayer);
+    app.delete('/api/:tribeId/players/:playerId', players.removePlayer);
+
+    app.get('/partials/:name', routes.partials);
+    app.get('*', routes.index);
+
+    http.createServer(app).listen(app.get('port'), function () {
+        console.log('Express server listening on port ' + app.get('port'));
+        console.log('Deployed at: ' + config.buildDate);
+        console.log('Git revision: ' + config.gitRev);
+    });
 });
