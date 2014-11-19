1662ceca42aa7c700671fccff806c56b91e7aa64
robertfmurdock <rmurdock@pillartechnology.com>
2014-11-18 20:01:14 -0500

    Added a protractor test verifying that the enter button is always accessible on the welcome page.

---
+++ test/e2e/welcome-e2e-spec.js
@@ -0,0 +1,16 @@
+"use strict";
+var monk = require("monk");
+var _ = require('underscore');
+var config = require("../../config");
+
+var hostName = 'http://localhost:' + config.port;
+var database = monk(config.tempMongoUrl);
+
+describe('The welcome page', function () {
+
+    iit('will have a clickable enter button', function () {
+        browser.get(hostName + '/welcome');
+        element(By.id('enter-button')).click();
+        expect(protractor.getInstance().getCurrentUrl()).toBe(hostName + '/auth/google');
+    });
+});
