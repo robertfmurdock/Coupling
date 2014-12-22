9b4ba912b2320292b26c082dc04a602a4463609a
robertfmurdock <rmurdock@pillartechnology.com>
2014-12-22 15:01:50 -0500

    Going to roll on protractor tests one at a time until we're good.

--- test/e2e/welcome-e2e-spec.js
+++ test/e2e/welcome-e2e-spec.js
@@ -6,7 +6,7 @@ var config = require("../../config");
 var hostName = 'http://localhost:' + config.port;
 var database = monk(config.tempMongoUrl);
 
-describe('The welcome page', function () {
+ddescribe('The welcome page', function () {
 
     it('will have a clickable enter button', function () {
         browser.ignoreSynchronization = false;
