624b379a50ce2e2bbb9b697a6e6c78e233ed34ca
robertfmurdock <rmurdock@pillartechnology.com>
2015-04-28 15:19:05 -0400

    Updating protractor to use jasmine 2. This should allow for beforeAll calls.

--- test/e2e/protractor-conf.js
+++ test/e2e/protractor-conf.js
@@ -14,19 +14,21 @@ exports.config = {
     specs: ['*-spec.js'],
 
     // Options to be passed to Jasmine-node.
+    framework: 'jasmine2',
+
     jasmineNodeOpts: {
         showColors: true,
         defaultTimeoutInterval: 30000
     },
-    onPrepare: function () {
+    onPrepare: function() {
 
         jasmine.getEnv().addReporter(new ScreenShotReporter({
             baseDirectory: '/tmp/screenshots',
             takeScreenShotsOnlyForFailedSpecs: true
         }));
 
-        var disableNgAnimate = function () {
-            angular.module('disableNgAnimate', []).run(['$animate', function ($animate) {
+        var disableNgAnimate = function() {
+            angular.module('disableNgAnimate', []).run(['$animate', function($animate) {
                 $animate.enabled(false);
             }]);
         };
