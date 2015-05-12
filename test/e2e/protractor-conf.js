e97c0607e5bc0b95f178b131ddacb7e152f049cd
robertfmurdock <rmurdock@pillartechnology.com>
2015-05-12 09:52:48 -0400

    Trying to add discovering the test results to circle.

--- test/e2e/protractor-conf.js
+++ test/e2e/protractor-conf.js
@@ -3,36 +3,45 @@ var ScreenShotReporter = require('protractor-jasmine2-screenshot-reporter');
 
 exports.config = {
 
-    allScriptsTimeout: 11000,
+  allScriptsTimeout: 11000,
 
-    capabilities: {
-        'browserName': 'chrome',
-    },
+  capabilities: {
+    'browserName': 'chrome',
+  },
 
-    // Spec patterns are relative to the current working directly when
-    // protractor is called.
-    specs: ['*-spec.js'],
+  // Spec patterns are relative to the current working directly when
+  // protractor is called.
+  specs: ['*-spec.js'],
 
-    // Options to be passed to Jasmine-node.
-    framework: 'jasmine2',
+  // Options to be passed to Jasmine-node.
+  framework: 'jasmine2',
 
-    jasmineNodeOpts: {
-        showColors: true,
-        defaultTimeoutInterval: 30000
-    },
-    onPrepare: function() {
+  jasmineNodeOpts: {
+    showColors: true,
+    defaultTimeoutInterval: 30000
+  },
+  onPrepare: function () {
 
-        jasmine.getEnv().addReporter(new ScreenShotReporter({
-            dest: '/tmp/screenshots',
-            captureOnlyFailedSpecs: true
-        }));
+    jasmine.getEnv().addReporter(new ScreenShotReporter({
+      dest: '/tmp/screenshots',
+      captureOnlyFailedSpecs: true
+    }));
 
-        var disableNgAnimate = function() {
-            angular.module('disableNgAnimate', []).run(['$animate', function($animate) {
-                $animate.enabled(false);
-            }]);
-        };
+    var jasmineReporters = require('jasmine-reporters');
+    jasmine.getEnv().addReporter(
+      new jasmineReporters.JUnitXmlReporter({
+        consolidateAll: true,
+        filePrefix: 'e2e',
+        savePath: 'test-output'
+      })
+    );
 
-        browser.addMockModule('disableNgAnimate', disableNgAnimate);
-    }
+    var disableNgAnimate = function () {
+      angular.module('disableNgAnimate', []).run(['$animate', function ($animate) {
+        $animate.enabled(false);
+      }]);
+    };
+
+    browser.addMockModule('disableNgAnimate', disableNgAnimate);
+  }
 };
