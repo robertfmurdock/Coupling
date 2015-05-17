96fff9c4a47d43334f6f08b925038b9f34720393
robertmurdock <rmurdock@pillartechnology.com>
2015-05-17 10:54:49 -0400

    Still trying to figure out why that test doesn't work in circle.

--- test/e2e/pair-assignments-e2e-spec.js
+++ test/e2e/pair-assignments-e2e-spec.js
@@ -8,23 +8,23 @@ var e2eHelp = require('./e2e-help');
 var database = monk(config.tempMongoUrl);
 var tribeCollection = database.get('tribes');
 
-describe('The current pair assignments', function() {
+describe('The current pair assignments', function () {
 
   var tribe = {
     _id: 'delete_me',
     name: 'Funkytown'
   };
 
-  beforeAll(function(done) {
-    RSVP.all([
-      tribeCollection.insert(tribe),
-      e2eHelp.authorizeUserForTribes([tribe._id])
-    ]).then(function() {
+  beforeAll(function (done) {
+    tribeCollection.insert(tribe).then(function () {
+      return e2eHelp.authorizeUserForTribes([tribe._id])
+    }).then(function () {
+      browser.get(hostName + '/test-login?username=' + e2eHelp.userEmail + '&password="pw"');
       done();
     });
   });
 
-  afterAll(function() {
+  afterAll(function () {
     tribeCollection.remove({
       _id: tribe._id
     }, false);
@@ -32,8 +32,7 @@ describe('The current pair assignments', function() {
 
   e2eHelp.afterEachAssertLogsAreEmpty();
 
-  it('shows the tribe', function() {
-    browser.get(hostName + '/test-login?username=' + e2eHelp.userEmail + '&password="pw"');
+  it('shows the tribe', function () {
     browser.waitForAngular();
 
     browser.setLocation('/' + tribe._id + '/pairAssignments/current/');
