ff500f17914588de7548a9ceb7b500ce43248f64
Rob Murdock <rmurdock@pillartechnology.com>
2017-11-10 14:43:18 -0500

    Rob, making the continous runner automatically rebuild the client module.

--- test/unit/client/continuous-run.js
+++ test/unit/client/continuous-run.js
@@ -4,3 +4,10 @@ const Server = require('karma').Server;
 const server = new Server({port: 9876, configFile: __dirname + '/karma.conf.js'});
 
 server.start();
+
+const webpackRunner = require('../../webpackRunner');
+const productionWebpackConfig = require('../../../client/webpack.config');
+
+webpackRunner.watch(productionWebpackConfig, () => {
+  console.log('Rebuild client module.');
+});
