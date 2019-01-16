af47209a1bd4a5e454f214d27f16a6d270804a1a
Robert Murdock <rmurdock@pillartechnology.com>
2019-01-16 08:39:59 -0600

    Applying the new test organization style to another tes.t

--- engine/src/commonTest/kotlin/SpinCommandTest.kt
+++ engine/src/commonTest/kotlin/SpinCommandTest.kt
@@ -3,77 +3,72 @@ import kotlin.test.assertEquals
 
 class SpinCommandTest {
 
-    class WithNoPlayers : SpinCommandDispatcher {
-        override val actionDispatcher: GetNextPairActionDispatcher = StubGetNextPairActionDispatcher()
-        override val wheel: Wheel = object : Wheel {}
+    @Test
+    fun withNoPlayersShouldReturnNoPairs() = setup(object : SpinCommandDispatcher, Wheel {
+        override val actionDispatcher = StubGetNextPairActionDispatcher()
+        override val wheel = this
+    }) exercise {
+        SpinCommand(Game(listOf(), listOf(), PairingRule.LongestTime))
+                .perform()
+    } verify { assertEquals(it, listOf()) }
 
-        @Test
-        fun withNoPlayersShouldReturnNoPairs() {
-            val result = SpinCommand(Game(listOf(), listOf(), PairingRule.LongestTime))
-                    .perform()
-            assertEquals(result, listOf())
-        }
-    }
 
-    class WithTwoPlayersTest : SpinCommandDispatcher {
-
-        private val bill: Player = KtPlayer(_id = "Bill")
-        private val ted: Player = KtPlayer(_id = "Ted")
-        private val players = listOf(bill, ted)
-
+    @Test
+    fun withTwoPlayersEachShouldBeRemovedFromWheelBeforeEachPlay() = setup(object : SpinCommandDispatcher {
         override val actionDispatcher = StubGetNextPairActionDispatcher()
         override val wheel = StubWheel()
+        val bill: Player = KtPlayer(_id = "Bill")
+        val ted: Player = KtPlayer(_id = "Ted")
+        val players = listOf(bill, ted)
 
-        @Test
-        fun playersShouldBeRemovedFromWheelBeforeEachPlay() {
+        init {
             wheel.spyReturnValues.add(bill)
             actionDispatcher.spyReturnValues.add(PairCandidateReport(ted, listOf(bill), TimeResultValue(0)))
-
-            SpinCommand(Game(listOf(), players, PairingRule.LongestTime))
-                    .perform()
-                    .assertIsEqualTo(listOf(CouplingPair.Double(ted, bill)))
-
-            actionDispatcher.spyReceivedValues.getOrNull(0)
-                    .assertIsEqualTo(GetNextPairAction(GameSpin(listOf(), players, PairingRule.LongestTime)))
-
-            wheel.spyReceivedValues.assertContains(listOf(bill))
         }
+    }) exercise {
+        SpinCommand(Game(listOf(), players, PairingRule.LongestTime))
+                .perform()
+    } verify { result ->
+        result.assertIsEqualTo(listOf(CouplingPair.Double(ted, bill)))
+        actionDispatcher.spyReceivedValues.getOrNull(0)
+                .assertIsEqualTo(GetNextPairAction(GameSpin(listOf(), players, PairingRule.LongestTime)))
+        wheel.spyReceivedValues.assertContains(listOf(bill))
     }
 
-    class WithThreePlayersInSingledOutMode : SpinCommandDispatcher {
-
-        private val bill: Player = KtPlayer(_id = "Bill")
-        private val ted: Player = KtPlayer(_id = "Ted")
-        private val mozart: Player = KtPlayer(_id = "Mozart")
-        private val players = listOf(bill, ted, mozart)
-
+    @Test
+    fun shouldRemoveAPlayerFromTheWheelBeforeEachPlay() = setup(object : SpinCommandDispatcher {
         override val actionDispatcher = StubGetNextPairActionDispatcher()
         override val wheel = StubWheel()
+        val bill: Player = KtPlayer(_id = "Bill")
+        val ted: Player = KtPlayer(_id = "Ted")
+        val mozart: Player = KtPlayer(_id = "Mozart")
+        val players = listOf(bill, ted, mozart)
 
-        @Test
-        fun shouldRemoveAPlayerFromTheWheelBeforeEachPlay() {
-            actionDispatcher spyWillReturn listOf(
-                    PairCandidateReport(mozart, listOf(bill, ted), TimeResultValue(0)),
-                    PairCandidateReport(ted, emptyList(), TimeResultValue(0))
-            )
+        val pairCandidateReports = listOf(
+                PairCandidateReport(mozart, listOf(bill, ted), TimeResultValue(0)),
+                PairCandidateReport(ted, emptyList(), TimeResultValue(0))
+        )
+
+        init {
+            actionDispatcher spyWillReturn pairCandidateReports
             wheel spyWillReturn bill
-
-            SpinCommand(Game(listOf(), players, PairingRule.LongestTime))
-                    .perform()
-                    .assertIsEqualTo(
-                            listOf(CouplingPair.Double(mozart, bill), CouplingPair.Single(ted))
-                    )
-
-            actionDispatcher.spyReceivedValues
-                    .assertIsEqualTo(listOf(
-                            GetNextPairAction(GameSpin(listOf(), players, PairingRule.LongestTime)),
-                            GetNextPairAction(GameSpin(listOf(), listOf(ted), PairingRule.LongestTime))
-                    ))
-
-            wheel.spyReceivedValues
-                    .assertContains(listOf(bill, ted))
         }
+    }) exercise {
+        SpinCommand(Game(listOf(), players, PairingRule.LongestTime))
+                .perform()
+    } verify { result ->
+        result.assertIsEqualTo(
+                listOf(CouplingPair.Double(mozart, bill), CouplingPair.Single(ted))
+        )
+        actionDispatcher.spyReceivedValues
+                .assertIsEqualTo(listOf(
+                        GetNextPairAction(GameSpin(listOf(), players, PairingRule.LongestTime)),
+                        GetNextPairAction(GameSpin(listOf(), listOf(ted), PairingRule.LongestTime))
+                ))
+        wheel.spyReceivedValues
+                .assertContains(listOf(bill, ted))
     }
+
 }
 
 class StubWheel : Wheel, Spy<List<Player>, Player> by SpyData() {
