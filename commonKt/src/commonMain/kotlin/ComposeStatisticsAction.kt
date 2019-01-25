0f55ab8aa809fc9fb84a8c40f49e911a14416e6f
Robert Murdock <rmurdock@pillartechnology.com>
2019-01-24 23:03:38 -0600

    Refactoring.

--- commonKt/src/commonMain/kotlin/ComposeStatisticsAction.kt
+++ commonKt/src/commonMain/kotlin/ComposeStatisticsAction.kt
@@ -4,9 +4,21 @@ interface ComposeStatisticsActionDispatcher {
 
     fun ComposeStatisticsAction.perform() = StatisticsReport(
             spinsUntilFullRotation = calculateFullRotation(),
-            pairReports = emptyList(),
+            pairReports = pairReports(),
             medianSpinDuration = 0
     )
+
+    private fun ComposeStatisticsAction.pairReports() = allPairCombinations()
+            .map { PairReport(it, NeverPaired) }
+
+    private fun ComposeStatisticsAction.allPairCombinations() =
+            players.mapIndexed { index, player -> players.sliceFrom(index + 1).toPairsWith(player) }
+                    .flatten()
+
+    private fun List<Player>.sliceFrom(startIndex: Int) = slice(startIndex..lastIndex)
+
+    private fun List<Player>.toPairsWith(player: Player) =
+            map { otherPlayer -> CouplingPair.Double(player, otherPlayer) }
 
     private fun ComposeStatisticsAction.calculateFullRotation() = players.size.ifEvenSubtractOne()
 
@@ -20,6 +32,8 @@ interface ComposeStatisticsActionDispatcher {
 
 data class StatisticsReport(
         val spinsUntilFullRotation: Int,
-        val pairReports: List<Any>,
+        val pairReports: List<PairReport>,
         val medianSpinDuration: Int
 )
+
+data class PairReport(val pair: CouplingPair, val timeSinceLastPair: TimeResult)
--- commonKt/src/commonTest/kotlin/ComposeStatisticsActionTest.kt
+++ commonKt/src/commonTest/kotlin/ComposeStatisticsActionTest.kt
@@ -5,17 +5,27 @@ import kotlin.test.Test
 class ComposeStatisticsActionTest {
 
     companion object : ComposeStatisticsActionDispatcher {
+        val tribe = KtTribe("LOL", PairingRule.LongestTime)
 
         fun makePlayers(tribe: KtTribe, numberOfPlayers: Int) = (1..numberOfPlayers)
                 .map { number -> makePlayer(tribe, "$number") }
 
         fun makePlayer(tribe: KtTribe, id: String) = Player(_id = id, tribe = tribe.id)
+
+        private fun List<CouplingPair>.checkPairs(expected: List<CouplingPair>) {
+            assertIsEqualTo(
+                    expected,
+                    "------WE EXPECT\n${expected.describe()}\n------RESULTS\n${this.describe()}\n-----END\n"
+            )
+        }
+
+        private fun List<CouplingPair>.describe() = map { it.asArray().map { player -> player._id } }
+                .joinToString(", ").let { "[ $it ]" }
     }
 
     class WillIncludeTheFullRotationNumber {
 
         companion object {
-            val tribe = KtTribe("LOL", PairingRule.LongestTime)
             val history = emptyList<HistoryDocument>()
 
             fun composeStatisticsAction(players: List<Player>) =
@@ -83,5 +93,73 @@ class ComposeStatisticsActionTest {
         }
     }
 
+    class WillGeneratePairReports {
 
+        class WithNoHistory {
+            companion object {
+                val history = emptyList<HistoryDocument>()
+            }
+
+            @Test
+            fun withNoPlayersNoPairReportsWillBeCreated() = setup(object {
+                val players = makePlayers(tribe, 0)
+            }) exercise {
+                ComposeStatisticsAction(tribe, players, history)
+                        .perform()
+            } verify { (_, pairReports) ->
+                pairReports.assertIsEqualTo(emptyList())
+            }
+
+            @Test
+            fun withOnePlayersNoPairReportsWillBeCreated() = setup(object {
+                val players = makePlayers(tribe, 1)
+            }) exercise {
+                ComposeStatisticsAction(tribe, players, history)
+                        .perform()
+            } verify { (_, pairReports) ->
+                pairReports.assertIsEqualTo(emptyList())
+            }
+
+            @Test
+            fun withTwoPlayersOnePairReportWillBeCreated() = setup(object {
+                val players = makePlayers(tribe, 2)
+            }) exercise {
+                ComposeStatisticsAction(tribe, players, history)
+                        .perform()
+            } verify { (_, pairReports) ->
+                pairReports.assertIsEqualTo(
+                        listOf(
+                                PairReport(CouplingPair.Double(players[0], players[1]), NeverPaired)
+                        )
+                )
+            }
+
+            @Test
+            fun withFivePlayersOnePairReportWillBeCreated() = setup(object {
+                val players = makePlayers(tribe, 5)
+            }) exercise {
+                ComposeStatisticsAction(tribe, players, history)
+                        .perform()
+            } verify { (_, pairReports) ->
+                val (player1, player2, player3, player4, player5) = players
+                val expected = listOf(
+                        CouplingPair.Double(player1, player2),
+                        CouplingPair.Double(player1, player3),
+                        CouplingPair.Double(player1, player4),
+                        CouplingPair.Double(player1, player5),
+
+                        CouplingPair.Double(player2, player3),
+                        CouplingPair.Double(player2, player4),
+                        CouplingPair.Double(player2, player5),
+                        CouplingPair.Double(player3, player4),
+                        CouplingPair.Double(player3, player5),
+                        CouplingPair.Double(player4, player5)
+                )
+                pairReports.map { it.pair }
+                        .checkPairs(expected)
+            }
+        }
+
+
+    }
 }
