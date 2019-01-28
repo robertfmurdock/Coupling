c8f259ab9d87abf3555d078d5cfafd68acfabc64
Robert Murdock <rmurdock@pillartechnology.com>
2019-01-28 12:30:30 -0600

    Adding some configurability to the active directory auth stuff.

--- server/config/azure-ad-config.js
+++ server/config/azure-ad-config.js
@@ -1,11 +1,11 @@
 export default {
   "identityMetadata": "https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration",
-  "clientID": "127e78ae-6b6a-4213-a83f-a644e2d1bb84",
+  "clientID": process.env.AZURE_AD_CLIENT_ID || "127e78ae-6b6a-4213-a83f-a644e2d1bb84",
+  "clientSecret": process.env.AZURE_AD_CLIENT_SECRET || "bhyrMCFDF9485=aoiJD1*%{",
   "responseType": "code id_token",
   "responseMode": "form_post",
   "redirectUrl": "http://localhost:3000/auth/signin-microsoft",
   "allowHttpForRedirectUrl": true,
-  "clientSecret": "bhyrMCFDF9485=aoiJD1*%{",
   "validateIssuer": false,
   "issuer": null,
   "passReqToCallback": false,
