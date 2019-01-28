1cc51546976cb8dc2734ed7fd1ab15f4ebe265c4
Robert Murdock <rmurdock@pillartechnology.com>
2019-01-28 15:34:38 -0600

    Read more about issuer, realized i was doing it wrong, fixed it.

--- server/config/azure-ad-config.js
+++ server/config/azure-ad-config.js
@@ -1,13 +1,15 @@
+let clientID = process.env.AZURE_AD_CLIENT_ID || "127e78ae-6b6a-4213-a83f-a644e2d1bb84";
+let clientSecret = process.env.AZURE_AD_CLIENT_SECRET || "bhyrMCFDF9485=aoiJD1*%{";
 export default {
   "identityMetadata": "https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration",
-  "clientID": process.env.AZURE_AD_CLIENT_ID || "127e78ae-6b6a-4213-a83f-a644e2d1bb84",
-  "clientSecret": process.env.AZURE_AD_CLIENT_SECRET || "bhyrMCFDF9485=aoiJD1*%{",
+  "clientID": clientID,
+  "clientSecret": clientSecret,
   "responseType": "code id_token",
   "responseMode": "form_post",
   "redirectUrl": (process.env.PUBLIC_URL || "http://localhost:3000") + "/auth/signin-microsoft",
   "allowHttpForRedirectUrl": true,
-  "validateIssuer": process.env.PUBLIC_URL !== undefined,
-  "issuer": null,
+  "validateIssuer": true,
+  "issuer": clientID,
   "passReqToCallback": false,
   "useCookieInsteadOfSession": true,
   "cookieEncryptionKeys": [
