d3f3704c9eb1163d8e5096716789ed6eaf5f2a02
Robert Murdock <rmurdock@pillartechnology.com>
2019-02-12 09:56:02 -0500

    Refactoring for fluency

--- engine/src/commonMain/kotlin/com/zegreatrob/coupling/entity/tribe/TribeQuery.kt
+++ engine/src/commonMain/kotlin/com/zegreatrob/coupling/entity/tribe/TribeQuery.kt
@@ -1,6 +1,7 @@
 package com.zegreatrob.coupling.entity.tribe
 
 import com.zegreatrob.coupling.UserContextSyntax
+import com.zegreatrob.coupling.common.entity.player.TribeIdPlayer
 import com.zegreatrob.coupling.common.entity.tribe.KtTribe
 import com.zegreatrob.coupling.common.entity.tribe.TribeId
 import com.zegreatrob.coupling.entity.player.PlayerRepository
@@ -12,12 +13,24 @@ interface TribeQueryDispatcher : UserContextSyntax {
     val tribeRepository: TribeRepository
     val playerRepository: PlayerRepository
 
-    suspend fun TribeQuery.perform(): List<KtTribe> {
-        val (tD, pD) = tribeRepository.getTribesAsync() to playerRepository.getPlayersByEmailAsync(userEmail())
-        val tribes = tD.await()
-        val players = pD.await()
-        val authenticatedTribeIds = players.map { it.tribeId } + userContext.tribeIds.map(::TribeId)
-        return tribes.filter { authenticatedTribeIds.contains(it.id) }
+    suspend fun TribeQuery.perform() = getTribesAndPlayers().onlyAuthenticatedTribes()
+
+    private suspend fun getTribesAndPlayers() = getTribesAndPlayersDeferred()
+            .let { (tribeDeferred, playerDeferred) ->
+                Pair(tribeDeferred.await(), playerDeferred.await())
+            }
+
+    private fun getTribesAndPlayersDeferred() =
+            tribeRepository.getTribesAsync() to playerRepository.getPlayersByEmailAsync(userEmail())
+
+    private fun Pair<List<KtTribe>, List<TribeIdPlayer>>.onlyAuthenticatedTribes() = let { (tribes, players) ->
+        tribes.filter(players.isAuthenticatedFilter())
     }
+
+    private fun List<TribeIdPlayer>.isAuthenticatedFilter() = authenticatedTribeIds().isAuthenticatedFilter()
+
+    private fun List<TribeIdPlayer>.authenticatedTribeIds() = map { it.tribeId } + userContext.tribeIds.map(::TribeId)
+
+    private fun List<TribeId>.isAuthenticatedFilter(): (KtTribe) -> Boolean = { contains(it.id) }
 
 }
