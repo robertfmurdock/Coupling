5b2fad116c3c7ba8d50853e121fbb7872fb2c1fc
Robert Murdock <rmurdock@pillartechnology.com>
2019-02-14 16:27:08 -0500

    Log tweaking.

--- client/src/test/kotlin/TestAdapter.kt
+++ client/src/test/kotlin/TestAdapter.kt
@@ -1,3 +1,4 @@
+import com.soywiz.klock.DateTime
 import mu.KotlinLogging
 import mu.KotlinLoggingLevel
 import mu.LOG_LEVEL
@@ -9,4 +10,33 @@ private val logger = KotlinLogging.logger("TestAdapter")
 fun setLogLevel() {
     LOG_LEVEL = KotlinLoggingLevel.DEBUG
     logger.warn { "Setting log level to debug." }
+
+    @Suppress("UNUSED_VARIABLE")
+    val reporter = JsonLoggingReporter()
+
+    js("jasmine.getEnv().addReporter(reporter)")
 }
+
+class JsonLoggingReporter {
+    private val logger = KotlinLogging.logger {}
+    private var lastStart: DateTime? = null
+
+    @Suppress("unused")
+    @JsName("specStarted")
+    fun specStarted(result: dynamic) = startTest(result.fullName.unsafeCast<String>())
+
+    @Suppress("unused")
+    @JsName("specDone")
+    fun specDone(result: dynamic) = endTest(result.fullName.unsafeCast<String>())
+
+    private fun startTest(testName: String) = logger.info { mapOf("type" to "TestStart", "test" to testName) }
+            .also { lastStart = DateTime.now() }
+
+    private fun endTest(testName: String) {
+        val duration = lastStart?.let { DateTime.now() - it }
+        logger.info { mapOf("type" to "TestEnd", "test" to testName, "duration" to "$duration") }
+                .also { lastStart = null }
+    }
+}
+
+
