ec6eaa6d5f427606a89b71b55d6deb6de79f3cad
Robert Murdock <robert.f.murdock@accenture.com>
2019-05-22 08:34:18 -0500

    Deleted unused pug and created react tribe browser file... but no content in there yet.

---
+++ client/app/components/tribebrowser/ReactTribeBrowser.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+
+export default class ReactTribeBrowser extends React.Component {
+
+    render() {
+        return <div></div>
+    }
+}
--- client/app/components/tribebrowser/TribeBrowserDirective.ts
+++ client/app/components/tribebrowser/TribeBrowserDirective.ts
@@ -2,6 +2,8 @@ import {module} from "angular";
 import * as template from "./template.pug";
 import * as styles from "./styles.css";
 
+import ReactTribeBrowser from './ReactTribeBrowser'
+
 export default module('coupling.tribebrowser', [])
     .directive('tribebrowser', function () {
         return {
