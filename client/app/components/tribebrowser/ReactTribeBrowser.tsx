cdc8be930c777f97dfff7ac044a3e269e535424f
Robert Murdock <robert.f.murdock@accenture.com>
2019-05-22 18:21:09 -0500

    Changing the tribe browser to a react component.

--- client/app/components/tribebrowser/ReactTribeBrowser.tsx
+++ client/app/components/tribebrowser/ReactTribeBrowser.tsx
@@ -1,8 +1,36 @@
 import * as React from "react";
+import * as Styles from './styles.css'
+import * as classNames from 'classnames'
+import ReactTribeCard from "../tribe-card/ReactTribeCard";
+import {Tribe} from "../../../../common";
 
-export default class ReactTribeBrowser extends React.Component {
+interface Props {
+    tribe: Tribe,
+    pathSetter: (string) => void
+}
+
+export default class ReactTribeBrowser extends React.Component<Props> {
 
     render() {
-        return <div></div>
+        const {pathSetter, tribe} = this.props;
+
+        return <div className={Styles.className}>
+            <span>
+                <ReactTribeCard pathSetter={pathSetter} tribe={tribe} size={50}/>
+                <h1>{tribe.name}</h1>
+            </span>
+            <span>
+                <span>
+                    <a className={classNames(Styles.statisticsButton, 'statistics-button', 'large gray button')}
+                       href={`/${tribe.id}/statistics`}>
+                        <text className="icon-button-text">Statistics</text>
+                    </a>
+                    <a id="tribe-select-button" className="large gray button" href="/tribes">
+                        <i className="fa fa-arrow-circle-up"/>
+                        <text className="icon-button-text">Sign Out</text>
+                    </a>
+                </span>
+            </span>
+        </div>
     }
 }
--- client/app/components/tribebrowser/TribeBrowserDirective.ts
+++ client/app/components/tribebrowser/TribeBrowserDirective.ts
@@ -1,20 +1,29 @@
 import {module} from "angular";
-import * as template from "./template.pug";
 import * as styles from "./styles.css";
-
+import {connectReactToNg} from "../ReactNgAdapter";
 import ReactTribeBrowser from './ReactTribeBrowser'
 
 export default module('coupling.tribebrowser', [])
     .directive('tribebrowser', function () {
         return {
             restrict: 'E',
-            controller: function () {
+            controller: ['$element', '$scope', '$location', function ($element, $scope, $location) {
                 this.styles = styles;
-            },
+                connectReactToNg({
+                    component: ReactTribeBrowser,
+                    props: () => ({
+                        tribe: this.tribe
+                    }),
+                    domNode: $element[0],
+                    $scope: $scope,
+                    watchExpression: "tribe",
+                    $location: $location
+                });
+            }],
             controllerAs: 'tribebrowser',
             bindToController: {
                 tribe: '=',
             },
-            template: template,
+            template: "<div/>",
         }
     });
