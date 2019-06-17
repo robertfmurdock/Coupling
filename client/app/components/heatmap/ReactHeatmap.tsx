dc46e07206036a5e29cf1fe9ec210f3d227c52cb
Robert Murdock <robert.f.murdock@accenture.com>
2019-06-17 13:09:09 -0500

    Converting the heatmap into a react component

---
+++ client/app/components/heatmap/ReactHeatmap.tsx
@@ -0,0 +1,56 @@
+import * as React from "react";
+import {useLayoutEffect, useRef} from "react";
+import * as Styles from './styles.css'
+
+import flatten from "ramda/es/flatten";
+import {select} from "d3-selection";
+import {rgb} from "d3-color";
+import {interpolateRgbBasis} from "d3-interpolate";
+
+const colorSuggestions = [
+    rgb("#2c7bb6"),
+    rgb("#00a6ca"),
+    rgb("#00ccbc"),
+    rgb("#90eb9d"),
+    rgb("#ffff8c"),
+    rgb("#f9d057"),
+    rgb("#f29e2e"),
+    rgb("#e76818"),
+    rgb("#d7191c")
+];
+
+const colorInterpolator = interpolateRgbBasis(colorSuggestions);
+
+function renderD3Heatmap(element, data) {
+    select(element)
+        .selectAll("div")
+        .data(data)
+        .enter().append(function () {
+        const cellElement = document.createElement('div');
+        cellElement.setAttribute('class', Styles.cell);
+        return cellElement;
+    })
+        .style("background-color", function (dataNumber: number) {
+            if (dataNumber === null) {
+                return '#EEE'
+            }
+            const percentage = dataNumber / 10;
+            return colorInterpolator(percentage);
+        });
+}
+
+export default function ReactHeatmap(props) {
+    const {data} = props;
+
+    const rowSize = data.length * 90;
+    const heatmapStyle = {
+        width: `${rowSize}px`,
+        height: `${rowSize}px`
+    };
+
+    const ref = useRef(null);
+
+    useLayoutEffect(() => renderD3Heatmap(ref.current, flatten(data)));
+
+    return <div ref={ref} className={Styles.heatmap} style={heatmapStyle}/>
+}
--- client/app/components/heatmap/heatmap.ts
+++ client/app/components/heatmap/heatmap.ts
@@ -1,69 +1,33 @@
 import {module, IController} from "angular";
-import {rgb} from "d3-color";
-import {interpolateRgbBasis} from "d3-interpolate";
-import {select} from "d3-selection";
-import * as Styles from "./styles.css";
-import flatten from "ramda/es/flatten";
-import * as template from "./heatmap.pug";
-
-const colorSuggestions = [
-    rgb("#2c7bb6"),
-    rgb("#00a6ca"),
-    rgb("#00ccbc"),
-    rgb("#90eb9d"),
-    rgb("#ffff8c"),
-    rgb("#f9d057"),
-    rgb("#f29e2e"),
-    rgb("#e76818"),
-    rgb("#d7191c")
-];
-
-const colorInterpolator = interpolateRgbBasis(colorSuggestions);
+import {connectReactToNg} from "../ReactNgAdapter";
+import ReactHeatmap from "./ReactHeatmap";
 
 class HeatmapController implements IController {
 
-    public data: any;
-    public heatmapStyle: any;
-    public Styles: any;
+    static $inject = ['$element', '$scope'];
 
-    $onInit() {
-        const rowSize = this.data.length * 90;
-        this.Styles = Styles;
-        this.heatmapStyle = {
-            width: `${rowSize}px`,
-            height: `${rowSize}px`
-        }
+    public data: any;
+
+    constructor($element, $scope) {
+        connectReactToNg({
+            component: ReactHeatmap,
+            props: () => ({data: this.data}),
+            domNode: $element[0],
+            $scope: $scope,
+            watchExpression: "data"
+        });
     }
 }
 
 export default module('coupling.heatmap', [])
     .directive('heatmap', function () {
         return {
-            template: template,
+            template: '<div/>',
             controller: HeatmapController,
             controllerAs: 'me',
             bindToController: true,
             scope: {
                 data: '=',
             },
-            link: function (scope, element, attributes, controller: any) {
-                const data = flatten(controller.data);
-
-                select(element[0].querySelector('div[ng-class="me.Styles.heatmap"]'))
-                    .selectAll("div")
-                    .data(data)
-                    .enter().append(function () {
-                    const cellElement = document.createElement('div');
-                    cellElement.setAttribute('class', Styles.cell);
-                    return cellElement;
-                })
-                    .style("background-color", function (dataNumber: number) {
-                        if (dataNumber === null) {
-                            return '#EEE'
-                        }
-                        const percentage = dataNumber / 10;
-                        return colorInterpolator(percentage);
-                    });
-            }
         }
     });
