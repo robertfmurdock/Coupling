6f0b5288d15646b7bbdcd7a0c1247030065ac722
Robert Murdock <robert.f.murdock@accenture.com>
2019-06-22 19:18:07 -0500

    Removing other unused angular directives!

--- client/app/components/components.ts
+++ client/app/components/components.ts
@@ -1,11 +1,8 @@
 import History from "./history/history";
 import PairAssignments from "./pair-assignments/PairAssignmentsDirective";
-import HeatMap from './heatmap/heatmap'
 import PinList from "./pin-list/pin-list";
 import PlayerConfig from "./player-config/player-config";
-import PlayerRoster from "./player-roster/player-roster";
 import Prepare from "./prepare/PrepareDirective";
-import ServerMessageDirective from "./server-message/ServerMessageDirective";
 import TribeCard from "./tribe-card/TribeCardController";
 import TribeConfig from "./tribe-config/tribe-config";
 import TribeList from "./tribe-list/tribe-list";
@@ -13,7 +10,6 @@ import Welcome from "./welcome/WelcomeDirective";
 import TribeBrowserDirective from "./tribebrowser/TribeBrowserDirective";
 import Statistics from "./statistics/statistics";
 import RetiredPlayers from "./retired-players/retired-players";
-import LoginChooser from './login-chooser/login-chooser'
 import {module} from "angular";
 import debounce from 'lodash.debounce'
 
@@ -24,18 +20,14 @@ module('coupling.component', [
     History.name,
     PinList.name,
     PlayerConfig.name,
-    PlayerRoster.name,
     Prepare.name,
     RetiredPlayers.name,
-    ServerMessageDirective.name,
     Statistics.name,
     TribeCard.name,
     TribeConfig.name,
     TribeList.name,
     Welcome.name,
-    HeatMap.name,
     TribeBrowserDirective.name,
-    LoginChooser.name
 ])
     .config(['fitTextConfigProvider', function (fitTextConfigProvider) {
         fitTextConfigProvider.config = {
