a1e4de7c80bdadc24489fe675eaf38f5cd56d8f2
Robert Murdock <robert.f.murdock@accenture.com>
2019-08-02 15:35:48 -0500

    Statistic page now uses the data query. Needs... a new one tbh

--- client/src/main/kotlin/com/zegreatrob/coupling/client/stats/StatisticsPage.kt
+++ client/src/main/kotlin/com/zegreatrob/coupling/client/stats/StatisticsPage.kt
@@ -1,14 +1,9 @@
 package com.zegreatrob.coupling.client.stats
 
 import com.zegreatrob.coupling.client.*
-import com.zegreatrob.coupling.common.entity.pairassignmentdocument.PairAssignmentDocument
-import com.zegreatrob.coupling.common.entity.player.Player
-import com.zegreatrob.coupling.common.entity.player.callsign.FindCallSignActionDispatcher
-import com.zegreatrob.coupling.common.entity.tribe.KtTribe
-import com.zegreatrob.coupling.common.entity.tribe.TribeId
-import kotlinx.coroutines.await
+import com.zegreatrob.coupling.client.pairassignments.TribeDataSetQuery
+import com.zegreatrob.coupling.client.pairassignments.TribeDataSetQueryDispatcher
 import react.RBuilder
-import kotlin.js.Promise
 
 
 object StatisticsPage : ComponentProvider<PageProps>(), StatisticsPageBuilder
@@ -18,33 +13,20 @@ val RBuilder.statisticsPage get() = StatisticsPage.captor(this)
 private val LoadedPairAssignments = dataLoadWrapper(TribeStatistics)
 private val RBuilder.loadedPairAssignments get() = LoadedPairAssignments.captor(this)
 
-interface StatisticsPageBuilder : ComponentBuilder<PageProps>, FindCallSignActionDispatcher {
+interface StatisticsPageBuilder : ComponentBuilder<PageProps>, TribeDataSetQueryDispatcher {
 
     override fun build() = reactFunctionComponent<PageProps> { pageProps ->
         val tribeId = pageProps.tribeId
 
         if (tribeId != null) {
-            loadedPairAssignments(DataLoadProps { pageProps.toPairAssignmentsProps(tribeId) })
+            loadedPairAssignments(
+                    dataLoadProps(
+                            query = { TribeDataSetQuery(tribeId, pageProps.coupling).perform() },
+                            toProps = { _, (tribe, players, history) ->
+                                TribeStatisticsProps(tribe, players, history, pageProps.pathSetter)
+                            }
+                    )
+            )
         } else throw Exception("WHAT")
     }
-
-    private suspend fun PageProps.toPairAssignmentsProps(tribeId: TribeId) = coupling.getData(tribeId)
-            .let { (tribe, players, history) ->
-                TribeStatisticsProps(
-                        tribe = tribe,
-                        players = players,
-                        history = history,
-                        pathSetter = pathSetter
-                )
-            }
-
-    private suspend fun Coupling.getData(tribeId: TribeId) =
-            Triple(getTribeAsync(tribeId), getPlayerListAsync(tribeId), getHistoryAsync(tribeId))
-                    .await()
-
-    private suspend fun Triple<Promise<KtTribe>, Promise<List<Player>>, Promise<List<PairAssignmentDocument>>>.await() =
-            Triple(
-                    first.await(),
-                    second.await(),
-                    third.await())
 }
