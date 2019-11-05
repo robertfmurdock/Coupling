48d1e0b80d6e4902263851664e37955645d57887
Robert Murdock <robert.f.murdock@accenture.com>
2019-11-04 22:38:49 -0600

    Reframing the test to explicitly use the full sdk... now with retired players

--- sdk/src/jsEndpointTest/kotlin/com/zegreatrob/coupling/sdk/RetiredPlayersTest.kt
+++ sdk/src/jsEndpointTest/kotlin/com/zegreatrob/coupling/sdk/RetiredPlayersTest.kt
@@ -5,7 +5,6 @@ import com.zegreatrob.coupling.model.player.Player
 import com.zegreatrob.coupling.model.player.TribeIdPlayer
 import com.zegreatrob.coupling.model.tribe.KtTribe
 import com.zegreatrob.coupling.model.tribe.TribeId
-import com.zegreatrob.coupling.sdk.external.axios.Axios
 import com.zegreatrob.minassert.assertIsEqualTo
 import com.zegreatrob.testmints.async.setupAsync
 import com.zegreatrob.testmints.async.testAsync
@@ -13,28 +12,25 @@ import kotlin.test.Test
 
 @Suppress("unused")
 class RetiredPlayersTest {
-
     class GET {
         @Test
         fun givenRetiredPlayerReturnsAllRetiredPlayers() = testAsync {
-            val hostAxios = authorizedAxios()
-            setupAsync(object : SdkPlayerSaver, SdkTribeSave, SdkPlayerGetDeleted, SdkPlayerDeleter {
-                override val axios: Axios get() = hostAxios
+            val sdk = authorizedSdk()
+            setupAsync(object {
                 val tribe = KtTribe(id = TribeId("et-${uuid4()}"))
                 val player = Player(
                     id = monk.id().toString(),
                     name = "Retiree"
                 )
             }) {
-                save(tribe)
-                save(TribeIdPlayer(tribe.id, player))
-                deletePlayer(tribe.id, player.id!!)
+                sdk.save(tribe)
+                sdk.save(TribeIdPlayer(tribe.id, player))
+                sdk.deletePlayer(tribe.id, player.id!!)
             } exerciseAsync {
-                getDeletedAsync(tribe.id).await()
+                sdk.getDeletedAsync(tribe.id).await()
             } verifyAsync { result ->
                 result.assertIsEqualTo(listOf(player))
             }
         }
     }
-
 }
