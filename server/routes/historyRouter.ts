9221f69cb911c979abb02823ad012f7a9eb36667
Robert Murdock <robert.f.murdock@accenture.com>
2019-11-12 14:26:32 -0500

    Moving the last pair assignments endopint into the kt

--- server/routes/historyRouter.ts
+++ server/routes/historyRouter.ts
@@ -10,22 +10,8 @@ class HistoryRoutes {
     );
 
     savePairs = handleRequest(
-        async (commandDispatcher, request) => {
-            const pairs = request.body;
-            if (pairs.date && pairs.pairs) {
-                pairs.date = new Date(pairs.date as string);
-                return await request.commandDispatcher.performSavePairAssignmentDocumentCommand(request.params.tribeId, pairs);
-            } else {
-                return null
-            }
-        },
-        (response, data) => {
-            if (data === null) {
-                response.statusCode = 400;
-                response.send({error: 'Pairs were not valid.'});
-            } else {
-                response.send(data);
-            }
+        async (commandDispatcher, request, response) => commandDispatcher.performSavePairAssignmentDocumentCommand(request, response),
+        () => {
         }
     );
 
--- server/src/main/kotlin/com/zegreatrob/coupling/server/entity/pairassignment/SavePairAssignmentDocumentCommandDispatcherJs.kt
+++ server/src/main/kotlin/com/zegreatrob/coupling/server/entity/pairassignment/SavePairAssignmentDocumentCommandDispatcherJs.kt
@@ -3,24 +3,30 @@ package com.zegreatrob.coupling.server.entity.pairassignment
 import com.zegreatrob.coupling.json.toJson
 import com.zegreatrob.coupling.json.toPairAssignmentDocument
 import com.zegreatrob.coupling.model.pairassignmentdocument.TribeIdPairAssignmentDocument
-import com.zegreatrob.coupling.model.tribe.TribeId
+import com.zegreatrob.coupling.server.JsonSendToResponseSyntax
 import com.zegreatrob.coupling.server.action.pairassignmentdocument.SavePairAssignmentDocumentCommand
 import com.zegreatrob.coupling.server.action.pairassignmentdocument.SavePairAssignmentDocumentCommandDispatcher
+import com.zegreatrob.coupling.server.entity.tribe.RequestTribeIdSyntax
 import com.zegreatrob.coupling.server.entity.tribe.ScopeSyntax
+import com.zegreatrob.coupling.server.external.express.Request
+import com.zegreatrob.coupling.server.external.express.Response
+import com.zegreatrob.coupling.server.external.express.jsonBody
 import kotlinx.coroutines.promise
-import kotlin.js.Json
 
-interface SavePairAssignmentDocumentCommandDispatcherJs : SavePairAssignmentDocumentCommandDispatcher, ScopeSyntax {
+interface SavePairAssignmentDocumentCommandDispatcherJs : SavePairAssignmentDocumentCommandDispatcher, ScopeSyntax,
+    RequestTribeIdSyntax, JsonSendToResponseSyntax {
+
     @JsName("performSavePairAssignmentDocumentCommand")
-    fun performSavePairAssignmentDocumentCommand(tribeId: String, pairAssignmentDocument: Json) = scope.promise {
+    fun performSavePairAssignmentDocumentCommand(request: Request, response: Response) = scope.promise {
         SavePairAssignmentDocumentCommand(
             TribeIdPairAssignmentDocument(
-                TribeId(tribeId),
-                pairAssignmentDocument.toPairAssignmentDocument()
+                request.tribeId(),
+                request.jsonBody().toPairAssignmentDocument()
             )
         )
             .perform()
             .document
             .toJson()
+            .sendTo(response)
     }
 }
