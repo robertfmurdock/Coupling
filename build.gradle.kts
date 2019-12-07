d8b92c8af01e945abc0d4f7d92b1441207af695c
Rob Murdock <robert.f.murdock@gmail.com>
2019-12-07 16:58:39 -0500

    Adding the use latest versions plugin. It doesn't seem to currently support my use case, but once it does it seems like exactly what I want.

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 plugins {
     id("com.github.node-gradle.node") apply false
     id("com.bmuschko.docker-remote-api") version "6.0.0"
+    id("se.patrikerdes.use-latest-versions") version "0.2.13"
     id("com.github.ben-manes.versions") version "0.27.0"
     id("net.rdrei.android.buildtimetracker") version "0.11.0"
 }
