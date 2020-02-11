579fe3561e696498a3b5b999536962f7cb66d2be
RoB Murdock <robert.f.murdock@gmail.com>
2020-02-11 12:09:13 -0500

    Readability change to the validator

--- repository_validation/src/jsMain/kotlin/com/zegreatrob/coupling/repositoryvalidation/TribeRepositoryValidator.kt
+++ repository_validation/src/jsMain/kotlin/com/zegreatrob/coupling/repositoryvalidation/TribeRepositoryValidator.kt
@@ -4,6 +4,7 @@ import com.zegreatrob.coupling.repository.tribe.TribeRepository
 import com.zegreatrob.minassert.assertIsEqualTo
 import com.zegreatrob.testmints.async.setupAsync
 import com.zegreatrob.testmints.async.testAsync
+import kotlinx.coroutines.CoroutineScope
 import stubTribe
 import stubTribes
 import kotlin.test.Test
@@ -12,56 +13,54 @@ abstract class TribeRepositoryValidator {
 
     abstract suspend fun withRepository(handler: suspend (TribeRepository) -> Unit)
 
+    private fun testRepository(block: suspend CoroutineScope.(TribeRepository) -> Any?) = testAsync {
+        withRepository { repository -> block(repository) }
+    }
+
     @Test
-    fun saveMultipleThenGetListWillReturnSavedTribes() = testAsync {
-        withRepository { repository ->
-            setupAsync(object {
-                val tribes = stubTribes(3)
-            }) {
-                tribes.forEach { repository.save(it) }
-            } exerciseAsync {
-                repository.getTribes()
-            } verifyAsync { result ->
-                result.takeLast(tribes.size)
-                    .assertIsEqualTo(tribes)
-            }
+    fun saveMultipleThenGetListWillReturnSavedTribes() = testRepository { repository ->
+        setupAsync(object {
+            val tribes = stubTribes(3)
+        }) {
+            tribes.forEach { repository.save(it) }
+        } exerciseAsync {
+            repository.getTribes()
+        } verifyAsync { result ->
+            result.takeLast(tribes.size)
+                .assertIsEqualTo(tribes)
         }
     }
 
     @Test
-    fun saveMultipleThenGetEachByIdWillReturnSavedTribes() = testAsync {
-        withRepository { repository ->
-            setupAsync(object {
-                val tribes = stubTribes(3)
-            }) {
-                tribes.forEach { repository.save(it) }
-            } exerciseAsync {
-                tribes.map { repository.getTribe(it.id) }
-            } verifyAsync { result ->
-                result.takeLast(tribes.size)
-                    .assertIsEqualTo(tribes)
-            }
+    fun saveMultipleThenGetEachByIdWillReturnSavedTribes() = testRepository { repository ->
+        setupAsync(object {
+            val tribes = stubTribes(3)
+        }) {
+            tribes.forEach { repository.save(it) }
+        } exerciseAsync {
+            tribes.map { repository.getTribe(it.id) }
+        } verifyAsync { result ->
+            result.takeLast(tribes.size)
+                .assertIsEqualTo(tribes)
         }
     }
 
     @Test
-    fun deleteWillMakeTribeInaccessible() = testAsync {
-        withRepository { repository ->
-            setupAsync(object {
-                val tribe = stubTribe()
-            }) {
-                repository.save(tribe)
-            } exerciseAsync {
-                repository.delete(tribe.id)
-                Pair(
-                    repository.getTribes(),
-                    repository.getTribe(tribe.id)
-                )
-            } verifyAsync { (listResult, getResult) ->
-                listResult.find { it.id == tribe.id }
-                    .assertIsEqualTo(null)
-                getResult.assertIsEqualTo(null)
-            }
+    fun deleteWillMakeTribeInaccessible() = testRepository { repository ->
+        setupAsync(object {
+            val tribe = stubTribe()
+        }) {
+            repository.save(tribe)
+        } exerciseAsync {
+            repository.delete(tribe.id)
+            Pair(
+                repository.getTribes(),
+                repository.getTribe(tribe.id)
+            )
+        } verifyAsync { (listResult, getResult) ->
+            listResult.find { it.id == tribe.id }
+                .assertIsEqualTo(null)
+            getResult.assertIsEqualTo(null)
         }
     }
 
