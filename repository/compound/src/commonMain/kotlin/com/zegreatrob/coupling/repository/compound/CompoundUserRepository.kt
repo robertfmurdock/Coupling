77fa60a6db6bd77e83e5046c1be52a1753de610b
RoB Murdock <robert.f.murdock@gmail.com>
2020-02-18 16:33:54 -0500

    Added compound tribe repository

--- repository/compound/src/commonMain/kotlin/com/zegreatrob/coupling/repository/compound/CompoundUserRepository.kt
+++ repository/compound/src/commonMain/kotlin/com/zegreatrob/coupling/repository/compound/CompoundUserRepository.kt
@@ -4,7 +4,7 @@ import com.zegreatrob.coupling.model.user.User
 import com.zegreatrob.coupling.repository.user.UserGet
 import com.zegreatrob.coupling.repository.user.UserRepository
 
-class CompoundUserRepository(val repository1: UserRepository, val repository2: UserRepository) : UserRepository,
+class CompoundUserRepository(private val repository1: UserRepository, private val repository2: UserRepository) : UserRepository,
     UserGet by repository1 {
 
     override suspend fun save(user: User) = arrayOf(repository1, repository2).forEach { it.save(user) }
---
+++ repository/compound/src/jsTest/kotlin/com/zegreatrob/coupling/repository/compound/CompoundTribeRepositoryTest.kt
@@ -0,0 +1,64 @@
+package com.zegreatrob.coupling.repository.compound
+
+import com.soywiz.klock.TimeProvider
+import com.zegreatrob.coupling.model.user.User
+import com.zegreatrob.coupling.repository.memory.MemoryTribeRepository
+import com.zegreatrob.coupling.repository.tribe.TribeRepository
+import com.zegreatrob.coupling.repository.validation.TribeRepositoryValidator
+import com.zegreatrob.minassert.assertIsEqualTo
+import com.zegreatrob.testmints.async.setupAsync
+import com.zegreatrob.testmints.async.testAsync
+import stubTribe
+import stubUser
+import kotlin.test.Test
+
+class CompoundTribeRepositoryTest : TribeRepositoryValidator {
+    override suspend fun withRepository(clock: TimeProvider, handler: suspend (TribeRepository, User) -> Unit) {
+        val stubUser = stubUser()
+
+        val repository1 = MemoryTribeRepository(stubUser.email, clock)
+        val repository2 = MemoryTribeRepository(stubUser.email, clock)
+
+        val compoundRepo = CompoundTribeRepository(repository1, repository2)
+        handler(compoundRepo, stubUser)
+    }
+
+    @Test
+    fun saveWillWriteToSecondRepositoryAsWell() = testAsync {
+        setupAsync(object {
+            val stubUser = stubUser()
+
+            val repository1 = MemoryTribeRepository(stubUser.email, TimeProvider)
+            val repository2 = MemoryTribeRepository(stubUser.email, TimeProvider)
+
+            val compoundRepo = CompoundTribeRepository(repository1, repository2)
+
+            val tribe = stubTribe()
+        }) exerciseAsync {
+            compoundRepo.save(tribe)
+        } verifyAsync {
+            repository2.getTribe(tribe.id)
+                .assertIsEqualTo(tribe)
+        }
+    }
+
+    @Test
+    fun deleteWillWriteToSecondRepositoryAsWell() = testAsync {
+        setupAsync(object {
+            val stubUser = stubUser()
+
+            val repository1 = MemoryTribeRepository(stubUser.email, TimeProvider)
+            val repository2 = MemoryTribeRepository(stubUser.email, TimeProvider)
+
+            val compoundRepo = CompoundTribeRepository(repository1, repository2)
+
+            val tribe = stubTribe()
+        }) exerciseAsync {
+            compoundRepo.save(tribe)
+            compoundRepo.delete(tribe.id)
+        } verifyAsync {
+            repository2.getTribe(tribe.id)
+                .assertIsEqualTo(null)
+        }
+    }
+}
