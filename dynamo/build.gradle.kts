1ec33574f928134b7721d8094c712ccd3ebbdaeb
RoB Murdock <robert.f.murdock@gmail.com>
2020-02-29 17:33:07 -0800

    Adding a logger for dynamo so I can do some query analysis before going ham with it

--- dynamo/build.gradle.kts
+++ dynamo/build.gradle.kts
@@ -28,6 +28,7 @@ kotlin {
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-common:1.3.3")
                 implementation("com.soywiz.korlibs.klock:klock:1.8.9")
                 implementation("com.benasher44:uuid:0.0.7")
+                implementation("io.github.microutils:kotlin-logging-common:1.7.8")
             }
         }
         getByName("commonTest") {
@@ -47,6 +48,7 @@ kotlin {
             dependencies {
                 api(project(":logging"))
                 implementation(npm("aws-sdk", "2.615.0"))
+                implementation("io.github.microutils:kotlin-logging-js:1.7.8")
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-js:1.3.3")
             }
         }
--- dynamo/src/jsMain/kotlin/com/zegreatrob/coupling/dynamo/DynamoItemDeleteSyntax.kt
+++ dynamo/src/jsMain/kotlin/com/zegreatrob/coupling/dynamo/DynamoItemDeleteSyntax.kt
@@ -5,19 +5,22 @@ import kotlinx.coroutines.await
 import kotlin.js.Json
 import kotlin.js.json
 
-interface DynamoItemDeleteSyntax : DynamoDatatypeSyntax, DynamoDBSyntax, DynamoTableNameSyntax, DynamoItemGetSyntax {
-    suspend fun performDelete(id: String, recordJson: Json, tribeId: TribeId? = null) = try {
-        val current = performGetSingleItemQuery(id, tribeId)
-        if (current == null) {
+interface DynamoItemDeleteSyntax : DynamoDatatypeSyntax, DynamoDBSyntax, DynamoTableNameSyntax, DynamoItemGetSyntax,
+    DynamoLoggingSyntax {
+    suspend fun performDelete(id: String, recordJson: Json, tribeId: TribeId? = null) = logAsync("deleteItem") {
+        try {
+            val current = performGetSingleItemQuery(id, tribeId)
+            if (current == null) {
+                false
+            } else {
+                val updatedCurrent = current.add(recordJson).add(json("isDeleted" to true))
+                documentClient.put(updatedCurrent.deleteItemParams()).promise().await()
+                true
+            }
+        } catch (uhOh: Throwable) {
+            println("message: ${uhOh.message} uh oh ${JSON.stringify(uhOh)}")
             false
-        } else {
-            val updatedCurrent = current.add(recordJson).add(json("isDeleted" to true))
-            documentClient.put(updatedCurrent.deleteItemParams()).promise().await()
-            true
         }
-    } catch (uhOh: Throwable) {
-        println("message: ${uhOh.message} uh oh ${JSON.stringify(uhOh)}")
-        false
     }
 
     private inline fun Json.deleteItemParams() = json(
