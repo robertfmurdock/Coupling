63cc2333bcb7acb5c46e595980698d79f16fa18e
RoB Murdock <robert.f.murdock@gmail.com>
2020-03-16 15:32:15 -0400

    Ok. Turning on compound in prod. Finger crossed.

--- dynamo/src/jsMain/kotlin/com/zegreatrob/coupling/dynamo/DynamoDbProvider.kt
+++ dynamo/src/jsMain/kotlin/com/zegreatrob/coupling/dynamo/DynamoDbProvider.kt
@@ -3,17 +3,23 @@ package com.zegreatrob.coupling.dynamo
 import com.zegreatrob.coupling.dynamo.external.DocumentClient
 import com.zegreatrob.coupling.dynamo.external.DynamoDB
 import com.zegreatrob.coupling.dynamo.external.config
+import kotlin.js.Json
 import kotlin.js.json
 
 object DynamoDbProvider : DynamoDBSyntax {
     override val dynamoDB: DynamoDB by lazy {
-        config.update(
-            json(
-                "region" to "us-east-1",
-                "endpoint" to "http://localhost:8000"
-            )
-        )
+        config.update(dynamoConfig())
         DynamoDB()
+    }
+
+    private fun dynamoConfig(): Json {
+        val secret = js("process.env.AWS_SECRET_ACCESS_KEY")
+        val json = json("region" to "us-east-1")
+        return if (secret == null)
+            json.add(json("endpoint" to "http://localhost:8000"))
+        else {
+            json
+        }
     }
 
     override val documentClient: DocumentClient by lazy {
--- server/src/main/kotlin/com/zegreatrob/coupling/server/RepositoryCatalog.kt
+++ server/src/main/kotlin/com/zegreatrob/coupling/server/RepositoryCatalog.kt
@@ -2,8 +2,10 @@ package com.zegreatrob.coupling.server
 
 import com.benasher44.uuid.Uuid
 import com.soywiz.klock.TimeProvider
+import com.zegreatrob.coupling.dynamo.DynamoUserRepository
 import com.zegreatrob.coupling.model.user.User
 import com.zegreatrob.coupling.mongo.user.MongoUserRepository
+import com.zegreatrob.coupling.repository.compound.CompoundUserRepository
 import com.zegreatrob.coupling.repository.pairassignmentdocument.PairAssignmentDocumentRepository
 import com.zegreatrob.coupling.repository.pin.PinRepository
 import com.zegreatrob.coupling.repository.player.PlayerEmailRepository
@@ -26,12 +28,14 @@ suspend fun commandDispatcher(
     scope: CoroutineScope,
     traceId: Uuid?
 ): CommandDispatcher {
-    val repositoryCatalog = MongoRepositoryCatalog(userCollection, jsRepository, user)
+    val repositoryCatalog = compoundRepositoryCatalog(userCollection, jsRepository, user)
     return CommandDispatcher(user, repositoryCatalog, scope, traceId)
 }
 
-suspend fun userRepository(userCollection: dynamic, userEmail: String) =
-    mongoUserRepository(userCollection, userEmail)
+suspend fun userRepository(userCollection: dynamic, userEmail: String) = CompoundUserRepository(
+    mongoUserRepository(userCollection, userEmail),
+    DynamoUserRepository(userEmail, TimeProvider)
+)
 
 private fun mongoUserRepository(userCollection: dynamic, userEmail: String) = object : MongoUserRepository {
     override val userCollection = userCollection
