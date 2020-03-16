7bc8a93009ebdab2e9fb0a80c5c5a9cb50f33de3
RoB Murdock <robert.f.murdock@gmail.com>
2020-03-16 13:44:12 -0400

    Added some more log clearing to e2e tests

--- server/src/endToEndTest/kotlin/com/zegreatrob/coupling/server/e2e/TestLogin.kt
+++ server/src/endToEndTest/kotlin/com/zegreatrob/coupling/server/e2e/TestLogin.kt
@@ -1,6 +1,8 @@
 package com.zegreatrob.coupling.server.e2e
 
 import com.zegreatrob.coupling.server.e2e.external.protractor.ProtractorSyntax
+import com.zegreatrob.coupling.server.e2e.external.protractor.browser
+import kotlinx.coroutines.await
 
 object TestLogin : ProtractorSyntax {
     suspend fun login(userEmail: String) {
@@ -13,6 +15,13 @@ object TestLogin : ProtractorSyntax {
                 println("Failed login attempt $attempt")
                 if (attempt == 3) throw throwable
             }
+        }
+        clearLogs()
+    }
+
+    private suspend fun clearLogs() {
+        if (browser.getCapabilities().await()["browserName"] != "firefox") {
+            browser.manage().logs().get("browser").await()
         }
     }
 
