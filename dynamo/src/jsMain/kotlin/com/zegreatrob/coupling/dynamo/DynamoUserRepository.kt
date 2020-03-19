6e9888b0d74a8eab855bb1b36e989d3df5c21d02
RoB Murdock <robert.f.murdock@gmail.com>
2020-03-19 11:42:57 -0400

    Changing the user table and scan to deal with realistic amounts of user data

--- dynamo/src/jsMain/kotlin/com/zegreatrob/coupling/dynamo/DynamoUserRepository.kt
+++ dynamo/src/jsMain/kotlin/com/zegreatrob/coupling/dynamo/DynamoUserRepository.kt
@@ -5,7 +5,7 @@ import com.zegreatrob.coupling.model.Record
 import com.zegreatrob.coupling.model.user.User
 import com.zegreatrob.coupling.model.user.UserEmailSyntax
 import com.zegreatrob.coupling.repository.user.UserRepository
-import kotlinx.coroutines.await
+import kotlin.js.Json
 import kotlin.js.json
 
 class DynamoUserRepository private constructor(override val userEmail: String, override val clock: TimeProvider) :
@@ -18,31 +18,116 @@ class DynamoUserRepository private constructor(override val userEmail: String, o
         CreateTableParamProvider,
         DynamoItemPutSyntax,
         DynamoQuerySyntax,
-        DynamoItemSyntax {
+        DynamoItemSyntax,
+        DynamoScanSyntax {
         override val tableName = "USER"
+        val userEmailIndex = "USER_EMAIL_INDEX"
         suspend operator fun invoke(email: String, clock: TimeProvider) = DynamoUserRepository(email, clock)
             .also { ensureTableExists() }
+
+        override val createTableParams: Json
+            get() = json(
+                "TableName" to tableName,
+                "KeySchema" to arrayOf(
+                    json(
+                        "AttributeName" to "id",
+                        "KeyType" to "HASH"
+                    ),
+                    json(
+                        "AttributeName" to "timestamp",
+                        "KeyType" to "RANGE"
+                    )
+                ),
+                "AttributeDefinitions" to arrayOf(
+                    json(
+                        "AttributeName" to "id",
+                        "AttributeType" to "S"
+                    ),
+                    json(
+                        "AttributeName" to "timestamp",
+                        "AttributeType" to "S"
+                    ),
+                    json(
+                        "AttributeName" to "email",
+                        "AttributeType" to "S"
+                    )
+                ),
+                "BillingMode" to "PAY_PER_REQUEST"
+            ).add(
+                json(
+                    "GlobalSecondaryIndexes" to arrayOf(
+                        json(
+                            "IndexName" to userEmailIndex,
+                            "KeySchema" to arrayOf(
+                                json(
+                                    "AttributeName" to "email",
+                                    "KeyType" to "HASH"
+                                ),
+                                json(
+                                    "AttributeName" to "id",
+                                    "KeyType" to "RANGE"
+                                )
+                            ),
+                            "Projection" to json(
+                                "NonKeyAttributes" to arrayOf(
+                                    "timestamp"
+                                ),
+                                "ProjectionType" to "INCLUDE"
+                            )
+                        )
+                    )
+                )
+            )
+
     }
 
     override suspend fun save(user: User) = performPutItem(user.toRecord().asDynamoJson())
 
-    override suspend fun getUser() = documentClient.scan(emailScanParams()).promise().await()
-        .itemsNode()
-        .sortByRecordTimestamp()
-        .lastOrNull()
-        ?.toUserRecord()
+    override suspend fun getUser(): Record<User>? {
+        return logAsync("getUser") {
+            val userIdsWithEmail = logAsync("userIdsWithEmail") {
+                performQuery(emailQueryParams(userEmail))
+                    .itemsNode()
+                    .mapNotNull { it.getDynamoStringValue("id") }
+            }
 
-    private fun emailScanParams() = json(
+            logAsync("get user with id latest revision") {
+                performQuery(queryParams(userIdsWithEmail.first()))
+                    .itemsNode()
+                    .sortByRecordTimestamp()
+                    .lastOrNull()
+                    ?.toUserRecord()
+            }
+        }
+    }
+
+    private fun queryParams(id: String) = json(
         "TableName" to tableName,
-        "ExpressionAttributeValues" to json(":email" to userEmail),
-        "FilterExpression" to "email = :email"
+        "ExpressionAttributeValues" to json(":id" to id),
+        "KeyConditionExpression" to "id = :id"
+    )
+
+    private fun emailQueryParams(email: String) = json(
+        "TableName" to tableName,
+        "IndexName" to userEmailIndex,
+        "ExpressionAttributeValues" to json(":email" to email),
+        "KeyConditionExpression" to "email = :email"
     )
 
     suspend fun saveRawRecord(record: Record<User>) = performPutItem(record.asDynamoJson())
 
-    suspend fun getUserRecords() = documentClient.scan(json("TableName" to tableName)).promise().await()
-        .itemsNode()
+    suspend fun getUserRecords() = scanRecords()
         .sortByRecordTimestamp()
         .map { it.toUserRecord() }
+
+    private suspend fun scanRecords(): Array<Json> = performScan(json("TableName" to tableName))
+        .continueScan()
+
+    private suspend fun Json.continueScan(): Array<Json> = if (this["LastEvaluatedKey"] != null) {
+        itemsNode() + performScan(
+            json("TableName" to tableName, "ExclusiveStartKey" to this["LastEvaluatedKey"])
+        ).continueScan()
+    } else
+        itemsNode()
 
 }
