95a4d663bb06ecf93bca3afb97ab686d7a27ba75
RoB Murdock <robert.f.murdock@gmail.com>
2020-03-25 21:56:22 -0400

    Looks like font-awesome wasn't loaded other than in the vendor lib. Fixed that.

--- client/src/main/kotlin/com/zegreatrob/coupling/client/Main.kt
+++ client/src/main/kotlin/com/zegreatrob/coupling/client/Main.kt
@@ -4,5 +4,6 @@ fun main() {
     js("require('prefixfree')")
     js("require('com/zegreatrob/coupling/client/style.scss')")
     js("require('com/zegreatrob/coupling/client/animations.scss')")
+    js("require('@fortawesome/fontawesome-free/css/all.css')")
     App.bootstrapApp()
 }
