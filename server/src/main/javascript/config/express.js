66169b67620e05b2754b45ad06020d02302fff3a
RoB Murdock <robert.f.murdock@gmail.com>
2020-03-27 13:24:52 -0400

    Adding a tiny bit of reporting to failures because of the sporadic e2e login problems in circleci

--- server/src/main/javascript/config/express.js
+++ server/src/main/javascript/config/express.js
@@ -156,6 +156,7 @@ module.exports = function (app, userDataService) {
   if (isInDevelopmentMode) {
     passport.use(new LocalStrategy(function (username, password, done) {
       userDataService.findOrCreate(username + "._temp", null, function (err, user) {
+        console.error(`Problem with find or create user '${username}' in local strategy`, err);
         done(err, user);
       });
     }));
