8fed1c95508cab8f00a9fd71b9ff7c680c02f2cb
RoB Murdock <robert.f.murdock@gmail.com>
2020-04-06 11:25:26 -0400

    Removing redundant authorization check in get tribe

--- server/server_action/src/commonMain/kotlin/com/zegreatrob/coupling/server/action/tribe/TribeQuery.kt
+++ server/server_action/src/commonMain/kotlin/com/zegreatrob/coupling/server/action/tribe/TribeQuery.kt
@@ -2,14 +2,8 @@ package com.zegreatrob.coupling.server.action.tribe
 
 import com.zegreatrob.coupling.action.Action
 import com.zegreatrob.coupling.action.ActionLoggingSyntax
-import com.zegreatrob.coupling.model.Record
-import com.zegreatrob.coupling.model.tribe.Tribe
-import com.zegreatrob.coupling.model.tribe.TribeElement
 import com.zegreatrob.coupling.model.tribe.TribeId
-import com.zegreatrob.coupling.repository.await
 import com.zegreatrob.coupling.repository.tribe.TribeIdGetRecordSyntax
-import kotlinx.coroutines.async
-import kotlinx.coroutines.coroutineScope
 
 data class TribeQuery(val tribeId: TribeId) : Action
 
@@ -17,18 +11,6 @@ interface TribeQueryDispatcher : ActionLoggingSyntax, UserAuthenticatedTribeIdSy
     TribeIdGetRecordSyntax,
     UserPlayerIdsSyntax {
 
-    suspend fun TribeQuery.perform() = logAsync { getTribeAndUserPlayerIds().onlyIfAuthenticated() }
-
-    private suspend fun TribeQuery.getTribeAndUserPlayerIds() = coroutineScope {
-        await(
-            async { tribeId.loadRecord() },
-            async { getUserPlayerIds() }
-        )
-    }
-
-    private fun Pair<Record<Tribe>?, List<TribeElement<String>>>.onlyIfAuthenticated() = let { (tribe, players) ->
-        tribe?.takeIf(players.authenticatedFilter())
-    }
+    suspend fun TribeQuery.perform() = logAsync { tribeId.loadRecord() }
 
 }
-
