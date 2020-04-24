9953ac5ea08804d4db0db65ec046e07499cf7c75
RoB Murdock <robert.f.murdock@gmail.com>
2020-04-24 17:42:06 -0400

    More minor refactors because I'm idling.

--- server/src/main/kotlin/com/zegreatrob/coupling/server/ConfigureExpress.kt
+++ server/src/main/kotlin/com/zegreatrob/coupling/server/ConfigureExpress.kt
@@ -5,7 +5,10 @@ import com.zegreatrob.coupling.dynamo.DynamoDbProvider
 import com.zegreatrob.coupling.logging.initializeLogging
 import com.zegreatrob.coupling.model.user.User
 import com.zegreatrob.coupling.server.external.bodyparser.urlencoded
-import com.zegreatrob.coupling.server.external.express.*
+import com.zegreatrob.coupling.server.external.express.Express
+import com.zegreatrob.coupling.server.external.express.Handler
+import com.zegreatrob.coupling.server.external.express.Response
+import com.zegreatrob.coupling.server.external.express.static
 import com.zegreatrob.coupling.server.external.googleauthlibrary.OAuth2Client
 import com.zegreatrob.coupling.server.external.passport.passport
 import com.zegreatrob.coupling.server.external.passportazuread.OIDCStrategy
@@ -102,7 +105,7 @@ private fun Express.configure() {
     set("views", arrayOf(resourcePath("public"), resourcePath("views")))
     set("view engine", "pug")
     use(favicon(resourcePath("public/images/favicon.ico")))
-    use(addTraceId())
+    use(tracer())
 
     if (Config.disableLogging) {
         use(logRequests())
@@ -125,7 +128,7 @@ private fun Express.configure() {
     configPassport()
 }
 
-private fun addTraceId(): Handler = { request, _, next -> request.asDynamic().traceId = uuid4(); next() }
+private fun tracer(): Handler = { request, _, next -> request.asDynamic().traceId = uuid4(); next() }
 
 private fun Express.isInDevMode() = when (get("env")) {
     "development" -> true
@@ -133,44 +136,35 @@ private fun Express.isInDevMode() = when (get("env")) {
     else -> false
 }
 
-fun azureODICStrategy() = OIDCStrategy(
-    json(
-        "identityMetadata" to Config.microsoft.identityMetadata,
-        "clientID" to Config.microsoft.clientID,
-        "responseType" to Config.microsoft.responseType,
-        "responseMode" to Config.microsoft.responseMode,
-        "redirectUrl" to Config.microsoft.redirectUrl,
-        "allowHttpForRedirectUrl" to Config.microsoft.allowHttpForRedirectUrl,
-        "clientSecret" to Config.microsoft.clientSecret,
-        "validateIssuer" to Config.microsoft.validateIssuer,
-        "isB2C" to Config.microsoft.isB2C,
-        "issuer" to Config.microsoft.issuer,
-        "passReqToCallback" to true,
-        "scope" to Config.microsoft.scope,
-        "loggingLevel" to Config.microsoft.loggingLevel,
-        "nonceLifetime" to Config.microsoft.nonceLifetime,
-        "nonceMaxAmount" to Config.microsoft.nonceMaxAmount,
-        "useCookieInsteadOfSession" to Config.microsoft.useCookieInsteadOfSession,
-        "cookieEncryptionKeys" to Config.microsoft.cookieEncryptionKeys,
-        "clockSkew" to Config.microsoft.clockSkew
-    ),
-    fun(
-        request: Request,
-        iss: String,
-        sub: String,
-        profile: Json,
-        accessToken: String,
-        refreshToken: String,
-        done: (dynamic, dynamic) -> Unit
-    ) {
-        MainScope().promise {
-            val email = profile["_json"].unsafeCast<Json>()["email"].unsafeCast<String?>()
-            email?.let {
-                UserDataService.findOrCreate(email, request.traceId)
-            }
-        }.then({ if (it != null) done(null, it) else done("Auth succeeded but no email found", null) },
-            { done(it, null) })
-    }
+fun azureODICStrategy() = OIDCStrategy(azureOidcConfig(), fun(request, _, _, profile, _, _, done) {
+    MainScope().promise {
+        val email = profile["_json"].unsafeCast<Json>()["email"].unsafeCast<String?>()
+        email?.let {
+            UserDataService.findOrCreate(email, request.traceId)
+        }
+    }.then({ if (it != null) done(null, it) else done("Auth succeeded but no email found", null) },
+        { done(it, null) })
+})
+
+private fun azureOidcConfig() = json(
+    "identityMetadata" to Config.microsoft.identityMetadata,
+    "clientID" to Config.microsoft.clientID,
+    "responseType" to Config.microsoft.responseType,
+    "responseMode" to Config.microsoft.responseMode,
+    "redirectUrl" to Config.microsoft.redirectUrl,
+    "allowHttpForRedirectUrl" to Config.microsoft.allowHttpForRedirectUrl,
+    "clientSecret" to Config.microsoft.clientSecret,
+    "validateIssuer" to Config.microsoft.validateIssuer,
+    "isB2C" to Config.microsoft.isB2C,
+    "issuer" to Config.microsoft.issuer,
+    "passReqToCallback" to true,
+    "scope" to Config.microsoft.scope,
+    "loggingLevel" to Config.microsoft.loggingLevel,
+    "nonceLifetime" to Config.microsoft.nonceLifetime,
+    "nonceMaxAmount" to Config.microsoft.nonceMaxAmount,
+    "useCookieInsteadOfSession" to Config.microsoft.useCookieInsteadOfSession,
+    "cookieEncryptionKeys" to Config.microsoft.cookieEncryptionKeys,
+    "clockSkew" to Config.microsoft.clockSkew
 )
 
 fun googleAuthTransferStrategy(): dynamic {
