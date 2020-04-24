d72aeb722084aa4f1f16ffc00e096aa765f7e661
RoB Murdock <robert.f.murdock@gmail.com>
2020-04-24 12:07:59 -0400

    Minor refactor.

--- server/src/main/javascript/app.ts
+++ server/src/main/javascript/app.ts
@@ -5,11 +5,13 @@ import * as expressWs from "express-ws";
 const config = require('./config/config');
 const serverKt = require("Coupling-server");
 
+const {configureExpressKt, configRoutes, logStartup} = serverKt.com.zegreatrob.coupling.server;
+
 function listen(app) {
     return new Promise(function (resolve) {
         const server = app.listen(app.get('port'), function () {
             // noinspection JSUnresolvedVariable, JSUnresolvedFunction
-            serverKt.com.zegreatrob.coupling.server.logStartup(
+            logStartup(
                 app.get('port'),
                 config.buildDate,
                 config.gitRev,
@@ -25,8 +27,6 @@ export async function start() {
     const wsInstance = expressWs(express());
     const app = wsInstance.app;
     const userDataService = new UserDataService();
-
-    const {configureExpressKt, configRoutes} = serverKt.com.zegreatrob.coupling.server;
 
     configureExpressKt(app)
     require('./config/express')(app, userDataService);
