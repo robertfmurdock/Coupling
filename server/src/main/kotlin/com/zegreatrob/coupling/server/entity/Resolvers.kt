62b8772090ec6fa4305a68b6df57af80a62b015b
RoB Murdock <robert.f.murdock@gmail.com>
2020-04-27 15:24:50 -0400

    Minor minor refactor

--- server/src/main/kotlin/com/zegreatrob/coupling/server/entity/Resolvers.kt
+++ server/src/main/kotlin/com/zegreatrob/coupling/server/entity/Resolvers.kt
@@ -8,17 +8,20 @@ import kotlin.js.Json
 
 typealias CommandResolver = suspend CommandDispatcher.(Json, Json) -> Any?
 
-fun verifyAuth(dispatcher: suspend CurrentTribeIdDispatcher.() -> Any?): CommandResolver =
-    { entity, _ -> verifyAuth(entity, dispatcher) }
+fun verifyAuth(dispatcher: suspend CurrentTribeIdDispatcher.() -> Any?): CommandResolver = { entity, _ ->
+    verifyAuth(entity, dispatcher)
+}
 
 fun buildResolver(func: CommandResolver) = { entity: Json, args: Json, request: Request ->
     request.scope.promise { func(request.commandDispatcher, entity, args) }
 }
 
 suspend fun CommandDispatcher.verifyAuth(entity: Json, func: suspend CurrentTribeIdDispatcher.() -> Any?): Any? {
-    val dispatcher = authorizedTribeIdDispatcher(entity["id"].toString())
+    val dispatcher = authorizedTribeIdDispatcher(entity.tribeId())
     return when {
         dispatcher.isAuthorized() -> func(dispatcher)
         else -> null
     }
 }
+
+private fun Json.tribeId() = this["id"].toString()
