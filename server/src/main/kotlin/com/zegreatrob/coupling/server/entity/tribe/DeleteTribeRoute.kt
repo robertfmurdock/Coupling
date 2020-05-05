5e30882d821ac06f3ca6f3080457aaec54756199
RoB Murdock <robert.f.murdock@gmail.com>
2020-05-05 16:52:09 -0400

    More use of the new route style.

--- server/src/main/kotlin/com/zegreatrob/coupling/server/entity/tribe/DeleteTribeRoute.kt
+++ server/src/main/kotlin/com/zegreatrob/coupling/server/entity/tribe/DeleteTribeRoute.kt
@@ -1,15 +1,11 @@
 package com.zegreatrob.coupling.server.entity.tribe
 
 import com.zegreatrob.coupling.server.DeleteTribeCommand
-import com.zegreatrob.coupling.server.DeleteTribeCommandDispatcher
 import com.zegreatrob.coupling.server.ResponseHelpers.sendDeleteResults
 import com.zegreatrob.coupling.server.external.express.Request
 import com.zegreatrob.coupling.server.external.express.tribeId
 import com.zegreatrob.coupling.server.route.dispatchCommand
 
-val deleteTribeRoute = dispatchCommand { endpointHandler(sendDeleteResults("Tribe"), ::runCommand) }
+val deleteTribeRoute = dispatchCommand(::deleteTribeCommand, { it.perform() }, { it }, sendDeleteResults("Tribe"))
 
-private suspend fun DeleteTribeCommandDispatcher.runCommand(request: Request) = request.deleteTribeCommand()
-    .perform()
-
-private fun Request.deleteTribeCommand() = DeleteTribeCommand(tribeId())
+private fun deleteTribeCommand(request: Request) = with(request) { DeleteTribeCommand(tribeId()) }
