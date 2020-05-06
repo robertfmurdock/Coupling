ca87f120350e4d5f3e0ee1d2c64bf01403a50e4d
RoB Murdock <robert.f.murdock@gmail.com>
2020-05-06 11:43:57 -0400

    Upgradle. Cache busting.

--- server/src/main/kotlin/com/zegreatrob/coupling/server/entity/tribe/DeleteTribeRoute.kt
+++ server/src/main/kotlin/com/zegreatrob/coupling/server/entity/tribe/DeleteTribeRoute.kt
@@ -2,9 +2,9 @@ package com.zegreatrob.coupling.server.entity.tribe
 
 import com.zegreatrob.coupling.server.action.DeleteTribeCommand
 import com.zegreatrob.coupling.server.express.ResponseHelpers.sendDeleteResults
+import com.zegreatrob.coupling.server.express.route.dispatchCommand
 import com.zegreatrob.coupling.server.external.express.Request
 import com.zegreatrob.coupling.server.external.express.tribeId
-import com.zegreatrob.coupling.server.express.route.dispatchCommand
 
 val deleteTribeRoute = dispatchCommand(::deleteTribeCommand, { it.perform() }, { it }, sendDeleteResults("Tribe"))
 
