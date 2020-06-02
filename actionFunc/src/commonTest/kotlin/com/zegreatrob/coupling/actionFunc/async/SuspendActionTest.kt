facade639e9070a0cbf5e5598cb78f24627bb603
RoB Murdock <robert.f.murdock@gmail.com>
2020-06-02 09:28:32 -0400

    Adding additional example case to the suspend test

--- actionFunc/src/commonTest/kotlin/com/zegreatrob/coupling/actionFunc/async/SuspendActionTest.kt
+++ actionFunc/src/commonTest/kotlin/com/zegreatrob/coupling/actionFunc/async/SuspendActionTest.kt
@@ -3,6 +3,7 @@ package com.zegreatrob.coupling.actionFunc.async
 import com.zegreatrob.minassert.assertIsEqualTo
 import com.zegreatrob.minspy.SpyData
 import com.zegreatrob.testmints.async.asyncSetup
+import kotlinx.coroutines.coroutineScope
 import kotlin.test.Test
 
 private typealias MultiplyActionDispatcher = suspend (SuspendActionTest.MultiplyAction) -> Int
@@ -11,6 +12,21 @@ class SuspendActionTest : SuspendActionExecuteSyntax {
 
     data class MultiplyAction(val left: Int, val right: Int) : SimpleSuspendAction<MultiplyActionDispatcher, Int> {
         override val performFunc = link(MultiplyActionDispatcher::invoke)
+    }
+
+    suspend fun sillyMultiplyImplementation(action: MultiplyAction) = coroutineScope {
+        (1..action.left)
+            .fold(0) { current, _ -> current + action.right }
+    }
+
+    @Test
+    fun usingTheActionWithTheDispatcherDoesTheWorkOfTheDispatchFunction() = asyncSetup(object {
+        val action = MultiplyAction(2, 3)
+        val dispatcher: MultiplyActionDispatcher = ::sillyMultiplyImplementation
+    }) exercise {
+        dispatcher.execute(action)
+    } verify { result ->
+        result.assertIsEqualTo(6)
     }
 
     @Test
