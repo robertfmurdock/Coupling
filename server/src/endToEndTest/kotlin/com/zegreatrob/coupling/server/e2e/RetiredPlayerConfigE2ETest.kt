287a19bdd9e3b24f0cde31f7b7b64b3f2a1d1bfc
RoB Murdock <robert.f.murdock@gmail.com>
2020-06-15 14:53:09 -0400

    And example of async config in the new world w/o template use.

--- server/src/endToEndTest/kotlin/com/zegreatrob/coupling/server/e2e/RetiredPlayerConfigE2ETest.kt
+++ server/src/endToEndTest/kotlin/com/zegreatrob/coupling/server/e2e/RetiredPlayerConfigE2ETest.kt
@@ -6,30 +6,30 @@ import com.zegreatrob.coupling.model.tribe.TribeId
 import com.zegreatrob.coupling.model.tribe.with
 import com.zegreatrob.coupling.server.e2e.CouplingLogin.sdkProvider
 import com.zegreatrob.minassert.assertIsEqualTo
-import com.zegreatrob.testmints.async.setupAsync
-import com.zegreatrob.testmints.async.testAsync
+import com.zegreatrob.testmints.async.asyncSetup
 import kotlinx.coroutines.await
 import kotlin.test.Test
 
 class RetiredPlayerConfigE2ETest {
 
     @Test
-    fun willShowThePlayerData() = testAsync {
-        val sdk = sdkProvider.await()
-        setupAsync(object {
+    fun willShowThePlayerData() = asyncSetup(
+        contextProvider = attachSdk(object : SdkContext() {
             val tribe = Tribe(TribeId("${randomInt()}-RetiredPlayerConfigE2E"))
             val player = Player("${randomInt()}-RetiredPlayerConfigE2E", name = "${randomInt()}-RetiredPlayerConfigE2E")
-        }) {
-            sdk.save(tribe)
-            sdk.save(tribe.id.with(player))
-            sdk.deletePlayer(tribe.id, player.id!!)
-            CouplingLogin.loginProvider
-        } exerciseAsync {
-            RetiredPlayerConfig.goTo(tribe.id, player.id)
-        } verifyAsync {
-            RetiredPlayerConfig.playerNameTextField.getAttribute("value").await()
-                .assertIsEqualTo(player.name)
-        }
+        })
+    ) {
+        sdk.save(tribe)
+        sdk.save(tribe.id.with(player))
+        sdk.deletePlayer(tribe.id, player.id!!)
+        CouplingLogin.loginProvider.await()
+    } exercise {
+        RetiredPlayerConfig.goTo(tribe.id, player.id)
+    } verify {
+        RetiredPlayerConfig.playerNameTextField.getAttribute("value").await()
+            .assertIsEqualTo(player.name)
     }
+
+    private fun <C : SdkContext> attachSdk(context: C) = suspend { context.attach(sdkProvider.await()) }
 
 }
--- server/src/endToEndTest/kotlin/com/zegreatrob/coupling/server/e2e/SdkContext.kt
+++ server/src/endToEndTest/kotlin/com/zegreatrob/coupling/server/e2e/SdkContext.kt
@@ -5,3 +5,7 @@ import com.zegreatrob.coupling.sdk.Sdk
 abstract class SdkContext {
     lateinit var sdk: Sdk
 }
+
+fun <C: SdkContext> C.attach(sdk: Sdk) = also {
+    this.sdk = sdk
+}
