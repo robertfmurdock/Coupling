5a0ac627904579cff328b9674726645ad6e7cd14
RoB Murdock <robert.f.murdock@gmail.com>
2020-06-15 15:16:36 -0400

    Removing more deprecations, seeing a few patterns.

--- server/src/endToEndTest/kotlin/com/zegreatrob/coupling/server/e2e/StatisticsE2ETest.kt
+++ server/src/endToEndTest/kotlin/com/zegreatrob/coupling/server/e2e/StatisticsE2ETest.kt
@@ -4,37 +4,30 @@ import com.zegreatrob.coupling.model.player.Player
 import com.zegreatrob.coupling.model.tribe.Tribe
 import com.zegreatrob.coupling.model.tribe.TribeId
 import com.zegreatrob.coupling.model.tribe.with
-import com.zegreatrob.coupling.server.e2e.CouplingLogin.sdkProvider
 import com.zegreatrob.minassert.assertIsEqualTo
-import com.zegreatrob.testmints.async.setupAsync
-import com.zegreatrob.testmints.async.testAsync
 import kotlinx.coroutines.await
 import kotlin.test.Test
 
 class StatisticsE2ETest {
-
     @Test
-    fun pageShowsImportantElements() = testAsync {
-        val sdk = sdkProvider.await()
-        setupAsync(object {
-            val tribe = Tribe(TribeId("${randomInt()}-statsE2E"), name = "Funkytown")
-            val players = generateSequence { Player(id = "${randomInt()}-statsE2E") }
-                .take(6).toList()
-        }) {
-            sdk.save(tribe)
-            players.forEach { player -> sdk.save(tribe.id.with(player)) }
-            CouplingLogin.loginProvider
-        } exerciseAsync {
-            StatisticsPage.goTo(tribe.id)
-        } verifyAsync {
-            with(StatisticsPage) {
-                TribeCard.element.getText().await()
-                    .assertIsEqualTo(tribe.name)
-                rotationNumber.getText().await()
-                    .assertIsEqualTo("5")
-                pairReports.count().await()
-                    .assertIsEqualTo(15)
-            }
+    fun pageShowsImportantElements() = sdkSetup(object : SdkContext() {
+        val tribe = Tribe(TribeId("${randomInt()}-statsE2E"), name = "Funkytown")
+        val players = generateSequence { Player(id = "${randomInt()}-statsE2E") }
+            .take(6).toList()
+    }) {
+        sdk.save(tribe)
+        players.forEach { player -> sdk.save(tribe.id.with(player)) }
+        CouplingLogin.loginProvider.await()
+    } exercise {
+        StatisticsPage.goTo(tribe.id)
+    } verify {
+        with(StatisticsPage) {
+            TribeCard.element.getText().await()
+                .assertIsEqualTo(tribe.name)
+            rotationNumber.getText().await()
+                .assertIsEqualTo("5")
+            pairReports.count().await()
+                .assertIsEqualTo(15)
         }
     }
 }
--- server/src/endToEndTest/kotlin/com/zegreatrob/coupling/server/e2e/TribeConfigPageE2ETest.kt
+++ server/src/endToEndTest/kotlin/com/zegreatrob/coupling/server/e2e/TribeConfigPageE2ETest.kt
@@ -8,7 +8,7 @@ import com.zegreatrob.coupling.server.e2e.external.protractor.ProtractorSyntax
 import com.zegreatrob.coupling.server.e2e.external.protractor.performClearSendKeys
 import com.zegreatrob.coupling.server.e2e.external.protractor.performClick
 import com.zegreatrob.minassert.assertIsEqualTo
-import com.zegreatrob.testmints.async.setupAsync
+import com.zegreatrob.testmints.async.asyncSetup
 import com.zegreatrob.testmints.async.testAsync
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.await
@@ -20,53 +20,57 @@ class TribeConfigPageE2ETest {
     class GivenExistingTribe {
 
         @Test
-        fun canSaveEditsCorrectly() = testWithSdk { sdk ->
+        fun canSaveEditsCorrectly() = sdkSetup(object : ProtractorSyntax, SdkContext() {
             val tribe = buildTribe()
+            val expectedNewName = "Different name"
+            val expectedDefaultBadgeName = "New Default Badge Name"
+            val expectedAltBadgeName = "New Alt Badge Name"
+            val expectedCallSignSelection = "true"
+            val expectedBadgeSelection = "true"
+            val page = TribeConfigPage
+        }) {
             sdk.save(tribe)
-            with(TribeConfigPage) {
-                setupAsync(object : ProtractorSyntax {
-                    val expectedNewName = "Different name"
-                    val expectedDefaultBadgeName = "New Default Badge Name"
-                    val expectedAltBadgeName = "New Alt Badge Name"
-                    val expectedCallSignSelection = "true"
-                    val expectedBadgeSelection = "true"
-                }) {
-                    goTo(tribe.id)
+            with(page) {
+                goTo(tribe.id)
 
-                    tribeNameInput.performClearSendKeys(expectedNewName)
-                    callSignCheckbox.performClick()
-                    badgeCheckbox.performClick()
-                    defaultBadgeNameInput.performClearSendKeys(expectedDefaultBadgeName)
-                    altBadgeNameInput.performClearSendKeys(expectedAltBadgeName)
-                    differentBadgesOption.performClick()
-                } exerciseAsync {
-                    saveButton.performClick()
-                    TribeListPage.waitForPage()
-                    goTo(tribe.id)
-                } verifyAsync {
-                    tribeNameInput.getAttribute("value").await()
-                        .assertIsEqualTo(expectedNewName)
-                    callSignCheckbox.getAttribute("checked").await()
-                        .assertIsEqualTo(expectedCallSignSelection)
-                    badgeCheckbox.getAttribute("checked").await()
-                        .assertIsEqualTo(expectedBadgeSelection)
-                    defaultBadgeNameInput.getAttribute("value").await()
-                        .assertIsEqualTo(expectedDefaultBadgeName)
-                    altBadgeNameInput.getAttribute("value").await()
-                        .assertIsEqualTo(expectedAltBadgeName)
-                    checkedOption.getAttribute("label").await()
-                        .assertIsEqualTo("Prefer Different Badges (Beta)")
-                }
+                tribeNameInput.performClearSendKeys(expectedNewName)
+                callSignCheckbox.performClick()
+                badgeCheckbox.performClick()
+                defaultBadgeNameInput.performClearSendKeys(expectedDefaultBadgeName)
+                altBadgeNameInput.performClearSendKeys(expectedAltBadgeName)
+                differentBadgesOption.performClick()
+            }
+        } exercise {
+            page.saveButton.performClick()
+            TribeListPage.waitForPage()
+            page.goTo(tribe.id)
+        } verify {
+            with(page) {
+                tribeNameInput.getAttribute("value").await()
+                    .assertIsEqualTo(expectedNewName)
+                callSignCheckbox.getAttribute("checked").await()
+                    .assertIsEqualTo(expectedCallSignSelection)
+                badgeCheckbox.getAttribute("checked").await()
+                    .assertIsEqualTo(expectedBadgeSelection)
+                defaultBadgeNameInput.getAttribute("value").await()
+                    .assertIsEqualTo(expectedDefaultBadgeName)
+                altBadgeNameInput.getAttribute("value").await()
+                    .assertIsEqualTo(expectedAltBadgeName)
+                checkedOption.getAttribute("label").await()
+                    .assertIsEqualTo("Prefer Different Badges (Beta)")
             }
         }
 
         @Test
-        fun showsBasicInformation() = testWithSdk { sdk ->
+        fun showsBasicInformation() = sdkSetup(object : SdkContext() {
             val tribe = buildTribe().copy(email = "${randomInt()}-email")
+            val page = TribeConfigPage
+        }) {
             sdk.save(tribe)
-            setupAsync(TribeConfigPage) exerciseAsync {
-                goTo(tribe.id)
-            } verifyAsync {
+        } exercise {
+            page.goTo(tribe.id)
+        } verify {
+            with(page) {
                 tribeNameInput.getAttribute("value").await()
                     .assertIsEqualTo(tribe.name)
                 tribeEmailInput.getAttribute("value").await()
@@ -75,21 +79,21 @@ class TribeConfigPageE2ETest {
         }
 
         @Test
-        fun canDeleteTribe() = testWithSdk { sdk ->
+        fun canDeleteTribe() = sdkSetup(object : SdkContext() {
             val tribe = buildTribe()
+        }) {
             sdk.save(tribe)
-            setupAsync(object {}) {
-                TribeConfigPage.goTo(tribe.id)
-            } exerciseAsync {
-                TribeConfigPage.deleteButton.performClick()
-                TribeListPage.waitForPage()
-            } verifyAsync {
-                TribeListPage.tribeCardElements
-                    .map { it.getText() }
-                    .await()
-                    .contains(tribe.name)
-                    .assertIsEqualTo(false)
-            }
+
+            TribeConfigPage.goTo(tribe.id)
+        } exercise {
+            TribeConfigPage.deleteButton.performClick()
+            TribeListPage.waitForPage()
+        } verify {
+            TribeListPage.tribeCardElements
+                .map { it.getText() }
+                .await()
+                .contains(tribe.name)
+                .assertIsEqualTo(false)
         }
 
         companion object {
@@ -111,27 +115,24 @@ class TribeConfigPageE2ETest {
 
     class NewTribe {
         @Test
-        fun idFieldShowsAndPersistsAsTextIsAdded() = testAsync {
+        fun idFieldShowsAndPersistsAsTextIsAdded() = asyncSetup(TribeConfigPage) {
             CouplingLogin.loginProvider.await()
-            setupAsync(TribeConfigPage) {
-                goToNew()
-            } exerciseAsync {
-                tribeIdInput.performClearSendKeys("oopsie")
-            } verifyAsync {
-                tribeIdInput.isDisplayed().await()
-                    .assertIsEqualTo(true)
-            }
+            goToNew()
+        } exercise {
+            tribeIdInput.performClearSendKeys("oopsie")
+        } verify {
+            tribeIdInput.isDisplayed().await()
+                .assertIsEqualTo(true)
         }
 
         @Test
-        fun willDefaultPairingRuleToLongestTime() = testAsync {
+        fun willDefaultPairingRuleToLongestTime() = asyncSetup(TribeConfigPage) {
             CouplingLogin.loginProvider.await()
-            setupAsync(TribeConfigPage) exerciseAsync {
-                goToNew()
-            } verifyAsync {
-                checkedOption.getAttribute("label").await()
-                    .assertIsEqualTo("Prefer Longest Time")
-            }
+        } exercise {
+            goToNew()
+        } verify {
+            checkedOption.getAttribute("label").await()
+                .assertIsEqualTo("Prefer Longest Time")
         }
     }
 
