108153e3b7b14a81dc6d28c9d5aeab82260b819f
RoB Murdock <robert.f.murdock@gmail.com>
2020-07-02 14:56:52 -0400

    The new setup has allowed even the top level to avoid having to manually await for deferreds. Lots of boilerplate gone.

--- server/src/endToEndTest/kotlin/com/zegreatrob/coupling/server/e2e/CouplingLogin.kt
+++ server/src/endToEndTest/kotlin/com/zegreatrob/coupling/server/e2e/CouplingLogin.kt
@@ -4,9 +4,4 @@ object CouplingLogin {
 
     val sdkProvider by lazyDeferred { authorizedSdk("user-${randomInt()}@email.com") }
 
-    val loginProvider by lazyDeferred {
-        val sdk = sdkProvider.await()
-        TestLogin.login(sdk.userEmail)
-    }
-
 }
--- server/src/endToEndTest/kotlin/com/zegreatrob/coupling/server/e2e/E2eSetup.kt
+++ server/src/endToEndTest/kotlin/com/zegreatrob/coupling/server/e2e/E2eSetup.kt
@@ -3,7 +3,12 @@ package com.zegreatrob.coupling.server.e2e
 import com.zegreatrob.testmints.async.TestTemplate
 import com.zegreatrob.testmints.async.asyncTestTemplate
 
-val e2eSetup: TestTemplate<Unit> = asyncTestTemplate(
-    sharedSetup = { CouplingLogin.loginProvider.await() },
+val e2eSetup: TestTemplate<AuthorizedSdk> = asyncTestTemplate(beforeAll = {
+    CouplingLogin.sdkProvider.await()
+        .also {
+            TestLogin.login(it.userEmail)
+        }
+}).extend(
+    sharedSetup = { },
     sharedTeardown = { checkLogs() }
 )
