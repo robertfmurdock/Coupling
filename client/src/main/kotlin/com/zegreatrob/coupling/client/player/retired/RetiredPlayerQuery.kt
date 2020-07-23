0af7fc552bfe851535a9701ecd52b2e51358dca1
RoB Murdock <robert.f.murdock@gmail.com>
2020-07-23 12:52:49 -0400

    More force unpack removes. Ahhhh.

--- client/src/main/kotlin/com/zegreatrob/coupling/client/player/retired/RetiredPlayerQuery.kt
+++ client/src/main/kotlin/com/zegreatrob/coupling/client/player/retired/RetiredPlayerQuery.kt
@@ -1,5 +1,7 @@
 package com.zegreatrob.coupling.client.player.retired
 
+import com.zegreatrob.coupling.action.NotFoundResult
+import com.zegreatrob.coupling.action.Result
 import com.zegreatrob.coupling.action.SimpleSuspendResultAction
 import com.zegreatrob.coupling.action.successResult
 import com.zegreatrob.coupling.model.player.Player
@@ -11,18 +13,24 @@ import com.zegreatrob.coupling.repository.tribe.TribeIdGetSyntax
 import kotlinx.coroutines.async
 import kotlinx.coroutines.coroutineScope
 
+typealias RetiredPlayerData = Triple<Tribe, List<Player>, Player>
+
 data class RetiredPlayerQuery(val tribeId: TribeId, val playerId: String) :
-    SimpleSuspendResultAction<RetiredPlayerQueryDispatcher, Triple<Tribe?, List<Player>, Player>> {
+    SimpleSuspendResultAction<RetiredPlayerQueryDispatcher, RetiredPlayerData> {
     override val performFunc = link(RetiredPlayerQueryDispatcher::perform)
 }
 
 interface RetiredPlayerQueryDispatcher : TribeIdGetSyntax, TribeIdRetiredPlayersSyntax {
-    suspend fun perform(query: RetiredPlayerQuery) = query.getData().successResult()
+    suspend fun perform(query: RetiredPlayerQuery): Result<RetiredPlayerData> = query.getData()
+        ?.successResult()
+        ?: NotFoundResult("Tribe")
 
-    private suspend fun RetiredPlayerQuery.getData() = tribeId.getData()
-        .let { (tribe, players) ->
+    private suspend fun RetiredPlayerQuery.getData() = tribeId.getData().let { (tribe, players) ->
+        if (tribe == null)
+            null
+        else
             Triple(tribe, players, players.first { it.id == playerId })
-        }
+    }
 
     private suspend fun TribeId.getData() = coroutineScope {
         await(async { get() }, async { loadRetiredPlayers() })
