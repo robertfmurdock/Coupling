36ea44c0718568375826bff3c1ad2e818414b390
RoB Murdock <robert.f.murdock@gmail.com>
2020-07-24 12:35:50 -0400

    More refactoring to existing components.

--- client/src/main/kotlin/com/zegreatrob/coupling/client/CommandFunc.kt
+++ client/src/main/kotlin/com/zegreatrob/coupling/client/CommandFunc.kt
@@ -4,7 +4,6 @@ import com.zegreatrob.coupling.action.ErrorResult
 import com.zegreatrob.coupling.action.Result
 import com.zegreatrob.coupling.action.SuspendResultAction
 import com.zegreatrob.testmints.action.async.SuspendActionExecuteSyntax
-import com.zegreatrob.testmints.action.async.execute
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 interface DispatchFunc<D> {
@@ -16,22 +15,24 @@ interface DispatchFunc<D> {
 
 class DecoratedDispatchFunc<D : SuspendActionExecuteSyntax>(
     val dispatcherFunc: () -> D,
-    private val bridge: DataLoadComponentTools
+    private val tools: DataLoadComponentTools
 ) : DispatchFunc<D> {
+
+    private val dispatcher get() = dispatcherFunc()
 
     @ExperimentalCoroutinesApi
     override fun <C : SuspendResultAction<D, R>, R> invoke(commandFunc: () -> C, response: (Result<R>) -> Unit) =
         fun() {
-            launchExecute(dispatcherFunc(), commandFunc(), response)
+            val command = commandFunc()
+            dispatcher.asyncExecute(command, response)
         }
 
     @ExperimentalCoroutinesApi
-    private fun <C : SuspendResultAction<D, R>, R> launchExecute(
-        dispatcher: D,
+    private fun <C : SuspendResultAction<D, R>, R> D.asyncExecute(
         command: C,
         onResponse: (Result<R>) -> Unit
-    ) = bridge.performSuspendWork(
-        { dispatcher.execute(command) },
+    ) = tools.performAsyncWork(
+        { execute(command) },
         { handler: Throwable -> makeErrorResult(command, handler) },
         onResponse
     )
