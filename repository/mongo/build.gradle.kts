c0e790b2ca04c54059b790639978bc85f4f15141
CircleCI in a RoB Suit <robert.f.murdock@gmail.com>
2020-09-08 17:13:48 +0000

    Automatic gradle dependency update.

--- repository/mongo/build.gradle.kts
+++ repository/mongo/build.gradle.kts
@@ -28,9 +28,9 @@ kotlin {
             dependencies {
                 api(project(":repository:validation"))
                 api(project(":stub-model"))
-                implementation("com.zegreatrob.testmints:standard:3.1.7")
-                implementation("com.zegreatrob.testmints:minassert:3.1.7")
-                implementation("com.zegreatrob.testmints:async:3.1.7")
+                implementation("com.zegreatrob.testmints:standard:3.1.8")
+                implementation("com.zegreatrob.testmints:minassert:3.1.8")
+                implementation("com.zegreatrob.testmints:async:3.1.8")
                 implementation("org.jetbrains.kotlin:kotlin-test-common")
                 implementation("org.jetbrains.kotlin:kotlin-test-annotations-common")
                 implementation("org.jetbrains.kotlin:kotlin-test-js")
