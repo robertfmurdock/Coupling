999e8ebb9c30970264c5dd2ca432f97439ec55b3
RoB Murdock <robert.f.murdock@gmail.com>
2020-09-08 08:19:27 -0400

    Manual library updates

--- action/build.gradle.kts
+++ action/build.gradle.kts
@@ -20,8 +20,8 @@ kotlin {
             dependencies {
                 implementation(project(":model"))
                 implementation(project(":logging"))
-                implementation("com.zegreatrob.testmints:action:3.1.5")
-                implementation("com.zegreatrob.testmints:action-async:3.1.5")
+                implementation("com.zegreatrob.testmints:action:3.1.7")
+                implementation("com.zegreatrob.testmints:action-async:3.1.7")
                 implementation("com.benasher44:uuid:0.2.2")
                 implementation("com.soywiz.korlibs.klock:klock:1.12.0")
                 implementation("org.jetbrains.kotlin:kotlin-stdlib-common:${BuildConstants.kotlinVersion}")
