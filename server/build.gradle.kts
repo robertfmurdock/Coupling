81cee637772a7e9cc4b131efbc12bdb0c5284073
CircleCI in a RoB Suit <robert.f.murdock@gmail.com>
2020-09-22 17:14:32 +0000

    Automatic gradle dependency update.

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -32,7 +32,7 @@ dependencies {
     implementation(project("server_action"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.9")
     implementation("com.soywiz.korlibs.klock:klock:1.12.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.0.0-RC")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.0.0-RC2")
     implementation("com.benasher44:uuid:0.2.2")
 
     packageJson.dependencies().forEach {
