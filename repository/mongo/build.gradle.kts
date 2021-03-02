75ed456bf6a86dc19273f8052cf0fe4a67492ef7
CircleCI in a RoB Suit <robert.f.murdock@gmail.com>
2021-03-02 17:20:37 +0000

    Automatic gradle dependency update.

--- repository/mongo/build.gradle.kts
+++ repository/mongo/build.gradle.kts
@@ -28,9 +28,9 @@ kotlin {
             dependencies {
                 api(project(":repository:validation"))
                 api(project(":stub-model"))
-                implementation("com.zegreatrob.testmints:standard:3.3.1")
-                implementation("com.zegreatrob.testmints:minassert:3.3.1")
-                implementation("com.zegreatrob.testmints:async:3.3.1")
+                implementation("com.zegreatrob.testmints:standard:3.3.2")
+                implementation("com.zegreatrob.testmints:minassert:3.3.2")
+                implementation("com.zegreatrob.testmints:async:3.3.2")
                 implementation("org.jetbrains.kotlin:kotlin-test-common")
                 implementation("org.jetbrains.kotlin:kotlin-test-annotations-common")
                 implementation("org.jetbrains.kotlin:kotlin-test-js")
--- test-logging/build.gradle.kts
+++ test-logging/build.gradle.kts
@@ -17,8 +17,8 @@ kotlin {
         getByName("commonMain") {
             dependencies {
                 implementation(project(":logging"))
-                implementation("com.zegreatrob.testmints:standard:3.3.1")
-                implementation("com.zegreatrob.testmints:report:3.3.1")
+                implementation("com.zegreatrob.testmints:standard:3.3.2")
+                implementation("com.zegreatrob.testmints:report:3.3.2")
                 implementation("org.jetbrains.kotlin:kotlin-stdlib-common:${BuildConstants.kotlinVersion}")
                 implementation("io.github.microutils:kotlin-logging:2.0.4")
                 implementation("com.soywiz.korlibs.klock:klock:2.0.6")
