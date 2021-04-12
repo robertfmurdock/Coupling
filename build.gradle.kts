712c71ff8994beef02a57a4d620288a63ef0bc6e
CircleCI in a RoB Suit <robert.f.murdock@gmail.com>
2021-04-12 17:22:10 +0000

    Automatic gradle dependency update.

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ import org.jetbrains.kotlin.gradle.targets.js.testing.KotlinJsTest
 plugins {
     id("com.github.node-gradle.node") apply false
     id("com.bmuschko.docker-remote-api") version "6.7.0"
-    id("se.patrikerdes.use-latest-versions") version "0.2.15"
+    id("se.patrikerdes.use-latest-versions") version "0.2.16"
     id("com.github.ben-manes.versions") version "0.38.0"
     id("net.rdrei.android.buildtimetracker") version "0.11.0"
 }
