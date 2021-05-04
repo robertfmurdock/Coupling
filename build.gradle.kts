731fc6eeafc0887bd5785d584f44c47439a2405e
RoB Murdock <robert.f.murdock@gmail.com>
2021-05-03 21:19:52 -0400

    going back to a working agent, but troubled.

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ import org.jetbrains.kotlin.gradle.targets.js.testing.KotlinJsTest
 
 plugins {
     id("com.github.node-gradle.node") apply false
-    id("com.bmuschko.docker-remote-api") version "7.0.0"
+    id("com.bmuschko.docker-remote-api") version "6.7.0"
     id("se.patrikerdes.use-latest-versions") version "0.2.16"
     id("com.github.ben-manes.versions") version "0.38.0"
     id("net.rdrei.android.buildtimetracker") version "0.11.0"
