f64e3c5afbd6574ba75e012c47937577746632c2
RoB Murdock <robert.f.murdock@gmail.com>
2021-05-23 00:20:06 -0400

    removing prefix free. we'll see if i regret this

--- client/src/main/javascript/com/zegreatrob/coupling/client/app.js
+++ client/src/main/javascript/com/zegreatrob/coupling/client/app.js
@@ -1,3 +1,1 @@
 __webpack_public_path__ = window.webpackPublicPath;
-
-console.log("app js yooooooo", window.webpackPublicPath)
--- client/src/main/kotlin/com/zegreatrob/coupling/client/Main.kt
+++ client/src/main/kotlin/com/zegreatrob/coupling/client/Main.kt
@@ -4,11 +4,8 @@ import kotlin.js.Json
 import kotlin.js.json
 
 fun main() {
-    console.log("__webpack_public_path__ is", js("__webpack_public_path__"))
-
     if (isTestRun())
         return
-    js("require('prefixfree')")
     js("require('com/zegreatrob/coupling/client/animations.css')")
     js("require('@fortawesome/fontawesome-free/css/all.css')")
 
