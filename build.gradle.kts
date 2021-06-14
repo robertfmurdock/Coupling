4439b856f0611619dcee37fd618c589ccf53806b
RoB Murdock <robert.f.murdock@gmail.com>
2021-06-14 13:53:48 -0400

    updating the filter for release candidate + milestone libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -57,7 +57,7 @@ allprojects {
             reportfileName = "report"
             revision = "release"
             rejectVersionIf {
-                "^[0-9.]+-M[0-9](-r)?\$"
+                "^[0-9.]+[0-9](-RC|-M[0-9]+)\$"
                     .toRegex()
                     .matches(candidate.version)
             }
