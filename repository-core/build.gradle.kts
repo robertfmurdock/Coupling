845bc504c03a413e624c8f8571d4a06f06919334
RoB Murdock <robert.f.murdock@gmail.com>
2021-07-07 15:33:30 -0400

    upgradle

--- repository-core/build.gradle.kts
+++ repository-core/build.gradle.kts
@@ -24,7 +24,7 @@ kotlin {
             dependencies {
                 implementation(project(":test-logging"))
                 implementation("com.zegreatrob.testmints:standard:4.1.2")
-                implementation("com.zegreatrob.testmints:minassert:4.1.2")
+                implementation("com.zegreatrob.testmints:minassert:4.1.6")
                 implementation("org.jetbrains.kotlin:kotlin-test")
             }
         }
