e5938a012d294172fb561d5cc1a6695ec8c57776
RoB Murdock <robert.f.murdock@gmail.com>
2021-07-14 18:08:54 -0400

    More serialization migration

--- server/src/main/kotlin/com/zegreatrob/coupling/server/entity/pairassignment/PairAssignmentListResolver.kt
+++ server/src/main/kotlin/com/zegreatrob/coupling/server/entity/pairassignment/PairAssignmentListResolver.kt
@@ -1,18 +1,17 @@
 package com.zegreatrob.coupling.server.entity.pairassignment
 
-import com.zegreatrob.coupling.json.toJson
+import com.zegreatrob.coupling.json.couplingJsonFormat
+import com.zegreatrob.coupling.json.toSerializable
 import com.zegreatrob.coupling.model.TribeRecord
 import com.zegreatrob.coupling.model.pairassignmentdocument.PairAssignmentDocument
 import com.zegreatrob.coupling.server.action.pairassignmentdocument.PairAssignmentDocumentListQuery
 import com.zegreatrob.coupling.server.graphql.DispatcherProviders.tribeCommand
 import com.zegreatrob.coupling.server.graphql.dispatch
+import kotlinx.serialization.json.encodeToDynamic
+import kotlin.js.Json
 
-val pairAssignmentListResolve = dispatch(
-    tribeCommand,
-    { _,_ -> PairAssignmentDocumentListQuery },
-    ::toJson
-)
+val pairAssignmentListResolve = dispatch(tribeCommand, { _, _ -> PairAssignmentDocumentListQuery }, ::toJson)
 
-private fun toJson(result: List<TribeRecord<PairAssignmentDocument>>) = result
-    .map(TribeRecord<PairAssignmentDocument>::toJson)
-    .toTypedArray()
+private fun toJson(result: List<TribeRecord<PairAssignmentDocument>>) = couplingJsonFormat.encodeToDynamic(
+    result.map(TribeRecord<PairAssignmentDocument>::toSerializable)
+).unsafeCast<Array<Json>>()
--- server/src/main/kotlin/com/zegreatrob/coupling/server/entity/player/RetiredPlayerListResolver.kt
+++ server/src/main/kotlin/com/zegreatrob/coupling/server/entity/player/RetiredPlayerListResolver.kt
@@ -1,15 +1,16 @@
 package com.zegreatrob.coupling.server.entity.player
 
-import com.zegreatrob.coupling.json.toJson
+import com.zegreatrob.coupling.json.couplingJsonFormat
+import com.zegreatrob.coupling.json.toSerializable
 import com.zegreatrob.coupling.model.TribeRecord
 import com.zegreatrob.coupling.model.player.Player
-import com.zegreatrob.coupling.model.player.player
 import com.zegreatrob.coupling.server.action.player.RetiredPlayersQuery
 import com.zegreatrob.coupling.server.graphql.DispatcherProviders.tribeCommand
 import com.zegreatrob.coupling.server.graphql.dispatch
+import kotlinx.serialization.json.encodeToDynamic
 
 val retiredPlayerListResolve = dispatch(tribeCommand, { _, _ -> RetiredPlayersQuery }, ::toJsonArray)
 
-private fun toJsonArray(list: List<TribeRecord<Player>>) = list.map {
-    it.toJson().add(it.data.player.toJson())
-}.toTypedArray()
+private fun toJsonArray(list: List<TribeRecord<Player>>) = couplingJsonFormat.encodeToDynamic(
+    list.map(TribeRecord<Player>::toSerializable)
+)
