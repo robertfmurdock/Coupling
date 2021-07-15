5e0161cdfa17848a159086e5ba5bb4b341fced9a
RoB Murdock <robert.f.murdock@gmail.com>
2021-07-15 18:10:52 -0400

    starting refactor toward serialization middleware in sdk

--- sdk/src/jsMain/kotlin/com/zegreatrob/coupling/sdk/SdkPairAssignmentDocumentGetCurrent.kt
+++ sdk/src/jsMain/kotlin/com/zegreatrob/coupling/sdk/SdkPairAssignmentDocumentGetCurrent.kt
@@ -1,15 +1,20 @@
 package com.zegreatrob.coupling.sdk
 
-import com.zegreatrob.coupling.json.toPairAssignmentDocumentRecord
+import com.zegreatrob.coupling.json.JsonPairAssignmentDocumentRecord
+import com.zegreatrob.coupling.json.couplingJsonFormat
+import com.zegreatrob.coupling.json.toModel
 import com.zegreatrob.coupling.model.TribeRecord
 import com.zegreatrob.coupling.model.pairassignmentdocument.PairAssignmentDocument
 import com.zegreatrob.coupling.model.tribe.TribeId
 import com.zegreatrob.coupling.repository.pairassignmentdocument.PairAssignmentDocumentGetCurrent
-import kotlin.js.Json
+import kotlinx.serialization.json.decodeFromDynamic
 
 interface SdkPairAssignmentDocumentGetCurrent : PairAssignmentDocumentGetCurrent, GqlQueryComponent {
     override suspend fun getCurrentPairAssignments(tribeId: TribeId): TribeRecord<PairAssignmentDocument>? =
         performQueryGetComponent(tribeId, TribeGQLComponent.CurrentPairAssignmentDocument) {
-            it.unsafeCast<Json?>()?.toPairAssignmentDocumentRecord()
-        }
+            if (it == null)
+                null
+            else
+                couplingJsonFormat.decodeFromDynamic<JsonPairAssignmentDocumentRecord>(it)
+        }?.toModel()
 }
