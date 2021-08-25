86d3b7eb403f5d2b3e59b6b6937fad63d77cd355
RoB Murdock <robert.f.murdock@gmail.com>
2021-08-25 09:35:39 -0400

    updating to kotlin 1.5.30

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
     id("net.researchgate.release") version "2.6.0"
 }
 
-val kotlinVersion = "1.5.21"
+val kotlinVersion = "1.5.30"
 
 dependencies {
     implementation(kotlin("stdlib", kotlinVersion))
