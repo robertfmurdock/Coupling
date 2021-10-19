be6a536160e0ed4b7da147eb24b7aee65ccd5633
CircleCI in a RoB Suit <robert.f.murdock@gmail.com>
2021-10-19 17:27:57 +0000

    Automatic gradle dependency update.

--- model/build.gradle.kts
+++ model/build.gradle.kts
@@ -26,8 +26,8 @@ kotlin {
             dependencies {
                 implementation(project(":test-logging"))
                 implementation(kotlin("test", BuildConstants.kotlinVersion))
-                implementation("com.zegreatrob.testmints:standard:5.0.9")
-                implementation("com.zegreatrob.testmints:minassert:5.0.9")
+                implementation("com.zegreatrob.testmints:standard:5.1.0")
+                implementation("com.zegreatrob.testmints:minassert:5.1.0")
             }
         }
 
--- test-logging/build.gradle.kts
+++ test-logging/build.gradle.kts
@@ -19,8 +19,8 @@ kotlin {
         getByName("commonMain") {
             dependencies {
                 implementation(project(":logging"))
-                implementation("com.zegreatrob.testmints:standard:5.0.9")
-                implementation("com.zegreatrob.testmints:report:5.0.9")
+                implementation("com.zegreatrob.testmints:standard:5.1.0")
+                implementation("com.zegreatrob.testmints:report:5.1.0")
                 implementation("org.jetbrains.kotlin:kotlin-stdlib-common:${BuildConstants.kotlinVersion}")
                 implementation("io.github.microutils:kotlin-logging:2.0.11")
                 implementation("com.soywiz.korlibs.klock:klock:2.1.0")
