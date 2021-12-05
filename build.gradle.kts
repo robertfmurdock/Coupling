382ac6ef50a94cbdddd821d80ec9deccff6b0479
RoB Murdock <robert.f.murdock@gmail.com>
2021-12-05 13:24:42 -0500

    Removing remaining contents of buildSrc

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,10 +7,7 @@ import org.jetbrains.kotlin.gradle.plugin.ProjectLocalConfigurations
 import org.jetbrains.kotlin.gradle.targets.js.KotlinJsCompilerAttribute
 
 plugins {
-    id("com.github.node-gradle.node") apply false
     id("com.bmuschko.docker-remote-api") version "7.1.0"
-    id("se.patrikerdes.use-latest-versions") version "0.2.18"
-    id("com.github.ben-manes.versions") version "0.39.0"
     id("net.rdrei.android.buildtimetracker") version "0.11.0"
     id("de.gliderpilot.semantic-release") version "1.4.2"
     id("com.avast.gradle.docker-compose") version "0.14.11"
