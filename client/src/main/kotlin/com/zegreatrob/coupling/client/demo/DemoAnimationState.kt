5a992b9ec5f765c153a29d71deb11b7d9e4be16a
RoB Murdock <robert.f.murdock@gmail.com>
2021-12-08 21:36:05 -0500

    Adding a few more frames

--- client/src/main/kotlin/com/zegreatrob/coupling/client/demo/DemoAnimationState.kt
+++ client/src/main/kotlin/com/zegreatrob/coupling/client/demo/DemoAnimationState.kt
@@ -2,11 +2,12 @@ package com.zegreatrob.coupling.client.demo
 
 import com.benasher44.uuid.uuid4
 import com.zegreatrob.coupling.client.Frame
+import com.zegreatrob.coupling.model.pin.Pin
 import com.zegreatrob.coupling.model.player.Player
 import com.zegreatrob.coupling.model.tribe.Tribe
 import com.zegreatrob.coupling.model.tribe.TribeId
 
-private val demoTribe = Tribe(id = TribeId("${uuid4()}"), name = "Your team name here")
+private val demoTribe = Tribe(id = TribeId("${uuid4()}"), name = "The Simpsons")
 
 private val player1 = Player(name = "Homer")
 private val player2 = Player(name = "Marge")
@@ -24,6 +25,10 @@ private val players = listOf(
     player6,
 )
 
+private val pins = listOf(
+    Pin(name = "watchman", icon = "eye")
+)
+
 sealed class DemoAnimationState {
 
     companion object {
@@ -34,7 +39,9 @@ sealed class DemoAnimationState {
 
         private fun listOfPairs() = listOf(Pair(ShowIntro, 3000)) +
                 makeTribeSequence().map { it to 100 } +
-                makePlayerSequence().map { it to 100 }
+                makePlayerSequence().map { it to 100 } +
+                makePinSequence().map { it to 100 } +
+                (CurrentPairs(demoTribe, players, pins) to 100)
     }
 }
 
@@ -50,10 +57,21 @@ fun makePlayerSequence() = players.flatMapIndexed { playerIndex, player ->
     player.name.rangeOfStringLength().map { index ->
         AddPlayer(demoTribe, player.copy(name = player.name.substring(0, index)), players.subList(0, playerIndex))
     }
-}
+} + AddPlayer(demoTribe, players.last(), players)
+
+
+fun makePinSequence() = pins.flatMapIndexed { pinIndex, pin ->
+    pin.name.rangeOfStringLength().map { index ->
+        AddPin(demoTribe, pin.copy(name = pin.name.substring(0, index)), pins.subList(0, pinIndex))
+    }
+} + AddPin(demoTribe, pins.last(), pins)
 
 private fun String?.rangeOfStringLength() = (0..(this ?: "").length)
 
 data class MakeTribe(val tribe: Tribe) : DemoAnimationState()
 
 data class AddPlayer(val tribe: Tribe, val newPlayer: Player, val players: List<Player>) : DemoAnimationState()
+
+data class AddPin(val tribe: Tribe, val newPin: Pin, val pins: List<Pin>) : DemoAnimationState()
+
+data class CurrentPairs(val tribe: Tribe, val players: List<Player>, val pins: List<Pin>) : DemoAnimationState()
--- client/src/main/kotlin/com/zegreatrob/coupling/client/demo/DemoPage.kt
+++ client/src/main/kotlin/com/zegreatrob/coupling/client/demo/DemoPage.kt
@@ -1,7 +1,13 @@
 package com.zegreatrob.coupling.client.demo
 
+import com.zegreatrob.coupling.client.Controls
 import com.zegreatrob.coupling.client.DispatchFunc
 import com.zegreatrob.coupling.client.frameRunner
+import com.zegreatrob.coupling.client.pairassignments.list.DeletePairAssignmentsCommandDispatcher
+import com.zegreatrob.coupling.client.pairassignments.pairAssignments
+import com.zegreatrob.coupling.client.pin.PinCommandDispatcher
+import com.zegreatrob.coupling.client.pin.PinConfig
+import com.zegreatrob.coupling.client.pin.PinConfigProps
 import com.zegreatrob.coupling.client.player.PlayerConfig
 import com.zegreatrob.coupling.client.player.PlayerConfigDispatcher
 import com.zegreatrob.coupling.client.player.PlayerConfigProps
@@ -9,12 +15,14 @@ import com.zegreatrob.coupling.client.routing.PageProps
 import com.zegreatrob.coupling.client.tribe.TribeConfig
 import com.zegreatrob.coupling.client.tribe.TribeConfigDispatcher
 import com.zegreatrob.coupling.client.tribe.TribeConfigProps
+import com.zegreatrob.coupling.model.CouplingSocketMessage
 import com.zegreatrob.minreact.reactFunction
 import com.zegreatrob.testmints.action.async.SuspendAction
 import react.dom.div
 import react.key
 
-interface NoOpDispatcher : TribeConfigDispatcher, PlayerConfigDispatcher
+interface NoOpDispatcher : TribeConfigDispatcher, PlayerConfigDispatcher, PinCommandDispatcher,
+    DeletePairAssignmentsCommandDispatcher
 
 private val noOpDispatchFunc = object : DispatchFunc<NoOpDispatcher> {
     override fun <C : SuspendAction<NoOpDispatcher, R>, R> invoke(
@@ -23,22 +31,36 @@ private val noOpDispatchFunc = object : DispatchFunc<NoOpDispatcher> {
 }
 
 val DemoPage = reactFunction<PageProps> {
-    frameRunner(DemoAnimationState.generateSequence(), 1.0) { thing: DemoAnimationState ->
-        when (thing) {
+    frameRunner(DemoAnimationState.generateSequence(), 1.0) { state: DemoAnimationState ->
+        when (state) {
             Start -> div { +"Starting..." }
             ShowIntro -> div { +"Alright, here's an example of how you might use the app." }
             is MakeTribe -> div {
-                child(TribeConfig, TribeConfigProps(thing.tribe, noOpDispatchFunc)) {
-                    attrs.key = thing.tribe.toString()
-                }
+                child(TribeConfig, TribeConfigProps(state.tribe, noOpDispatchFunc)) { attrs.key = "$state" }
             }
             is AddPlayer -> div {
                 child(
                     PlayerConfig,
-                    PlayerConfigProps(thing.tribe, thing.newPlayer, thing.players, {}, noOpDispatchFunc)
+                    PlayerConfigProps(state.tribe, state.newPlayer, state.players, {}, noOpDispatchFunc)
                 ) {
-                    attrs.key = thing.newPlayer.toString()
+                    attrs.key = "$state"
                 }
+            }
+            is AddPin -> div {
+                child(PinConfig, PinConfigProps(state.tribe, state.newPin, state.pins, {}, noOpDispatchFunc)) {
+                    attrs.key = "$state"
+                }
+            }
+            is CurrentPairs -> div {
+                pairAssignments(
+                    state.tribe,
+                    state.players,
+                    null,
+                    {},
+                    Controls(noOpDispatchFunc) {},
+                    CouplingSocketMessage("", emptySet()),
+                    false
+                )
             }
         }
     }
