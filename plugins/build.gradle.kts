ed13f5b4577ec311284ae2ee0671600d964bafcc
RoB Murdock <robert.f.murdock@gmail.com>
2021-12-13 14:50:37 -0500

    tweaking compose file to work with new graylog images, in anticipation of using compose in CI

--- plugins/build.gradle.kts
+++ plugins/build.gradle.kts
@@ -21,7 +21,7 @@ dependencies {
     implementation("com.github.ben-manes:gradle-versions-plugin:0.39.0")
     implementation("se.patrikerdes:gradle-use-latest-versions-plugin:0.2.18")
     implementation("com.soywiz.korlibs.klock:klock:2.4.8")
-    implementation("org.apache.logging.log4j:log4j-core:2.15.0")
+    implementation("org.apache.logging.log4j:log4j-core:2.16.0")
     implementation("org.slf4j:slf4j-api:2.0.0-alpha5")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.13.0")
 }
