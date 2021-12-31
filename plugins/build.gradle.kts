5586bfa4de499737e5a518297224e5eea4daf6c9
CircleCI in a RoB Suit <robert.f.murdock@gmail.com>
2021-12-31 17:29:29 +0000

    Automatic gradle dependency update.

--- plugins/build.gradle.kts
+++ plugins/build.gradle.kts
@@ -19,7 +19,7 @@ dependencies {
     implementation(kotlin("stdlib", kotlinVersion))
     implementation(kotlin("gradle-plugin", kotlinVersion))
     implementation("org.jetbrains.kotlin.plugin.serialization:org.jetbrains.kotlin.plugin.serialization.gradle.plugin:1.6.10")
-    implementation("com.github.ben-manes:gradle-versions-plugin:0.39.0")
+    implementation("com.github.ben-manes:gradle-versions-plugin:0.40.0")
     implementation("se.patrikerdes:gradle-use-latest-versions-plugin:0.2.18")
     implementation("com.soywiz.korlibs.klock:klock:2.4.10")
     implementation("org.apache.logging.log4j:log4j-core:2.17.1")
