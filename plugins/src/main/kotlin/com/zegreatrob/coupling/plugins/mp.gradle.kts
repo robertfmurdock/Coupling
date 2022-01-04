d0592361cb0118b1117a7cf54716943257978a1b
CircleCI in a RoB Suit <robert.f.murdock@gmail.com>
2022-01-04 17:31:35 +0000

    Automatic gradle dependency update.

--- plugins/src/main/kotlin/com/zegreatrob/coupling/plugins/mp.gradle.kts
+++ plugins/src/main/kotlin/com/zegreatrob/coupling/plugins/mp.gradle.kts
@@ -8,8 +8,8 @@ plugins {
 }
 
 dependencies {
-    "commonMainImplementation"(enforcedPlatform("com.zegreatrob.testmints:testmints-bom:5.5.10"))
+    "commonMainImplementation"(enforcedPlatform("com.zegreatrob.testmints:testmints-bom:5.5.12"))
     "commonMainImplementation"(enforcedPlatform("org.jetbrains.kotlinx:kotlinx-serialization-bom:1.3.2"))
     "commonMainImplementation"(enforcedPlatform("org.jetbrains.kotlinx:kotlinx-coroutines-bom:1.6.0"))
-    "commonMainImplementation"(enforcedPlatform("org.jetbrains.kotlin-wrappers:kotlin-wrappers-bom:0.0.1-pre.285-kotlin-1.6.10"))
+    "commonMainImplementation"(enforcedPlatform("org.jetbrains.kotlin-wrappers:kotlin-wrappers-bom:0.0.1-pre.286-kotlin-1.6.10"))
 }
