539610e18e7b9b6f915855a68404d44744823a74
RoB Murdock <robert.f.murdock@gmail.com>
2022-01-24 23:55:19 -0500

    Alleviating performance problem when a single user uses massive number of tribes over time. really pretty much just relevant for testing

--- server_action/src/commonMain/kotlin/com/zegreatrob/coupling/server/action/connection/DeleteTribeCommand.kt
+++ server_action/src/commonMain/kotlin/com/zegreatrob/coupling/server/action/connection/DeleteTribeCommand.kt
@@ -1,13 +1,31 @@
 package com.zegreatrob.coupling.server.action.connection
 
 import com.zegreatrob.coupling.action.SimpleSuspendResultAction
+import com.zegreatrob.coupling.action.SuccessfulResult
 import com.zegreatrob.coupling.action.deletionResult
+import com.zegreatrob.coupling.model.user.AuthenticatedUserSyntax
+import com.zegreatrob.coupling.model.user.User
 import com.zegreatrob.coupling.repository.tribe.TribeIdDeleteSyntax
+import com.zegreatrob.coupling.server.action.user.UserSaveSyntax
 
 object DeleteTribeCommand : SimpleSuspendResultAction<DeleteTribeCommandDispatcher, Unit> {
     override val performFunc = link(DeleteTribeCommandDispatcher::perform)
 }
 
-interface DeleteTribeCommandDispatcher : TribeIdDeleteSyntax, CurrentTribeIdSyntax {
+interface DeleteTribeCommandDispatcher : TribeIdDeleteSyntax, CurrentTribeIdSyntax, AuthenticatedUserSyntax,
+    UserSaveSyntax {
     suspend fun perform(command: DeleteTribeCommand) = currentTribeId.delete().deletionResult("Tribe")
+        .also {
+            if (it is SuccessfulResult) {
+                user
+                    .copy(authorizedTribeIds = user.authorizedTribeIds.filter { id -> id != currentTribeId }.toSet())
+                    .saveIfChanged()
+            }
+        }
+
+    private suspend fun User.saveIfChanged() {
+        if (this != user) {
+            save()
+        }
+    }
 }
