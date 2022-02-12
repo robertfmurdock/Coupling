9fb57ee6a02bd99f67b00bfddc94ebb2ca3f2005
RoB Murdock <robert.f.murdock@gmail.com>
2022-02-11 23:12:28 -0500

    Going back to leaving the building of deployment artifacts in the server project

--- deploy/prod/build.gradle.kts
+++ deploy/prod/build.gradle.kts
@@ -1,3 +1,9 @@
 plugins {
     id("com.zegreatrob.coupling.plugins.deploy")
 }
+
+tasks {
+    "deploy" {
+        dependsOn(":server:serverlessBuild")
+    }
+}
--- plugins/src/main/kotlin/com/zegreatrob/coupling/plugins/deploy.gradle.kts
+++ plugins/src/main/kotlin/com/zegreatrob/coupling/plugins/deploy.gradle.kts
@@ -16,34 +16,10 @@ fun serverlessBuildDir(stage: String) = "${serverProject.buildDir.absolutePath}/
 val serverlessYmlPath = "${serverProject.projectDir.absolutePath}/serverless.yml"
 
 tasks {
-    val buildDeployBundle by creating(NodeExec::class) {
-        configureBuild(project.name)
-    }
     val deploy by creating(NodeExec::class) {
-        dependsOn(buildDeployBundle)
         configureDeploy(project.name)
     }
     findByPath(":release")!!.finalizedBy(deploy)
-}
-
-fun NodeExec.configureBuild(stage: String) {
-    dependsOn(":server:assemble", ":server:test", ":server:compileKotlinJs")
-    environment(
-        "SERVER_DIR" to serverProject.projectDir.absolutePath,
-        "AWS_ACCESS_KEY_ID" to (System.getenv("AWS_ACCESS_KEY_ID") ?: "fake"),
-        "AWS_SECRET_ACCESS_KEY" to (System.getenv("AWS_SECRET_ACCESS_KEY") ?: "fake"),
-        "CLIENT_URL" to "https://assets.zegreatrob.com/coupling/${project.version}",
-    )
-    nodeCommand = "serverless"
-    arguments = listOf(
-        "package",
-        "--config",
-        serverlessYmlPath,
-        "--package",
-        serverlessBuildDir(stage),
-        "--stage",
-        stage
-    )
 }
 
 fun NodeExec.configureDeploy(stage: String) {
