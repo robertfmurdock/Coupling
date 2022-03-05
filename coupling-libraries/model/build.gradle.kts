3afd41675209e35996d5f2140109563b4e3095ab
robertfmurdock <robertfmurdock@users.noreply.github.com>
2022-03-05 17:12:16 +0000

    [create-pull-request] Gradle Update

--- coupling-libraries/model/build.gradle.kts
+++ coupling-libraries/model/build.gradle.kts
@@ -16,7 +16,7 @@ kotlin {
             dependencies {
                 api(kotlin("stdlib"))
                 api(kotlin("stdlib-common"))
-                api("com.soywiz.korlibs.klock:klock:2.5.3")
+                api("com.soywiz.korlibs.klock:klock:2.6.1")
                 api("org.jetbrains.kotlinx:kotlinx-datetime:0.3.2")
                 implementation("com.benasher44:uuid:0.4.0")
             }
