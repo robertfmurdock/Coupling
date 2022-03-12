fe1fda1c6a5e9d917379f06e25a1128c1510b2d6
robertfmurdock <robertfmurdock@users.noreply.github.com>
2022-03-12 17:13:09 +0000

    [create-pull-request] Gradle Update

--- plugins/src/main/kotlin/com/zegreatrob/coupling/plugins/jstools.gradle.kts
+++ plugins/src/main/kotlin/com/zegreatrob/coupling/plugins/jstools.gradle.kts
@@ -24,7 +24,7 @@ dependencies {
     toolsExtension.packageJson.devDependencies()?.forEach {
         testImplementation(npm(it.first, it.second.asText()))
     }
-    implementation(enforcedPlatform("org.jetbrains.kotlin-wrappers:kotlin-wrappers-bom:0.0.1-pre.318-kotlin-1.6.10"))
+    implementation(enforcedPlatform("org.jetbrains.kotlin-wrappers:kotlin-wrappers-bom:0.0.1-pre.320-kotlin-1.6.10"))
     implementation(enforcedPlatform("com.zegreatrob.testmints:testmints-bom:6.0.9"))
     implementation(enforcedPlatform("org.jetbrains.kotlinx:kotlinx-serialization-bom:1.3.2"))
     implementation(enforcedPlatform("org.jetbrains.kotlinx:kotlinx-coroutines-bom:1.6.0"))
