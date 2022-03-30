6f927713d140d61b25181427730da706781fee5a
robertfmurdock <robertfmurdock@users.noreply.github.com>
2022-03-30 17:14:07 +0000

    [create-pull-request] Gradle Update

--- plugins/src/main/kotlin/com/zegreatrob/coupling/plugins/mp.gradle.kts
+++ plugins/src/main/kotlin/com/zegreatrob/coupling/plugins/mp.gradle.kts
@@ -8,8 +8,8 @@ plugins {
 }
 
 dependencies {
-    "commonMainImplementation"(enforcedPlatform("com.zegreatrob.testmints:testmints-bom:7.0.1"))
-    "commonMainImplementation"(enforcedPlatform("com.zegreatrob.jsmints:jsmints-bom:1.0.47"))
+    "commonMainImplementation"(enforcedPlatform("com.zegreatrob.testmints:testmints-bom:7.1.0"))
+    "commonMainImplementation"(enforcedPlatform("com.zegreatrob.jsmints:jsmints-bom:1.0.48"))
     "commonMainImplementation"(enforcedPlatform("org.jetbrains.kotlinx:kotlinx-serialization-bom:1.3.2"))
     "commonMainImplementation"(enforcedPlatform("org.jetbrains.kotlinx:kotlinx-coroutines-bom:1.6.0"))
     "commonMainImplementation"(enforcedPlatform("org.jetbrains.kotlin-wrappers:kotlin-wrappers-bom:0.0.1-pre.323-kotlin-1.6.10"))
