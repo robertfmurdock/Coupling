118f88dd510b1df549bccd7d0ca11b965dea85b7
RoB Murdock <robert.f.murdock@gmail.com>
2022-04-24 11:36:57 -0400

    interesting!

--- plugins/src/main/kotlin/com/zegreatrob/coupling/plugins/tagger.gradle.kts
+++ plugins/src/main/kotlin/com/zegreatrob/coupling/plugins/tagger.gradle.kts
@@ -1,21 +1,42 @@
 package com.zegreatrob.coupling.plugins
 
-import org.ajoberstar.grgit.gradle.GrgitService
+import org.ajoberstar.grgit.gradle.GrgitServiceExtension
 
 plugins {
     id("org.ajoberstar.grgit.service")
+    base
 }
 
 tasks {
     val describe by registering(DescribeTask::class) {
-
+        this.extension = grgitService
+    }
+    val check by getting {
+        dependsOn(describe)
     }
 }
 
-class DescribeTask @Inject constructor(@Internal val service: Property<GrgitService>) : DefaultTask() {
+open class DescribeTask : DefaultTask() {
+
+    @Input
+    var extension: GrgitServiceExtension? = null
 
     @TaskAction
     fun execute() {
-        println(service.get().grgit.describe())
+        val grgit = extension!!.service.get().grgit
+        val description = grgit.describe {}
+        println(description)
+
+        val (previousVersionNumber, commitDistanceFromTag, dirty) = description.split("-")
+
+        println(previousVersionNumber)
+
+        val (major, minor, patch) = previousVersionNumber.substring(1).split(".")
+
+        val nextVersion = "v${major}.${minor}.${patch.toInt() + 1}"
+        println("prospective next version number $nextVersion")
+
+        val isClean = dirty.isBlank()
+        println("is clean $isClean")
     }
 }
