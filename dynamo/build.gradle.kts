2e66de09d03af8866837b2c3ae5d3f7a40b50e9f
RoB Murdock <robert.f.murdock@gmail.com>
2022-07-01 19:07:36 -0400

    making dynamo a js only module, and giving it a package json

--- dynamo/build.gradle.kts
+++ dynamo/build.gradle.kts
@@ -1,60 +1,22 @@
 plugins {
-    id("com.zegreatrob.coupling.plugins.mp")
+    id("com.zegreatrob.coupling.plugins.jstools")
     id("com.avast.gradle.docker-compose")
 }
 
 kotlin {
-    targets {
-        js {
-            useCommonJs()
-            nodejs {
-                testTask {
-                    useMocha {
-                        timeout = "20s"
-                    }
+    js {
+        nodejs {
+            testTask {
+                useMocha {
+                    timeout = "20s"
                 }
-            }
-        }
-    }
-
-    sourceSets {
-        getByName("commonMain") {
-            dependencies {
-                api(project(":coupling-libraries:model"))
-                api(project(":coupling-libraries:repository-core"))
-                implementation("org.jetbrains.kotlin:kotlin-stdlib-js")
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
-                api("org.jetbrains.kotlin-wrappers:kotlin-extensions")
-                implementation("com.soywiz.korlibs.klock:klock:2.7.0")
-                implementation("com.benasher44:uuid:0.4.1")
-                implementation("io.github.microutils:kotlin-logging:2.1.23")
-            }
-        }
-        getByName("commonTest") {
-            dependencies {
-                api(project(":coupling-libraries:repository-validation"))
-                api(project(":coupling-libraries:stub-model"))
-                implementation("com.zegreatrob.testmints:standard")
-                implementation("com.zegreatrob.testmints:minassert")
-                implementation("com.zegreatrob.testmints:async")
-                implementation("org.jetbrains.kotlin:kotlin-test-common")
-                implementation("org.jetbrains.kotlin:kotlin-test-annotations-common")
-                implementation("org.jetbrains.kotlin:kotlin-test-js")
-            }
-        }
-
-        val jsMain by getting {
-            dependencies {
-//                api(project(":coupling-libraries:logging"))
-//                implementation(npm("@aws-sdk/client-dynamodb", "3.18.0"))
-//                implementation(npm("@aws-sdk/lib-dynamodb", "3.18.0"))
             }
         }
     }
 }
 
 tasks {
-    named("jsNodeTest") {
+    named("nodeTest") {
         dependsOn("composeUp")
     }
 }
@@ -64,4 +26,25 @@ dockerCompose {
     setProjectName("Coupling-root")
     startedServices.set(listOf("dynamo"))
     containerLogToDir.set(project.file("build/test-output/containers-logs"))
+}
+
+dependencies {
+    api(project(":coupling-libraries:model"))
+    api(project(":coupling-libraries:repository-core"))
+    api(project(":coupling-libraries:logging"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-js")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
+    api("org.jetbrains.kotlin-wrappers:kotlin-extensions")
+    implementation("com.soywiz.korlibs.klock:klock:2.7.0")
+    implementation("com.benasher44:uuid:0.4.1")
+    implementation("io.github.microutils:kotlin-logging:2.1.23")
+
+    testImplementation(project(":coupling-libraries:repository-validation"))
+    testImplementation(project(":coupling-libraries:stub-model"))
+    testImplementation("com.zegreatrob.testmints:standard")
+    testImplementation("com.zegreatrob.testmints:minassert")
+    testImplementation("com.zegreatrob.testmints:async")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-common")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-annotations-common")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-js")
 }
