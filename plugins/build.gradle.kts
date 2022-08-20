8c0fe91c44f35df2dcf9b4b590795c9d35910cc1
robertfmurdock <robertfmurdock@users.noreply.github.com>
2022-08-20 10:11:55 +0000

    [create-pull-request] Gradle Plugin Update

--- plugins/build.gradle.kts
+++ plugins/build.gradle.kts
@@ -20,7 +20,7 @@ dependencies {
     implementation("org.jetbrains.kotlin.plugin.serialization:org.jetbrains.kotlin.plugin.serialization.gradle.plugin:1.7.10")
     implementation("com.github.ben-manes:gradle-versions-plugin:0.42.0")
     implementation("se.patrikerdes:gradle-use-latest-versions-plugin:0.2.18")
-    implementation("com.zegreatrob.jsmints.plugins.jspackage:com.zegreatrob.jsmints.plugins.jspackage.gradle.plugin:1.6.25")
+    implementation("com.zegreatrob.jsmints.plugins.jspackage:com.zegreatrob.jsmints.plugins.jspackage.gradle.plugin:1.6.26")
     implementation("com.soywiz.korlibs.klock:klock:3.0.0")
     implementation("org.apache.logging.log4j:log4j-core:2.18.0")
     implementation("org.apache.logging.log4j:log4j-iostreams:2.18.0")
