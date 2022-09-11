268f57c17df1d9bab02d1b802062600432ec1dcb
RoB Murdock <robert.f.murdock@gmail.com>
2022-09-11 15:25:36 -0400

    converting cdn lookup to also use the npmconstrained style and the jstools kit

--- coupling-libraries/cdnLookup/build.gradle.kts
+++ coupling-libraries/cdnLookup/build.gradle.kts
@@ -2,42 +2,34 @@ import org.jetbrains.kotlin.gradle.targets.js.ir.KotlinJsIrLink
 import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsExec
 
 plugins {
-    id("com.zegreatrob.coupling.plugins.mp")
+    id("com.zegreatrob.coupling.plugins.jstools")
     kotlin("plugin.serialization")
 }
 
 kotlin {
-    targets {
-        js {
-            nodejs {
-                binaries.executable()
-                testTask { useMocha { timeout = "400s" } }
-            }
+    js {
+        nodejs {
+            binaries.executable()
+            testTask { useMocha { timeout = "400s" } }
         }
     }
-    sourceSets {
-        getByName("commonMain") {
-            dependencies {
-                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json")
-                implementation("io.ktor:ktor-client-core")
-                implementation("io.ktor:ktor-client-content-negotiation")
-                implementation("io.ktor:ktor-serialization-kotlinx-json")
-                implementation("io.ktor:ktor-client-logging")
-                implementation("io.ktor:ktor-client-encoding")
-                implementation(npm("resolve-pkg", "^1.0.0"))
-                implementation(npm("read-pkg-up", "^4.0.0"))
-            }
-        }
-        getByName("commonTest") {
-            dependencies {
-                implementation(kotlin("test"))
-                implementation(npm("react", "18.1.0"))
-                implementation("com.zegreatrob.testmints:standard")
-                implementation("com.zegreatrob.testmints:async")
-                implementation("com.zegreatrob.testmints:minassert")
-            }
-        }
-    }
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json")
+    implementation("io.ktor:ktor-client-core")
+    implementation("io.ktor:ktor-client-content-negotiation")
+    implementation("io.ktor:ktor-serialization-kotlinx-json")
+    implementation("io.ktor:ktor-client-logging")
+    implementation("io.ktor:ktor-client-encoding")
+    implementation(npmConstrained("resolve-pkg"))
+    implementation(npmConstrained("read-pkg-up"))
+
+    testImplementation(kotlin("test"))
+    testImplementation(npmConstrained("react"))
+    testImplementation("com.zegreatrob.testmints:standard")
+    testImplementation("com.zegreatrob.testmints:async")
+    testImplementation("com.zegreatrob.testmints:minassert")
 }
 
 val cdnLookupConfiguration: Configuration by configurations.creating
@@ -45,7 +37,7 @@ val cdnLookupConfiguration: Configuration by configurations.creating
 val outputFile: String? by project
 
 tasks {
-    named("jsNodeRun", NodeJsExec::class) {
+    named("nodeRun", NodeJsExec::class) {
         this.args("react")
         outputFile?.let {
             standardOutput = file("${System.getProperty("user.dir")}/$it").outputStream()
