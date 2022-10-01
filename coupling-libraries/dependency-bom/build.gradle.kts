ffb0c919ddedcb4644fdbf1535182857b6f2f006
RoB Murdock <robert.f.murdock@gmail.com>
2022-09-30 21:37:22 -0400

    minor toml cleanup

--- coupling-libraries/dependency-bom/build.gradle.kts
+++ coupling-libraries/dependency-bom/build.gradle.kts
@@ -24,17 +24,17 @@ dependencies {
     api(platform(libs.com.zegreatrob.jsmints.jsmints.bom))
     api(platform(libs.com.zegreatrob.testmints.testmints.bom))
     api(platform(libs.io.ktor.ktor.bom))
+    api(platform(libs.org.jetbrains.kotlin.wrappers.kotlin.wrappers.bom))
     api(platform(libs.org.jetbrains.kotlinx.kotlinx.coroutines.bom))
     api(platform(libs.org.jetbrains.kotlinx.kotlinx.serialization.bom))
-    api(platform(libs.org.jetbrains.kotlin.wrappers.kotlin.wrappers.bom))
     api(platform(libs.org.junit.junit.bom))
     constraints {
         api(libs.com.benasher44.uuid)
         api(libs.com.fasterxml.jackson.core.jackson.databind)
         api(libs.com.soywiz.korlibs.klock)
         api(libs.io.github.microutils.kotlin.logging)
-        api(libs.org.slf4j.slf4j.simple)
         api(libs.org.jetbrains.kotlinx.kotlinx.datetime)
         api(libs.org.jetbrains.kotlinx.kotlinx.html.js)
+        api(libs.org.slf4j.slf4j.simple)
     }
 }
