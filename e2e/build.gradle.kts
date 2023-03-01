2a203e7efba86b7a86e4b07385844c3f69ce69f8
RoB Murdock <robert.f.murdock@gmail.com>
2023-03-01 00:17:47 -0500

    updating wdiotest plugin, simplifying e2e configuration

--- e2e/build.gradle.kts
+++ e2e/build.gradle.kts
@@ -3,7 +3,6 @@
 import org.jetbrains.kotlin.gradle.plugin.KotlinPlatformType
 import org.jetbrains.kotlin.gradle.plugin.ProjectLocalConfigurations
 import org.jetbrains.kotlin.gradle.targets.js.KotlinJsCompilerAttribute
-import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension
 
 plugins {
     id("com.zegreatrob.coupling.plugins.jstools")
@@ -13,38 +12,6 @@ plugins {
 kotlin {
     js {
         nodejs { testTask { enabled = false } }
-    }
-}
-
-kotlin {
-    sourceSets {
-        getByName("e2eTest") {
-            dependencies {
-                implementation(project(":sdk"))
-                implementation(project(":coupling-libraries:test-logging"))
-                implementation(kotlin("test-js"))
-                implementation("io.github.microutils:kotlin-logging")
-                implementation("com.zegreatrob.testmints:standard")
-                implementation("com.zegreatrob.testmints:minassert")
-                implementation("com.zegreatrob.testmints:async")
-                implementation("com.zegreatrob.jsmints:wdio")
-                implementation("com.zegreatrob.jsmints:wdio-testing-library")
-
-
-                implementation(npmConstrained("@log4js-node/log4js-api"))
-                implementation(npmConstrained("@wdio/reporter"))
-                implementation(npmConstrained("@wdio/dot-reporter"))
-                implementation(npmConstrained("@wdio/jasmine-framework"))
-                implementation(npmConstrained("@wdio/junit-reporter"))
-                implementation(npmConstrained("chromedriver"))
-                implementation(npmConstrained("fs-extra"))
-                implementation(npmConstrained("wdio-html-nice-reporter"))
-                implementation(npmConstrained("webpack"))
-                implementation(npmConstrained("webpack-node-externals"))
-                implementation(npmConstrained("wdio-chromedriver-service"))
-                implementation(npmConstrained("jwt-decode"))
-            }
-        }
     }
 }
 
@@ -62,17 +29,22 @@ val clientConfiguration: Configuration by configurations.creating
 dependencies {
     clientConfiguration(project(mapOf("path" to ":client", "configuration" to "clientConfiguration")))
     appConfiguration(project(mapOf("path" to ":server", "configuration" to "appConfiguration")))
-    implementation(kotlin("stdlib"))
-    implementation("com.soywiz.korlibs.klock:klock")
-    implementation("io.github.microutils:kotlin-logging")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
-    implementation("org.jetbrains.kotlin-wrappers:kotlin-js")
-    implementation("org.jetbrains.kotlin-wrappers:kotlin-extensions")
-    implementation("org.jetbrains.kotlin-wrappers:kotlin-node")
-    implementation("com.benasher44:uuid")
-    implementation("org.jetbrains.kotlin-wrappers:kotlin-extensions")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
-    implementation("com.zegreatrob.jsmints:wdio")
+    e2eTestImplementation(project(":sdk"))
+    e2eTestImplementation(project(":coupling-libraries:test-logging"))
+    e2eTestImplementation(kotlin("test-js"))
+    e2eTestImplementation("io.github.microutils:kotlin-logging")
+    e2eTestImplementation("com.zegreatrob.testmints:standard")
+    e2eTestImplementation("com.zegreatrob.testmints:minassert")
+    e2eTestImplementation("com.zegreatrob.testmints:async")
+    e2eTestImplementation("com.zegreatrob.jsmints:wdio")
+    e2eTestImplementation("com.zegreatrob.jsmints:wdio-testing-library")
+    e2eTestImplementation(npmConstrained("fs-extra"))
+    e2eTestImplementation(npmConstrained("jwt-decode"))
+}
+
+wdioTest {
+    baseUrl.set("https://localhost/local/")
+    // wdioConfigFile.set(projectDir.resolve("wdio.conf.mjs"))
 }
 
 tasks {
@@ -86,8 +58,7 @@ tasks {
         into(e2eTestProcessResources.map { it.destinationDir })
         from("$rootDir/client/build/processedResources/js/main")
     }
-
-    named("e2eRun", com.zegreatrob.jsmints.plugins.NodeExec::class) {
+    e2eRun {
         dependsOn(
             dependencyResources,
             appConfiguration,
@@ -101,14 +72,9 @@ tasks {
             testLoggingLib
         )
         environment(
-            "BASEURL" to "https://localhost/local/",
             "NODE_TLS_REJECT_UNAUTHORIZED" to 0,
             "CLIENT_BASENAME" to "local",
         )
     }
 
-}
-
-rootProject.extensions.findByType(NodeJsRootExtension::class.java).let {
-    it?.nodeVersion = "19.6.0"
 }
