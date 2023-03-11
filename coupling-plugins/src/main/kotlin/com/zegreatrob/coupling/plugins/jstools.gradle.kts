9b6269c848825c8583c0d7b7ffd0f367def5c86b
RoB Murdock <robert.f.murdock@gmail.com>
2023-03-11 14:42:42 -0500

    adding mint-logs plugin

--- coupling-plugins/src/main/kotlin/com/zegreatrob/coupling/plugins/jstools.gradle.kts
+++ coupling-plugins/src/main/kotlin/com/zegreatrob/coupling/plugins/jstools.gradle.kts
@@ -10,6 +10,7 @@ plugins {
     id("com.zegreatrob.coupling.plugins.reports")
     id("com.zegreatrob.coupling.plugins.testLogging")
     id("com.zegreatrob.coupling.plugins.linter")
+    id("com.zegreatrob.testmints.logs.mint-logs")
 }
 
 kotlin {
