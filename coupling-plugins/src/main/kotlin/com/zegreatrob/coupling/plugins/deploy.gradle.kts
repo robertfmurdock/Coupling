f9a705dbfe91052448032dc58c656f4030c3088e
RoB Murdock <robert.f.murdock@gmail.com>
2023-04-01 00:16:47 -0400

    reworking deploys to avoid concurrency problems with the new serverless

--- coupling-plugins/src/main/kotlin/com/zegreatrob/coupling/plugins/deploy.gradle.kts
+++ coupling-plugins/src/main/kotlin/com/zegreatrob/coupling/plugins/deploy.gradle.kts
@@ -11,15 +11,25 @@ kotlin {
 }
 
 val serverProject: Project = project.project(":server")
-fun serverlessBuildDir(stage: String) = "${serverProject.buildDir.absolutePath}/$stage/lambda-dist"
-val serverlessYmlPath = "${serverProject.projectDir.absolutePath}/serverless.yml"
 
 val taggerExtension = TaggerExtension.apply(rootProject)
 
+val deployDir = buildDir.resolve("deploy")
+
 tasks {
+    val copyServerYml by registering(Copy::class) {
+        into(deployDir)
+        from("${serverProject.projectDir.absolutePath}/serverless.yml")
+    }
+    val copyDeployResources by registering(Copy::class) {
+        dependsOn(copyServerYml, ":server:assemble")
+        into(buildDir.resolve("deploy/build/executable"))
+        from(serverProject.buildDir.resolve("executable"))
+    }
+
     val deploy by registering(NodeExec::class) {
         configureDeploy(project.name)
-        dependsOn(":release")
+        dependsOn(":release", copyDeployResources)
         mustRunAfter(":server:check")
         mustRunAfter(":e2e:check")
     }
@@ -37,14 +47,14 @@ fun NodeExec.configureDeploy(stage: String) {
     if (("${rootProject.version}").run { contains("SNAPSHOT") || isBlank() }) {
         enabled = false
     }
-    workingDir = serverProject.projectDir
+    workingDir = deployDir
     nodeCommand = "serverless"
     arguments = listOf(
         "deploy",
         "--config",
-        serverlessYmlPath,
+        deployDir.resolve("serverless.yml").absolutePath,
         "--package",
-        serverlessBuildDir(stage),
+        deployDir.absolutePath,
         "--stage",
         stage
     )
