04276cb688c33b2d802e15deb4dcb6fcc7ca21b9
RoB Murdock <robert.f.murdock@gmail.com>
2023-04-18 23:49:50 -0400

    redoing core layout to recenter toolbar

--- client/components/src/main/kotlin/com/zegreatrob/coupling/client/components/ConfigHeader.kt
+++ client/components/src/main/kotlin/com/zegreatrob/coupling/client/components/ConfigHeader.kt
@@ -17,12 +17,12 @@ import csstype.TextAlign
 import csstype.TextDecoration
 import csstype.VerticalAlign
 import csstype.WhiteSpace
+import csstype.integer
 import csstype.number
 import csstype.pt
 import csstype.px
 import csstype.rgba
 import emotion.react.css
-import react.ChildrenBuilder
 import react.PropsWithChildren
 import react.dom.html.ReactHTML.div
 import react.dom.html.ReactHTML.h1
@@ -41,82 +41,96 @@ val ConfigHeader by nfc<ConfigHeaderProps> { props ->
         }
         div {
             css {
-                display = Display.flex
+                display = Display.grid
                 flexDirection = FlexDirection.row
                 alignItems = AlignItems.center
                 whiteSpace = WhiteSpace.nowrap
             }
-            add(PartyCard(party, 75))
-            partyControls(props, party)
-        }
-    }
-}
-
-private fun ChildrenBuilder.partyControls(props: ConfigHeaderProps, party: Party) = div {
-    css {
-        flexGrow = number(2.0)
-    }
-    h1 {
-        css {
-            display = Display.flex
-            flexDirection = FlexDirection.column
-            alignItems = AlignItems.center
-            marginLeft = 15.px
-            textDecoration = TextDecoration.underline
-            flexGrow = number(2.0)
-        }
-        topControlRow(props)
-        div {
-            css {
-                margin = Margin(0.px, 20.px)
-                display = Display.flex
+            div {
+                css {
+                    gridColumn = integer(1)
+                    gridRowStart = integer(1)
+                    gridRowEnd = integer(3)
+                }
+                add(PartyCard(party, 75))
             }
             div {
                 css {
-                    display = Display.inlineFlex
-                    alignItems = AlignItems.center
-                    borderRadius = 20.px
-                    padding = Padding(5.px, 5.px)
-                    margin = Margin(2.px, 2.px)
-                    fontSize = 0.pt
-                    backgroundColor = Color("#00000014")
-                    boxShadow = BoxShadow(1.px, 1.px, 3.px, rgba(0, 0, 0, 0.6))
+                    gridColumnStart = integer(2)
+                    gridColumnEnd = integer(12)
+                    gridRow = integer(1)
                 }
-                settingsButton(party)
-                addPlayerButton { this.partyId = party.id }
-                viewHistoryButton(party)
-                pinListButton(party)
-                statisticsButton(party)
-                viewRetireesButton(party)
+                h1 {
+                    css {
+                        display = Display.flex
+                        flexDirection = FlexDirection.column
+                        alignItems = AlignItems.center
+                        marginLeft = 15.px
+                        textDecoration = TextDecoration.underline
+                        flexGrow = number(2.0)
+                    }
+                    div {
+                        css {
+                            display = Display.inlineBlock
+                            marginLeft = 15.px
+                            textDecoration = TextDecoration.underline
+                            flexGrow = number(2.0)
+                            alignSelf = AlignSelf.stretch
+                            "*" { verticalAlign = VerticalAlign.middle }
+                        }
+                        div {
+                            css {
+                                display = Display.flex
+                                alignItems = AlignItems.baseline
+                            }
+                            span {
+                                css { flexGrow = number(2.0) }
+                                +props.children
+                            }
+                            span {
+                                css {
+                                    margin = Margin(0.px, 20.px)
+                                    alignItems = AlignItems.baseline
+                                    alignSelf = AlignSelf.stretch
+                                }
+                                PartySelectButton()
+                                LogoutButton()
+                                GqlButton()
+                                NotificationButton()
+                            }
+                        }
+                    }
+                }
             }
-        }
-    }
-}
-
-private fun ChildrenBuilder.topControlRow(props: ConfigHeaderProps) = div {
-    css {
-        display = Display.inlineBlock
-        marginLeft = 15.px
-        textDecoration = TextDecoration.underline
-        flexGrow = number(2.0)
-        alignSelf = AlignSelf.stretch
-        "*" { verticalAlign = VerticalAlign.middle }
-    }
-    div {
-        css {
-            display = Display.flex
-            alignItems = AlignItems.baseline
-        }
-        span {
-            css { flexGrow = number(2.0) }
-            +props.children
-        }
-        span {
-            css { margin = Margin(0.px, 20.px) }
-            PartySelectButton()
-            LogoutButton()
-            GqlButton()
-            NotificationButton()
+            div {
+                css {
+                    gridColumnStart = integer(2)
+                    gridColumnEnd = integer(11)
+                    gridRowStart = integer(2)
+                    gridRowEnd = integer(2)
+                    display = Display.flex
+                    alignItems = AlignItems.center
+                    flexDirection = FlexDirection.column
+                }
+                div {
+                    css {
+                        display = Display.inlineFlex
+                        alignItems = AlignItems.center
+                        borderRadius = 20.px
+                        padding = Padding(5.px, 5.px)
+                        margin = Margin(2.px, 2.px)
+                        fontSize = 0.pt
+                        backgroundColor = Color("#00000014")
+                        boxShadow = BoxShadow(1.px, 1.px, 3.px, rgba(0, 0, 0, 0.6))
+                    }
+                    settingsButton(party)
+                    addPlayerButton { this.partyId = party.id }
+                    viewHistoryButton(party)
+                    pinListButton(party)
+                    statisticsButton(party)
+                    viewRetireesButton(party)
+                }
+            }
         }
     }
 }
