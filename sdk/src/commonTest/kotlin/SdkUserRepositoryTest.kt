925c9d0eab1392c1425e67285cd3a85a14562a73
RoB Murdock <robert.f.murdock@gmail.com>
2023-05-08 14:34:43 -0400

    switching sdk user test to use query on the sdk

--- sdk/src/commonTest/kotlin/SdkUserRepositoryTest.kt
+++ sdk/src/commonTest/kotlin/SdkUserRepositoryTest.kt
@@ -1,25 +1,15 @@
+
 import com.zegreatrob.coupling.action.user.UserQuery
-import com.zegreatrob.coupling.repository.validation.MagicClock
-import com.zegreatrob.coupling.repository.validation.SharedContextData
-import com.zegreatrob.coupling.sdk.Sdk
-import com.zegreatrob.coupling.stubmodel.stubUser
 import com.zegreatrob.minassert.assertIsEqualTo
 import com.zegreatrob.minassert.assertIsNotEqualTo
-import com.zegreatrob.testmints.async.asyncTestTemplate
+import com.zegreatrob.testmints.async.asyncSetup
 import kotlin.test.Test
 
 class SdkUserRepositoryTest {
 
-    val repositorySetup = asyncTestTemplate<SharedContextData<Sdk>>(sharedSetup = {
-        val clock = MagicClock()
-        val sdk = authorizedSdk()
-        SharedContextData(sdk, clock, stubUser().copy(email = primaryAuthorizedUsername))
-    })
-
     @Test
-    fun canPerformUserQuery() = repositorySetup {
-    } exercise {
-        repository.perform(UserQuery())
+    fun canPerformUserQuery() = asyncSetup() exercise {
+        authorizedSdk().perform(UserQuery())
     } verify { result ->
         result.let {
             it?.email.assertIsEqualTo(primaryAuthorizedUsername)
