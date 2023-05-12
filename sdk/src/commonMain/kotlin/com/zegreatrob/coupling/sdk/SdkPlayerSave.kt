c6f457ad4993db010985a4a3464a76ce29a07e55
RoB Murdock <robert.f.murdock@gmail.com>
2023-05-12 10:51:16 -0400

    starting the process of de-repositorying the sdk functions

--- sdk/src/commonMain/kotlin/com/zegreatrob/coupling/sdk/SdkPlayerSave.kt
+++ sdk/src/commonMain/kotlin/com/zegreatrob/coupling/sdk/SdkPlayerSave.kt
@@ -1,7 +1,9 @@
 package com.zegreatrob.coupling.sdk
 
+import com.zegreatrob.coupling.action.player.SavePlayerCommand
 import com.zegreatrob.coupling.json.SavePlayerInput
 import com.zegreatrob.coupling.model.party.PartyElement
+import com.zegreatrob.coupling.model.party.with
 import com.zegreatrob.coupling.model.player.Player
 import com.zegreatrob.coupling.model.player.partyId
 import com.zegreatrob.coupling.repository.player.PlayerSave
@@ -10,16 +12,23 @@ interface SdkPlayerSave : PlayerSave, GqlSyntax, GraphQueries {
     override suspend fun save(partyPlayer: PartyElement<Player>) {
         doQuery(mutations.savePlayer, partyPlayer.input())
     }
-
-    private fun PartyElement<Player>.input() = SavePlayerInput(
-        partyId = partyId,
-        playerId = element.id,
-        name = element.name,
-        email = element.email,
-        badge = "${element.badge}",
-        callSignAdjective = element.callSignAdjective,
-        callSignNoun = element.callSignNoun,
-        imageURL = element.imageURL,
-        avatarType = element.avatarType?.name,
-    )
 }
+
+interface SdkSavePlayerCommandDispatcher : SavePlayerCommand.Dispatcher, GqlSyntax {
+    override suspend fun perform(command: SavePlayerCommand) = with(command) {
+        doQuery(Mutation.savePlayer, partyId.with(player).input())
+        Unit
+    }
+}
+
+fun PartyElement<Player>.input() = SavePlayerInput(
+    partyId = partyId,
+    playerId = element.id,
+    name = element.name,
+    email = element.email,
+    badge = "${element.badge}",
+    callSignAdjective = element.callSignAdjective,
+    callSignNoun = element.callSignNoun,
+    imageURL = element.imageURL,
+    avatarType = element.avatarType?.name,
+)
