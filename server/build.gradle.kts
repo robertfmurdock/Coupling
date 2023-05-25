30e7f620073a112d8cc0d4b74605cc95bc98eb9b
RoB Murdock <robert.f.murdock@gmail.com>
2023-05-25 11:30:10 -0400

    moving server base project to inside of server, as it is a single-use dependency

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -167,7 +167,7 @@ tasks {
     register<Exec>("buildImage") {
         dependsOn(
             prepareDockerData,
-            ":server-base:pullImage"
+            ":server:base:pullImage"
         )
         commandLine(
             "docker build --tag ghcr.io/robertfmurdock/coupling-serverless:latest build/docker-data"
