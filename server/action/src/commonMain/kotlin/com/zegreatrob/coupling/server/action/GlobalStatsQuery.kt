f27a9de9ac8fc7dee061ea2137fdd3976ef3ad05
RoB Murdock <robert.f.murdock@gmail.com>
2023-05-26 14:44:07 -0400

    some simple refactors

--- server/action/src/commonMain/kotlin/com/zegreatrob/coupling/server/action/GlobalStatsQuery.kt
+++ server/action/src/commonMain/kotlin/com/zegreatrob/coupling/server/action/GlobalStatsQuery.kt
@@ -6,8 +6,11 @@ import com.zegreatrob.coupling.action.successResult
 import com.zegreatrob.coupling.model.GlobalStats
 import com.zegreatrob.coupling.model.PartyRecord
 import com.zegreatrob.coupling.model.PartyStats
+import com.zegreatrob.coupling.model.Record
+import com.zegreatrob.coupling.model.elements
 import com.zegreatrob.coupling.model.pairassignmentdocument.PairAssignmentDocument
-import com.zegreatrob.coupling.model.pairassignmentdocument.document
+import com.zegreatrob.coupling.model.pairassignmentdocument.PinnedCouplingPair
+import com.zegreatrob.coupling.model.party.Party
 import com.zegreatrob.coupling.repository.pairassignmentdocument.PairAssignmentDocumentRepository
 import com.zegreatrob.coupling.repository.party.PartyRepository
 import kotlinx.coroutines.flow.asFlow
@@ -24,27 +27,38 @@ data class GlobalStatsQuery(val year: Int) : SimpleSuspendResultAction<GlobalSta
         val pairAssignmentDocumentRepository: PairAssignmentDocumentRepository
 
         suspend fun perform(query: GlobalStatsQuery): Result<GlobalStats> {
-            val matchesYear = { record: PartyRecord<PairAssignmentDocument> ->
-                record.data.element.date.year.year == query.year
-            }
-            val partyStatsFlow = partyRepository.loadParties()
+            val matchesYear = yearMatcher(query.year)
+            val partyStats = partyRepository.loadParties()
                 .asFlow()
                 .map { it to pairAssignmentDocumentRepository.loadPairAssignments(it.data.id) }
                 .filter { (_, docs) -> docs.any(matchesYear) }
-                .map { (party, docs) ->
-                    val pairDocsThisYear = docs.filter(matchesYear)
-                    PartyStats(
-                        name = party.data.name ?: party.data.id.value,
-                        id = party.data.id,
-                        playerCount = pairDocsThisYear.distinctPlayersPairedThisYear().size,
-                        spins = pairDocsThisYear.size,
-                    )
-                }
-            return GlobalStats(parties = partyStatsFlow.toList()).successResult()
+                .map { (party, docs) -> partyStats(party, docs, matchesYear) }
+                .toList()
+            return GlobalStats(parties = partyStats).successResult()
         }
-
-        private fun List<PartyRecord<PairAssignmentDocument>>.distinctPlayersPairedThisYear() =
-            flatMap { it.data.document.pairs.flatMap { it.players.map { it.player.id } } }
-                .distinct()
     }
 }
+
+private fun yearMatcher(year: Int) = { record: PartyRecord<PairAssignmentDocument> ->
+    record.data.element.date.year.year == year
+}
+
+private fun partyStats(
+    party: Record<Party>,
+    docs: List<PartyRecord<PairAssignmentDocument>>,
+    filter: (PartyRecord<PairAssignmentDocument>) -> Boolean,
+): PartyStats {
+    val pairDocsThisYear = docs.filter(filter)
+    return PartyStats(
+        name = party.data.name ?: party.data.id.value,
+        id = party.data.id,
+        playerCount = pairDocsThisYear.distinctPlayersPairedThisYear().size,
+        spins = pairDocsThisYear.size,
+    )
+}
+
+private fun List<PartyRecord<PairAssignmentDocument>>.distinctPlayersPairedThisYear() = elements
+    .flatMap(PairAssignmentDocument::pairs)
+    .flatMap(PinnedCouplingPair::players)
+    .map { it.player.id }
+    .distinct()
