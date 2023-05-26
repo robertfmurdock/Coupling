1f4fecee2e95e0e0e3bc06be0bb797e762f9d0e7
Rob Murdock <robertfmurdock@gmail.com>
2023-05-26 17:05:50 -0400

    Revert "react-ga and attempting to hook it up to react router" as it looks like the new ga library watches browser history and shouldn't need manual page additions
    This reverts commit 9e0866a09b4ed33074215455b97e40b7797a7d68.

--- client/src/main/kotlin/com/zegreatrob/coupling/client/Main.kt
+++ client/src/main/kotlin/com/zegreatrob/coupling/client/Main.kt
@@ -1,6 +1,5 @@
 package com.zegreatrob.coupling.client
 
-import com.zegreatrob.coupling.client.external.react.ga.ReactGA
 import kotlinext.js.require
 import kotlin.js.Json
 import kotlin.js.json
@@ -9,7 +8,6 @@ fun main() {
     if (isTestRun()) {
         return
     }
-    ReactGA.initialize("G-F28M9PKERE")
     require("com/zegreatrob/coupling/client/animations.css")
     require("com/zegreatrob/coupling/client/style.css")
 
--- client/src/main/kotlin/com/zegreatrob/coupling/client/routing/CouplingRoute.kt
+++ client/src/main/kotlin/com/zegreatrob/coupling/client/routing/CouplingRoute.kt
@@ -1,7 +1,6 @@
 package com.zegreatrob.coupling.client.routing
 
 import com.zegreatrob.coupling.client.components.external.auth0.react.useAuth0Data
-import com.zegreatrob.coupling.client.external.react.ga.ReactGA
 import com.zegreatrob.minreact.nfc
 import js.core.jso
 import kotlinx.browser.window
@@ -13,7 +12,6 @@ import react.router.RouteObject
 import react.router.dom.useSearchParams
 import react.router.useNavigate
 import react.router.useParams
-import react.useEffect
 
 fun couplingRoute(path: String, rComponent: FC<PageProps>) = jso<RouteObject> {
     this.path = path
@@ -30,10 +28,6 @@ val CouplingRoute by nfc<CouplingRouteProps> {
     val navigate = useNavigate()
 
     val auth0Data = useAuth0Data()
-
-    useEffect {
-        ReactGA.pageview(window.location.pathname + window.location.search)
-    }
 
     it.rComponent {
         pathParams = params
