ddcbe2ee041580dc56c2e59acc5c420a80d58b54
RoB Murdock <robert.f.murdock@gmail.com>
2023-05-26 13:43:22 -0400

    only including spins from this year in the count

--- server/action/src/commonMain/kotlin/com/zegreatrob/coupling/server/action/GlobalStatsQuery.kt
+++ server/action/src/commonMain/kotlin/com/zegreatrob/coupling/server/action/GlobalStatsQuery.kt
@@ -4,7 +4,9 @@ import com.zegreatrob.coupling.action.Result
 import com.zegreatrob.coupling.action.SimpleSuspendResultAction
 import com.zegreatrob.coupling.action.successResult
 import com.zegreatrob.coupling.model.GlobalStats
+import com.zegreatrob.coupling.model.PartyRecord
 import com.zegreatrob.coupling.model.PartyStats
+import com.zegreatrob.coupling.model.pairassignmentdocument.PairAssignmentDocument
 import com.zegreatrob.coupling.repository.pairassignmentdocument.PairAssignmentDocumentRepository
 import com.zegreatrob.coupling.repository.party.PartyRepository
 import kotlinx.coroutines.flow.asFlow
@@ -21,18 +23,19 @@ data class GlobalStatsQuery(val year: Int) : SimpleSuspendResultAction<GlobalSta
         val pairAssignmentDocumentRepository: PairAssignmentDocumentRepository
 
         suspend fun perform(query: GlobalStatsQuery): Result<GlobalStats> {
+            val matchesYear = { record: PartyRecord<PairAssignmentDocument> ->
+                record.data.element.date.year.year == query.year
+            }
             val partyStatsFlow = partyRepository.loadParties()
                 .asFlow()
-                .map {
-                    it to pairAssignmentDocumentRepository.loadPairAssignments(it.data.id)
-                }.filter { (_, docs) ->
-                    docs.any { it.data.element.date.year.year == query.year }
-                }.map { (party, docs) ->
+                .map { it to pairAssignmentDocumentRepository.loadPairAssignments(it.data.id) }
+                .filter { (_, docs) -> docs.any(matchesYear) }
+                .map { (party, docs) ->
                     PartyStats(
                         name = party.data.name ?: party.data.id.value,
                         id = party.data.id,
                         playerCount = 0,
-                        spins = docs.size,
+                        spins = docs.filter(matchesYear).size,
                     )
                 }
             return GlobalStats(parties = partyStatsFlow.toList()).successResult()
