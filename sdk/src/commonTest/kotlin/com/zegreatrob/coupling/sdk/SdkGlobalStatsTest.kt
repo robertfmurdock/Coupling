6d410bcfb627ab564781883b30523147bbb29660
RoB Murdock <robert.f.murdock@gmail.com>
2023-05-28 11:29:24 -0400

    ensuring there's always something in the database so stats show up

--- sdk/src/commonTest/kotlin/com/zegreatrob/coupling/sdk/SdkGlobalStatsTest.kt
+++ sdk/src/commonTest/kotlin/com/zegreatrob/coupling/sdk/SdkGlobalStatsTest.kt
@@ -1,6 +1,10 @@
 package com.zegreatrob.coupling.sdk
 
+import com.zegreatrob.coupling.action.pairassignmentdocument.SavePairAssignmentsCommand
+import com.zegreatrob.coupling.action.party.SavePartyCommand
 import com.zegreatrob.coupling.sdk.gql.graphQuery
+import com.zegreatrob.coupling.stubmodel.stubPairAssignmentDoc
+import com.zegreatrob.coupling.stubmodel.stubParty
 import com.zegreatrob.minassert.assertIsNotEqualTo
 import com.zegreatrob.testmints.async.asyncSetup
 import korlibs.time.DateTime
@@ -11,7 +15,11 @@ class SdkGlobalStatsTest {
     @Test
     fun canGetGlobalStats() = asyncSetup(object {
         val now = DateTime.now().year
-    }) exercise {
+        val party = stubParty()
+    }) {
+        sdk().perform(SavePartyCommand(party))
+        sdk().perform(SavePairAssignmentsCommand(party.id, stubPairAssignmentDoc().copy(date = DateTime.now())))
+    } exercise {
         sdk().perform(
             graphQuery {
                 globalStats(now)
