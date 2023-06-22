2c2ac1591e3080f0ddde8a6ac2e38bd804aadd4d
RoB Murdock <robert.f.murdock@gmail.com>
2023-06-22 00:23:40 -0400

    some cleanup of connect command

--- server/src/main/kotlin/com/zegreatrob/coupling/server/express/route/SlackCommandResponse.kt
+++ server/src/main/kotlin/com/zegreatrob/coupling/server/express/route/SlackCommandResponse.kt
@@ -11,32 +11,41 @@ fun slackCommandResponse(): Handler = { request, response, _ ->
     val body = request.body as Buffer
     val params = URLSearchParams(body.toString(encoding = BufferEncoding.utf8))
 
-    val targetParams = URLSearchParams(emptyArray()).apply {
-        append("slackTeam", params["team_id"] ?: "")
-        append("slackChannel", params["channel_id"] ?: "")
-    }
+    val slackTeam = params["team_id"] ?: ""
+    val slackChannel = params["channel_id"] ?: ""
 
-    response.send(
+    response.send(connectMessage(slackTeam, slackChannel))
+}
+
+private fun connectMessage(slackTeam: String, slackChannel: String) = json(
+    "blocks" to arrayOf(
         json(
-            "blocks" to arrayOf(
+            "type" to "header",
+            "text" to json(
+                "type" to "plain_text",
+                "text" to "Connect Channel to Coupling",
+                "emoji" to true,
+            ),
+        ),
+        json(
+            "type" to "section",
+            "fields" to arrayOf(
                 json(
-                    "type" to "header",
-                    "text" to json(
-                        "type" to "plain_text",
-                        "text" to "Install Coupling",
-                        "emoji" to true,
-                    ),
-                ),
-                json(
-                    "type" to "section",
-                    "fields" to arrayOf(
-                        json(
-                            "type" to "mrkdwn",
-                            "text" to "Click [here](${Config.publicUrl}/integration/slack/connect?$targetParams)!",
-                        ),
-                    ),
+                    "type" to "button",
+                    "text" to "Click here!",
+                    "value" to "connect_link",
+                    "url" to connectUrl(slackTeam, slackChannel),
                 ),
             ),
         ),
-    )
+    ),
+)
+
+private fun connectUrl(slackTeam: String, slackChannel: String): String {
+    val targetParams = URLSearchParams(emptyArray()).apply {
+        append("slackTeam", slackTeam)
+        append("slackChannel", slackChannel)
+    }
+
+    return "${Config.publicUrl}/integration/slack/connect?$targetParams"
 }
