f107c6b7d220c2362592427c0b56f013e42c4e35
RoB Murdock <rob@zegreatrob.com>
2023-07-18 23:58:53 -0400

    fixing stats bug

--- client/src/jsMain/kotlin/com/zegreatrob/coupling/client/stats/StatisticsPage.kt
+++ client/src/jsMain/kotlin/com/zegreatrob/coupling/client/stats/StatisticsPage.kt
@@ -1,14 +1,19 @@
 package com.zegreatrob.coupling.client.stats
 
 import com.zegreatrob.coupling.action.stats.StatisticsQuery
+import com.zegreatrob.coupling.client.ClientDispatcher
+import com.zegreatrob.coupling.client.components.DispatchFunc
 import com.zegreatrob.coupling.client.components.stats.PartyStatistics
-import com.zegreatrob.coupling.client.components.stats.create
 import com.zegreatrob.coupling.client.memory.ClientStatisticsAction
 import com.zegreatrob.coupling.client.partyPageFunction
 import com.zegreatrob.coupling.client.routing.CouplingQuery
 import com.zegreatrob.coupling.model.elements
 import com.zegreatrob.coupling.sdk.gql.graphQuery
-import react.useEffect
+import com.zegreatrob.minreact.nfc
+import react.Fragment
+import react.PropsWithValue
+import react.create
+import react.useEffectOnce
 import react.useState
 
 val StatisticsPage = partyPageFunction { props, partyId ->
@@ -19,11 +24,16 @@ val StatisticsPage = partyPageFunction { props, partyId ->
             val party = queryResult.party?.details?.data ?: return@CouplingQuery null
             val players = queryResult.party?.playerList?.elements ?: return@CouplingQuery null
             val history = queryResult.party?.pairAssignmentDocumentList?.elements ?: return@CouplingQuery null
-            var results by useState<StatisticsQuery.Results?>(null)
-            val calculate = dispatchFunc { results = fire(ClientStatisticsAction(party, players, history)) }
-            useEffect { calculate() }
-            results?.let { PartyStatistics.create(it) }
+            val action = ClientStatisticsAction(party, players, history)
+            Fragment.create { CalculatingPartyStats(action to dispatchFunc) }
         },
         key = partyId.value,
     )
+}
+
+val CalculatingPartyStats by nfc<PropsWithValue<Pair<ClientStatisticsAction, DispatchFunc<ClientDispatcher>>>> { props ->
+    var results by useState<StatisticsQuery.Results?>(null)
+    val calculate = props.value.second { results = fire(props.value.first) }
+    useEffectOnce { calculate() }
+    results?.let { PartyStatistics(it) }
 }
