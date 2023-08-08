f7c10660c05b5ad54cc11b135e0d171e3070d096
RoB Murdock <rob@zegreatrob.com>
2023-08-08 10:59:08 -0400

    enabling docker compose v2

--- libraries/repository/dynamo/build.gradle.kts
+++ libraries/repository/dynamo/build.gradle.kts
@@ -19,6 +19,7 @@ kotlin {
 }
 
 dockerCompose {
+    useDockerComposeV2.set(true)
     tcpPortsToIgnoreWhenWaiting.set(listOf(5555))
     setProjectName("Coupling-root")
     startedServices.set(listOf("dynamo"))
