b9063fa8f590e21ce87baf6d94513e3ac5cd7ca1
RoB Murdock <rob@zegreatrob.com>
2023-09-14 10:04:29 -0400

    adding latest route that produces the js cli

--- server/src/jsMain/kotlin/com/zegreatrob/coupling/server/express/route/CliRoute.kt
+++ server/src/jsMain/kotlin/com/zegreatrob/coupling/server/express/route/CliRoute.kt
@@ -3,6 +3,12 @@ package com.zegreatrob.coupling.server.express.route
 import com.zegreatrob.coupling.server.express.Config
 import com.zegreatrob.coupling.server.external.express.Handler
 
-fun cliRoute(): Handler = { _, response, _ ->
-    response.redirect("${Config.cliUrl}/coupling-cli.tgz")
+fun cliRoute(): Handler = { request, response, _ ->
+    val type = request.query["type"]
+    response.redirect(
+        when (type) {
+            "js" -> "${Config.cliUrl}/coupling-cli-js.tgz"
+            else -> "${Config.cliUrl}/coupling-cli.tgz"
+        },
+    )
 }
