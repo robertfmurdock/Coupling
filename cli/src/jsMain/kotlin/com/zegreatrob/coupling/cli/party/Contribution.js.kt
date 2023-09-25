40b1b6a58ee8cd8ad354503ab277f62f3f0faded
RoB Murdock <rob@zegreatrob.com>
2023-09-25 08:17:48 -0400

    resolving buffer signature collision

--- cli/src/jsMain/kotlin/com/zegreatrob/coupling/cli/party/Contribution.js.kt
+++ cli/src/jsMain/kotlin/com/zegreatrob/coupling/cli/party/Contribution.js.kt
@@ -7,7 +7,7 @@ import node.fs.readSync
 actual fun readLineFromStandardIn(): String = try {
     buildString {
         var char = ""
-        val buf = Buffer.alloc(1)
+        val buf = Buffer.alloc(size = 1, fill = 0.0, encoding = BufferEncoding.utf8)
         do {
             val bytesRead = readSync(fd = 0, buffer = buf, offset = 0, length = 1, position = null)
             if (bytesRead.toInt() > 0) {
