3b49839510f3bc039015d166632d30679b2045c8
RoB Murdock <rob@zegreatrob.com>
2023-12-18 22:59:19 -0500

    fixing loading of date fns

--- client/components/src/jsMain/kotlin/com/zegreatrob/coupling/client/components/stats/PartyStatistics.kt
+++ client/components/src/jsMain/kotlin/com/zegreatrob/coupling/client/components/stats/PartyStatistics.kt
@@ -34,8 +34,8 @@ import kotlin.time.Duration
 @JsModule("date-fns/formatDistance")
 external val formatDistanceModule: dynamic
 
-val formatDistance: (Int?, Int) -> String = if (formatDistanceModule.default != undefined) {
-    formatDistanceModule.default.unsafeCast<(Int?, Int) -> String>()
+val formatDistance: (Int?, Int) -> String = if (formatDistanceModule["formatDistance"] != undefined) {
+    formatDistanceModule["formatDistance"].unsafeCast<(Int?, Int) -> String>()
 } else {
     formatDistanceModule.unsafeCast<(Int?, Int) -> String>()
 }
