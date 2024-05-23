b76f0b0eb86c056d2ce58f64afa2b058f96ff2e4
RoB Murdock <rob@zegreatrob.com>
2024-05-23 10:55:54 -0400

    fixed remaining warnings

--- client/components/external/src/jsMain/kotlin/com/zegreatrob/coupling/client/components/external/blueimp/md5/Dsl.kt
+++ client/components/external/src/jsMain/kotlin/com/zegreatrob/coupling/client/components/external/blueimp/md5/Dsl.kt
@@ -1,12 +1,3 @@
-@file:Suppress(
-    "INTERFACE_WITH_SUPERCLASS",
-    "OVERRIDING_FINAL_MEMBER",
-    "RETURN_TYPE_MISMATCH_ON_OVERRIDE",
-    "CONFLICTING_OVERLOADS",
-    "EXTERNAL_DELEGATION",
-    "NESTED_CLASS_IN_EXTERNAL_INTERFACE",
-)
-
 package com.zegreatrob.coupling.client.components.external.blueimp.md5
 
 @JsModule("blueimp-md5")
