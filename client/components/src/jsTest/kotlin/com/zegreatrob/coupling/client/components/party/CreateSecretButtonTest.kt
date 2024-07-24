3d68902c43e0fe9d53f9ec788b8182c45374bf5d
RoB Murdock <rob@zegreatrob.com>
2024-07-24 10:47:51 -0400

    creating createsecretbutton w/ functionality

---
+++ client/components/src/jsTest/kotlin/com/zegreatrob/coupling/client/components/party/CreateSecretButtonTest.kt
@@ -0,0 +1,69 @@
+package com.zegreatrob.coupling.client.components.party
+
+import com.zegreatrob.coupling.action.secret.CreateSecretCommand
+import com.zegreatrob.coupling.client.components.StubDispatcher
+import com.zegreatrob.coupling.stubmodel.stubPartyId
+import com.zegreatrob.coupling.stubmodel.stubSecret
+import com.zegreatrob.minassert.assertIsEqualTo
+import com.zegreatrob.testmints.async.asyncSetup
+import com.zegreatrob.wrapper.testinglibrary.react.TestingLibraryReact.act
+import com.zegreatrob.wrapper.testinglibrary.react.TestingLibraryReact.render
+import com.zegreatrob.wrapper.testinglibrary.react.TestingLibraryReact.screen
+import com.zegreatrob.wrapper.testinglibrary.userevent.UserEvent
+import org.w3c.dom.HTMLInputElement
+import kotlin.test.Test
+
+class CreateSecretButtonTest {
+    @Test
+    fun givenNoDescriptionClickWillNotFireCreateCommand() = asyncSetup(object {
+        val partyId = stubPartyId()
+        val dispatcher = StubDispatcher()
+        val actor = UserEvent.setup()
+    }) {
+        render { CreateSecretButton(partyId, dispatcher.func()) }
+    } exercise {
+        actor.click(screen.findByRole("button"))
+    } verify {
+        dispatcher.receivedActions
+            .assertIsEqualTo(emptyList())
+    }
+
+    @Test
+    fun givenDescriptionClickWillFireCreateCommand() = asyncSetup(object {
+        val partyId = stubPartyId()
+        val dispatcher = StubDispatcher()
+        val description = "We represent the lolly-pop kids!"
+        val actor = UserEvent.setup()
+    }) {
+        render { CreateSecretButton(partyId, dispatcher.func()) }
+    } exercise {
+        actor.type(screen.findByLabelText("Description"), description)
+        actor.click(screen.findByRole("button"))
+    } verify {
+        dispatcher.receivedActions
+            .assertIsEqualTo(listOf(CreateSecretCommand(partyId, description)))
+    }
+
+    @Test
+    fun givenSuccessfulCommandWillShowSecretIdAndSecretValue() = asyncSetup(object {
+        val partyId = stubPartyId()
+        val dispatcher = StubDispatcher.Channel()
+        val description = "We represent the lolly-pop kids!"
+        val actor = UserEvent.setup()
+        val expectedSecret = stubSecret()
+        val expectedSecretValue = "Don't tell nobody!"
+    }) {
+        render { CreateSecretButton(partyId, dispatcher.func()) }
+        actor.type(screen.findByLabelText("Description"), description)
+    } exercise {
+        actor.click(screen.findByRole("button"))
+        act {
+            dispatcher.onActionReturn(Pair(expectedSecret, expectedSecretValue))
+        }
+    } verify {
+        val idInput = screen.getByLabelText("Secret ID") as HTMLInputElement
+        val valueInput = screen.getByLabelText("Secret Value") as HTMLInputElement
+        valueInput.value.assertIsEqualTo(expectedSecretValue)
+        idInput.value.assertIsEqualTo(expectedSecret.id)
+    }
+}
