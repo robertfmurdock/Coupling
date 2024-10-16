c644d842634bc858f47d7264b73d29cf3265e5a7
RoB Murdock <rob@zegreatrob.com>
2024-10-15 22:11:23 -0400

    refactoring contribution pop up menu

---
+++ client/components/src/jsMain/kotlin/com/zegreatrob/coupling/client/components/ContributionPopUpMenu.kt
@@ -0,0 +1,68 @@
+package com.zegreatrob.coupling.client.components
+
+import com.zegreatrob.coupling.action.player.SavePlayerCommand
+import com.zegreatrob.coupling.client.components.contributor.ContributorMenu
+import com.zegreatrob.coupling.model.party.PartyId
+import com.zegreatrob.coupling.model.player.Player
+import com.zegreatrob.minreact.ReactFunc
+import com.zegreatrob.minreact.nfc
+import js.objects.jso
+import popper.core.Placement
+import popper.core.ReferenceElement
+import popper.core.modifier
+import popper.core.modifiers.Arrow
+import popper.core.modifiers.Offset
+import react.MutableRefObject
+import react.Props
+import react.ReactNode
+import react.popper.UsePopperOptions
+import react.popper.usePopper
+import react.useRef
+import react.useState
+import web.html.HTMLElement
+
+external interface ContributionPopUpMenuProps : Props {
+    @Suppress("INLINE_CLASS_IN_EXTERNAL_DECLARATION_WARNING")
+    var partyId: PartyId
+    var players: List<Player>
+    var dispatchFunc: DispatchFunc<SavePlayerCommand.Dispatcher>
+    var child: ((ReferenceElement, Player) -> Unit) -> ReactNode
+}
+
+@ReactFunc
+val ContributionPopUpMenu by nfc<ContributionPopUpMenuProps> { props ->
+    val (partyId, players, dispatchFunc, child) = props
+    val (menuTarget, setMenuTarget) = useState<Pair<ReferenceElement, Player>?>(null)
+    val popperRef = useRef<HTMLElement>()
+    val arrowRef = useRef<HTMLElement>()
+    val popperInstance = usePopper(menuTarget?.first, popperRef.current, popperOptions(arrowRef))
+
+    CouplingPopUp(
+        hide = menuTarget == null,
+        popperRef = popperRef,
+        arrowRef = arrowRef,
+        popperInstance = popperInstance,
+    ) {
+        if (menuTarget != null) {
+            ContributorMenu(menuTarget.second, players, partyId, dispatchFunc)
+        }
+    }
+    +child { element, player ->
+        setMenuTarget(element to player)
+        popperInstance.forceUpdate?.invoke()
+    }
+}
+
+private fun popperOptions(arrowRef: MutableRefObject<HTMLElement>): UsePopperOptions = jso {
+    placement = Placement.right
+    modifiers = arrayOf(
+        Arrow.modifier {
+            this.options = jso {
+                this.element = arrowRef.current
+            }
+        },
+        Offset.modifier {
+            this.options = jso { offset = Offset(0.0, 10.0) }
+        },
+    )
+}
--- client/components/src/jsMain/kotlin/com/zegreatrob/coupling/client/components/contribution/ContributionOverviewContent.kt
+++ client/components/src/jsMain/kotlin/com/zegreatrob/coupling/client/components/contribution/ContributionOverviewContent.kt
@@ -1,29 +1,20 @@
 package com.zegreatrob.coupling.client.components.contribution
 
 import com.zegreatrob.coupling.action.player.SavePlayerCommand
-import com.zegreatrob.coupling.client.components.CouplingPopUp
+import com.zegreatrob.coupling.client.components.ContributionPopUpMenu
 import com.zegreatrob.coupling.client.components.DispatchFunc
-import com.zegreatrob.coupling.client.components.contributor.ContributorMenu
 import com.zegreatrob.coupling.model.Contribution
 import com.zegreatrob.coupling.model.party.PartyDetails
 import com.zegreatrob.coupling.model.player.Player
 import com.zegreatrob.minreact.ReactFunc
 import com.zegreatrob.minreact.nfc
 import emotion.react.css
-import js.objects.jso
-import popper.core.Placement
 import popper.core.ReferenceElement
-import popper.core.modifier
-import popper.core.modifiers.Arrow
-import popper.core.modifiers.Offset
-import react.MutableRefObject
+import react.Fragment
 import react.Props
+import react.create
 import react.dom.html.ReactHTML.div
 import react.dom.html.ReactHTML.h2
-import react.popper.UsePopperOptions
-import react.popper.usePopper
-import react.useRef
-import react.useState
 import web.cssom.AlignItems
 import web.cssom.Display
 import web.cssom.FlexDirection
@@ -40,10 +31,6 @@ external interface ContributionOverviewContentProps : Props {
 @ReactFunc
 val ContributionOverviewContent by nfc<ContributionOverviewContentProps> { props ->
     val (party, contributions, contributors, players) = props
-    val popperRef = useRef<HTMLElement>()
-    val arrowRef = useRef<HTMLElement>()
-    val (menuTarget, setMenuTarget) = useState<Pair<ReferenceElement, Player>?>(null)
-    val popperInstance = usePopper(menuTarget?.first, popperRef.current, popperOptions(arrowRef))
     div {
         div {
             css {
@@ -51,44 +38,22 @@ val ContributionOverviewContent by nfc<ContributionOverviewContentProps> { props
                 flexDirection = FlexDirection.column
                 alignItems = AlignItems.center
             }
-            h2 {
-                +"Most Recent ${contributions.size} Contributions:"
-            }
-            CouplingPopUp(
-                hide = menuTarget == null,
-                popperRef = popperRef,
-                arrowRef = arrowRef,
-                popperInstance = popperInstance,
-            ) {
-                if (menuTarget != null) {
-                    ContributorMenu(menuTarget.second, players, party.id, props.dispatchFunc)
+            h2 { +"Most Recent ${contributions.size} Contributions:" }
+            ContributionPopUpMenu(party.id, players, props.dispatchFunc, child = { setSelectedPlayerCard ->
+                val onPlayerClick = { player: Player, element: HTMLElement ->
+                    setSelectedPlayerCard(ReferenceElement(element)!!, player)
                 }
-            }
-            contributions.forEach { contribution ->
-                ContributionCard(
-                    contribution = contribution,
-                    contributors = contributors,
-                    key = contribution.id,
-                    onPlayerClick = { player, element ->
-                        setMenuTarget(ReferenceElement(element)!! to player)
-                        popperInstance.forceUpdate?.invoke()
-                    },
-                )
-            }
+                Fragment.create {
+                    contributions.forEach { contribution ->
+                        ContributionCard(
+                            key = contribution.id,
+                            contribution = contribution,
+                            contributors = contributors,
+                            onPlayerClick = onPlayerClick,
+                        )
+                    }
+                }
+            })
         }
     }
-}
-
-private fun popperOptions(arrowRef: MutableRefObject<HTMLElement>): UsePopperOptions = jso {
-    this.placement = Placement.right
-    this.modifiers = arrayOf(
-        Arrow.modifier {
-            this.options = jso {
-                this.element = arrowRef.current
-            }
-        },
-        Offset.modifier {
-            this.options = jso { offset = Offset(0.0, 10.0) }
-        },
-    )
 }
