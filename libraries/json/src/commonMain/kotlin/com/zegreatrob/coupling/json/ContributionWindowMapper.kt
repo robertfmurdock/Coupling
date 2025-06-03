a826da36cebf680312febe3a3cbc5667d171ead8
RoB Murdock <rob@zegreatrob.com>
2025-06-03 12:44:03 -0400

    adding half year time window

--- libraries/json/src/commonMain/kotlin/com/zegreatrob/coupling/json/ContributionWindowMapper.kt
+++ libraries/json/src/commonMain/kotlin/com/zegreatrob/coupling/json/ContributionWindowMapper.kt
@@ -5,7 +5,8 @@ import kotlin.time.Duration.Companion.days
 fun GqlContributionWindow.toModel() = when (this) {
     GqlContributionWindow.All -> null
     GqlContributionWindow.Year -> 365.days
-    GqlContributionWindow.Quarter -> (365 / 4).days
+    GqlContributionWindow.HalfYear -> (365 / 2.0).days
+    GqlContributionWindow.Quarter -> (365 / 4.0).days
     GqlContributionWindow.Month -> 30.days
     GqlContributionWindow.Week -> 7.days
 }
