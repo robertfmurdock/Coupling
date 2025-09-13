c28cf2498264d747cac86820197282d7d1f59763
RoB Murdock <rob@zegreatrob.com>
2025-09-12 22:44:33 -0400

    adding lazy for the visualization page, to great effect

--- client/src/jsMain/kotlin/com/zegreatrob/coupling/client/contribution/ContributionVisualizationPage.kt
+++ client/src/jsMain/kotlin/com/zegreatrob/coupling/client/contribution/ContributionVisualizationPage.kt
@@ -4,7 +4,9 @@ import com.zegreatrob.coupling.client.components.contribution.ContributionConten
 import com.zegreatrob.coupling.client.partyPageFunction
 import com.zegreatrob.coupling.client.routing.CouplingQuery
 import com.zegreatrob.coupling.sdk.gql.graphQuery
+import js.lazy.Lazy
 
+@Lazy
 val ContributionVisualizationPage = partyPageFunction { props, partyId ->
     CouplingQuery(
         commander = props.commander,
