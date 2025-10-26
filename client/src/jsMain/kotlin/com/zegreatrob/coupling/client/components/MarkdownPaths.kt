e267ff7d7b40e3c1809dd3c5c6f7d693e0bdcfac
RoB Murdock <rob@zegreatrob.com>
2025-10-25 20:17:11 -0400

    more vite tweaks

--- client/src/jsMain/kotlin/com/zegreatrob/coupling/client/components/MarkdownPaths.kt
+++ client/src/jsMain/kotlin/com/zegreatrob/coupling/client/components/MarkdownPaths.kt
@@ -1,21 +1,21 @@
 package com.zegreatrob.coupling.client.components
 
-@JsModule("/com/zegreatrob/coupling/client/ConnectSuccess.md")
+@JsModule("/com/zegreatrob/coupling/client/ConnectSuccess.md?raw")
 private external val connectSuccessMd: String
 
-@JsModule("/com/zegreatrob/coupling/client/ContributionStart.md")
+@JsModule("/com/zegreatrob/coupling/client/ContributionStart.md?raw")
 private external val contributionStartMd: String
 
-@JsModule("/com/zegreatrob/coupling/client/Boost.md")
+@JsModule("/com/zegreatrob/coupling/client/Boost.md?raw")
 private external val boostMd: String
 
-@JsModule("/com/zegreatrob/coupling/client/recent-info.md")
+@JsModule("/com/zegreatrob/coupling/client/recent-info.md?raw")
 private external val recentInfoMd: String
 
-@JsModule("/com/zegreatrob/coupling/client/About.md")
+@JsModule("/com/zegreatrob/coupling/client/About.md?raw")
 private external val aboutMd: String
 
-@JsModule("/com/zegreatrob/coupling/client/InstallSuccess.md")
+@JsModule("/com/zegreatrob/coupling/client/InstallSuccess.md?raw")
 private external val installSuccessMd: String
 
 fun loadMarkdown() {
