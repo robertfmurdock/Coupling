5348e70e00e9308f5e5c4d3b7e8ef9925f0d9902
RoB Murdock <rob@zegreatrob.com>
2025-10-31 12:56:15 -0400

    using apollo query in cli

--- cli/src/commonMain/kotlin/com/zegreatrob/coupling/cli/party/List.kt
+++ cli/src/commonMain/kotlin/com/zegreatrob/coupling/cli/party/List.kt
@@ -4,16 +4,18 @@ import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.options.default
 import com.github.ajalt.clikt.parameters.options.option
 import com.zegreatrob.coupling.cli.cliScope
+import com.zegreatrob.coupling.cli.gql.PartyListQuery
 import com.zegreatrob.coupling.cli.withSdk
-import com.zegreatrob.coupling.sdk.gql.graphQuery
+import com.zegreatrob.coupling.sdk.gql.ApolloGraphQuery
+import com.zegreatrob.coupling.sdk.toModel
 
 class List : CliktCommand() {
     private val env by option().default("production")
     override fun run() {
         withSdk(cliScope, env, ::echo) { sdk ->
-            sdk.fire(graphQuery { partyList { details() } })
+            sdk.fire(ApolloGraphQuery(PartyListQuery()))
                 ?.partyList
-                ?.mapNotNull { it.details?.data }
+                ?.mapNotNull { it.details?.partyDetailsFragment?.toModel() }
                 ?.joinToString("\n") { "Party: id = ${it.id.value}, name = ${it.name}" }
                 .let { it ?: "" }
                 .let { echo(it) }
