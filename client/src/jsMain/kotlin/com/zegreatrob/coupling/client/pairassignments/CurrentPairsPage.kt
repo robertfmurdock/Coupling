f49f06fcbb092482b510f2f80a92871a60459cee
RoB Murdock <rob@zegreatrob.com>
2025-10-30 23:36:47 -0400

    current pair query to apollo

--- client/src/jsMain/kotlin/com/zegreatrob/coupling/client/pairassignments/CurrentPairsPage.kt
+++ client/src/jsMain/kotlin/com/zegreatrob/coupling/client/pairassignments/CurrentPairsPage.kt
@@ -1,37 +1,31 @@
 package com.zegreatrob.coupling.client.pairassignments
 
 import com.zegreatrob.coupling.client.components.Controls
+import com.zegreatrob.coupling.client.gql.CurrentPairsPageQuery
+import com.zegreatrob.coupling.client.pairassignments.list.toModel
+import com.zegreatrob.coupling.client.party.toModel
 import com.zegreatrob.coupling.client.partyPageFunction
 import com.zegreatrob.coupling.client.routing.CouplingQuery
-import com.zegreatrob.coupling.model.element
-import com.zegreatrob.coupling.model.elements
-import com.zegreatrob.coupling.model.party.PartyId
-import com.zegreatrob.coupling.sdk.gql.graphQuery
+import com.zegreatrob.coupling.sdk.gql.ApolloGraphQuery
+import com.zegreatrob.coupling.sdk.toModel
 import js.lazy.Lazy
 
 @Lazy
 val CurrentPairsPage = partyPageFunction { props, partyId ->
     CouplingQuery(
         commander = props.commander,
-        query = currentPairsQuery(partyId),
+        query = ApolloGraphQuery(CurrentPairsPageQuery(partyId)),
         key = partyId.value.toString(),
     ) { reload, dispatchFunc, result ->
         SocketedPairAssignments(
-            party = result.party?.details?.data ?: return@CouplingQuery,
-            boost = result.party?.boost?.data,
-            players = result.party?.playerList?.elements ?: return@CouplingQuery,
-            pairAssignments = result.party?.currentPairAssignmentDocument?.element,
+            party = result.party?.details?.partyDetailsFragment?.toModel()
+                ?: return@CouplingQuery,
+            boost = result.party.boost?.boostDetailsFragment?.toModel(),
+            players = result.party.playerList?.map { it.playerDetailsFragment.toModel() }
+                ?: return@CouplingQuery,
+            pairAssignments = result.party.currentPairAssignmentDocument?.pairAssignmentDetailsFragment?.toModel(),
             controls = Controls(dispatchFunc, reload),
             allowSave = false,
         )
-    }
-}
-
-private fun currentPairsQuery(partyId: PartyId) = graphQuery {
-    party(partyId) {
-        details()
-        playerList()
-        currentPairAssignments()
-        boost()
     }
 }
