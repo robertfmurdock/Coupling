a2cb2cfe2bc8498e443da599b11a5b030925e03c
RoB Murdock <rob@zegreatrob.com>
2025-10-31 09:01:42 -0400

    more apollo

--- client/src/jsMain/kotlin/com/zegreatrob/coupling/client/pairassignments/NewPairAssignmentsPage.kt
+++ client/src/jsMain/kotlin/com/zegreatrob/coupling/client/pairassignments/NewPairAssignmentsPage.kt
@@ -1,30 +1,26 @@
 package com.zegreatrob.coupling.client.pairassignments
 
 import com.zegreatrob.coupling.client.components.Controls
+import com.zegreatrob.coupling.client.gql.NewPairsPageQuery
+import com.zegreatrob.coupling.client.pairassignments.list.toModel
+import com.zegreatrob.coupling.client.party.toModel
 import com.zegreatrob.coupling.client.partyPageFunction
 import com.zegreatrob.coupling.client.routing.CouplingQuery
-import com.zegreatrob.coupling.model.element
-import com.zegreatrob.coupling.model.elements
-import com.zegreatrob.coupling.sdk.gql.graphQuery
+import com.zegreatrob.coupling.sdk.gql.ApolloGraphQuery
+import com.zegreatrob.coupling.sdk.toModel
 import js.lazy.Lazy
 
 @Lazy
 val NewPairAssignmentsPage = partyPageFunction { props, partyId ->
     CouplingQuery(
         commander = props.commander,
-        query = graphQuery {
-            party(partyId) {
-                details()
-                playerList()
-                currentPairAssignments()
-            }
-        },
+        query = ApolloGraphQuery(NewPairsPageQuery(partyId)),
         key = partyId.value.toString(),
     ) { reload, dispatchFunc, result ->
         SocketedPairAssignments(
-            party = result.party?.details?.data ?: return@CouplingQuery,
-            players = result.party?.playerList?.elements ?: return@CouplingQuery,
-            pairAssignments = result.party?.currentPairAssignmentDocument?.element,
+            party = result.party?.details?.partyDetailsFragment?.toModel() ?: return@CouplingQuery,
+            players = result.party.playerList?.map { it.playerDetailsFragment.toModel() } ?: return@CouplingQuery,
+            pairAssignments = result.party.currentPairAssignmentDocument?.pairAssignmentDetailsFragment?.toModel(),
             controls = Controls(dispatchFunc, reload),
             allowSave = true,
         )
--- client/src/jsMain/kotlin/com/zegreatrob/coupling/client/stats/StatisticsPage.kt
+++ client/src/jsMain/kotlin/com/zegreatrob/coupling/client/stats/StatisticsPage.kt
@@ -2,42 +2,51 @@ package com.zegreatrob.coupling.client.stats
 
 import com.zegreatrob.coupling.client.components.graphing.CouplingResponsiveLine
 import com.zegreatrob.coupling.client.components.stats.PartyStatistics
+import com.zegreatrob.coupling.client.gql.StatisticsPageQuery
+import com.zegreatrob.coupling.client.party.toModel
 import com.zegreatrob.coupling.client.partyPageFunction
 import com.zegreatrob.coupling.client.routing.CouplingQuery
-import com.zegreatrob.coupling.model.elements
-import com.zegreatrob.coupling.sdk.gql.graphQuery
+import com.zegreatrob.coupling.model.PlayerPair
+import com.zegreatrob.coupling.model.pairassignmentdocument.PairAssignment
+import com.zegreatrob.coupling.model.partyRecord
+import com.zegreatrob.coupling.sdk.gql.ApolloGraphQuery
+import com.zegreatrob.coupling.sdk.toModel
 import js.lazy.Lazy
 
 @Lazy
 val StatisticsPage = partyPageFunction { props, partyId ->
     CouplingQuery(
         commander = props.commander,
-        query = graphQuery {
-            party(partyId) {
-                details()
-                playerList()
-                pairs(includeRetired = false) {
-                    players()
-                    spinsSinceLastPaired()
-                    recentTimesPaired()
-                    pairAssignmentHistory {
-                        date()
-                        recentTimesPaired()
-                    }
-                }
-                medianSpinDuration()
-                spinsUntilFullRotation()
-            }
-        },
+        query = ApolloGraphQuery(StatisticsPageQuery(partyId)),
         key = partyId.value.toString(),
     ) { _, _, queryResult ->
         PartyStatistics(
-            party = queryResult.party?.details?.data ?: return@CouplingQuery,
-            players = queryResult.party?.playerList?.elements ?: return@CouplingQuery,
-            pairs = queryResult.party?.pairs ?: return@CouplingQuery,
-            spinsUntilFullRotation = queryResult.party?.spinsUntilFullRotation ?: return@CouplingQuery,
-            medianSpinDuration = queryResult.party?.medianSpinDuration,
+            party = queryResult.party?.details?.partyDetailsFragment?.toModel() ?: return@CouplingQuery,
+            players = queryResult.party.playerList?.map { it.playerDetailsFragment.toModel() } ?: return@CouplingQuery,
+            pairs = queryResult.party.pairs?.map { it.toModel() } ?: return@CouplingQuery,
+            spinsUntilFullRotation = queryResult.party.spinsUntilFullRotation ?: return@CouplingQuery,
+            medianSpinDuration = queryResult.party.medianSpinDuration,
             chartComponent = CouplingResponsiveLine,
         )
     }
 }
+
+private fun StatisticsPageQuery.Pair.toModel(): PlayerPair = PlayerPair(
+    players = players?.map {
+        partyRecord(
+            partyId = it.partyPlayerDetailsFragment.partyId,
+            modifyingUserEmail = it.partyPlayerDetailsFragment.modifyingUserEmail!!,
+            isDeleted = it.partyPlayerDetailsFragment.isDeleted,
+            timestamp = it.partyPlayerDetailsFragment.timestamp,
+            data = it.partyPlayerDetailsFragment.playerDetailsFragment.toModel(),
+        )
+    },
+    spinsSinceLastPaired = spinsSinceLastPaired,
+    recentTimesPaired = recentTimesPaired,
+    pairAssignmentHistory = pairAssignmentHistory?.map { it.toModel() },
+)
+
+private fun StatisticsPageQuery.PairAssignmentHistory.toModel(): PairAssignment = PairAssignment(
+    date = date,
+    recentTimesPaired = recentTimesPaired,
+)
