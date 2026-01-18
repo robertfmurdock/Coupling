7e67a4e8f1af1bf3906cfb3d57b759b0a161a5f2
RoB Murdock <rob@zegreatrob.com>
2026-01-17 22:22:18 -0500

    confirming config generation will not override explicit settings unless set

--- cli/src/commonTest/kotlin/com/zegreatrob/coupling/cli/ConfigCommandCLITest.kt
+++ cli/src/commonTest/kotlin/com/zegreatrob/coupling/cli/ConfigCommandCLITest.kt
@@ -5,6 +5,7 @@ import com.zegreatrob.coupling.stubmodel.stubPartyId
 import com.zegreatrob.minassert.assertIsEqualTo
 import com.zegreatrob.testmints.async.ScopeMint
 import com.zegreatrob.testmints.async.asyncSetup
+import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.Json.Default.decodeFromString
 import kotlin.test.Test
 
@@ -15,12 +16,7 @@ class ConfigCommandCLITest {
         val workingDir = createTempDirectory()
         val configFile = "$workingDir/.coupling"
         val partyId = stubPartyId()
-    }) {
-//        Json.encodeToString(
-//            CouplingCliConfig(partyId = partyId),
-//        )
-//            .writeToFile(configFile)
-    } exercise {
+    }) exercise {
         cli()
             .test("config --party-id=${partyId.value}", envvars = mapOf("PWD" to workingDir))
     } verify { result ->
@@ -30,5 +26,50 @@ class ConfigCommandCLITest {
         readFromFile(configFile)
             ?.let { decodeFromString(CouplingCliConfig.serializer(), it) }
             .assertIsEqualTo(CouplingCliConfig(partyId = partyId), "Config file should have been updated")
+    }
+
+    @Test
+    fun givenExistingContentOfFileCommandWillNotOverrideSetting() = asyncSetup(object : ScopeMint() {
+        val workingDir = createTempDirectory()
+        val configFile = "$workingDir/.coupling"
+        val partyId = stubPartyId()
+    }) {
+        Json.encodeToString(
+            CouplingCliConfig(partyId = partyId),
+        )
+            .writeToFile(configFile)
+    } exercise {
+        cli()
+            .test("config", envvars = mapOf("PWD" to workingDir))
+    } verify { result ->
+        result.statusCode.assertIsEqualTo(0, result.output)
+        result.output.trim()
+            .assertIsEqualTo("Updated file: $configFile")
+        readFromFile(configFile)
+            ?.let { decodeFromString(CouplingCliConfig.serializer(), it) }
+            .assertIsEqualTo(CouplingCliConfig(partyId = partyId), "Config file should have been updated")
+    }
+
+    @Test
+    fun givenExistingContentOfFileCommandWillOverrideSettingForSetValues() = asyncSetup(object : ScopeMint() {
+        val workingDir = createTempDirectory()
+        val configFile = "$workingDir/.coupling"
+        val originalPartyId = stubPartyId()
+        val newPartyId = stubPartyId()
+    }) {
+        Json.encodeToString(
+            CouplingCliConfig(partyId = originalPartyId),
+        )
+            .writeToFile(configFile)
+    } exercise {
+        cli()
+            .test("config --party-id ${newPartyId.value}", envvars = mapOf("PWD" to workingDir))
+    } verify { result ->
+        result.statusCode.assertIsEqualTo(0, result.output)
+        result.output.trim()
+            .assertIsEqualTo("Updated file: $configFile")
+        readFromFile(configFile)
+            ?.let { decodeFromString(CouplingCliConfig.serializer(), it) }
+            .assertIsEqualTo(CouplingCliConfig(partyId = newPartyId), "Config file should have been updated")
     }
 }
